85537fd76ce60116df5f0e269d525d454537bc4b
uniconstructor <php1602agregator@gmail.com>
2013-07-16 01:52:42 +0400

    Плагин соцсетей в правом верхнем углу замиенен на кнопку "стать участником". Все результаты поиска теперь открываются в новом окне.

--- protected/extensions/ECMarkup/ECMainInformer/ECMainInformer.php
+++ protected/extensions/ECMarkup/ECMainInformer/ECMainInformer.php
@@ -22,6 +22,41 @@ class ECMainInformer extends CWidget
      */
     public function run()
     {
-        $this->render('main');
+        if ( Yii::app()->user->isGuest )
+        {// гостю предлагаем стать участником
+            $this->render('main');
+        }elseif ( $this->isCustomer() )
+        {// заказчикам показываем корзину
+            $this->render('customer');
+        }elseif ( $this->isUser() )
+        {// участникам показываем сообщения
+            $this->render('user');
+        }
+    }
+    
+    /**
+     * Определить, является ли пользователь заказчиком (он вошел как заказчик
+     * или добавил хотя бы 1 анкету в заказ)
+     * 
+     * @return bool
+     */
+    protected function isCustomer()
+    {
+        $orders = FastOrder::getPendingOrderUsers();
+        if ( ! empty($orders) )
+        {// у пользователя есть хотя бы 1 заказ - это заказчик
+            return true;
+        }
+        return Yii::app()->user->checkAccess('Customer');
+    }
+    
+    /**
+     * определить, является ли пользователь участником
+     * 
+     * @return bool
+     */
+    protected function isUser()
+    {
+        return ( Yii::app()->user->checkAccess('User') AND ! Yii::app()->user->checkAccess('Admin') );
     }
 }
--- protected/modules/catalog/extensions/search/QSearchResults/views/_user.php
+++ protected/modules/catalog/extensions/search/QSearchResults/views/_user.php
@@ -19,7 +19,7 @@ if ( $data and isset($data->user) )
 ?>
 <li class="span2">
     <a href="<?= Yii::app()->createUrl(Yii::app()->getModule('questionary')->profileUrl, array('id' => $data->id));?>" id="catalog-thumbnail-<?= $data->id; ?>" class="thumbnail" 
-        rel="tooltip" data-title="<?= implode(', ', array($data->user->fullname, $data->age)); ?>">
+        rel="tooltip" data-title="<?= implode(', ', array($data->user->fullname, $data->age)); ?>" target="_blank">
         <img style="border-radius: 10px;width: 150px;height: 150px;" src="<?= $image; ?>" alt="<?= $data->user->fullname; ?>">
     </a>
 </li>
