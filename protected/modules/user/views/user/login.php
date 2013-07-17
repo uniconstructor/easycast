00eb2d124315102071248ff7295151c0247a36d7
uniconstructor <php1602agregator@gmail.com>
2013-07-17 12:04:17 +0400

    Исправлен внешний вид формы входа на сайт, оптимизирован код формы

--- protected/modules/user/views/user/login.php
+++ protected/modules/user/views/user/login.php
@@ -5,70 +5,41 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h1><?php echo UserModule::t("Login"); ?></h1>
-
 <?php if(Yii::app()->user->hasFlash('loginMessage')): ?>
-
 <div class="success">
 	<?php echo Yii::app()->user->getFlash('loginMessage'); ?>
 </div>
-
 <?php endif; ?>
 
-<p><?php // echo UserModule::t("Please fill out the following form with your login credentials:"); ?></p>
+<div class="form span6">
+    <h1><?php echo UserModule::t("Login"); ?></h1>
+    
+    <?php 
+        $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+        	'id' => 'login-form',
+        ));
+    ?>
+    
+	<?php echo $form->errorSummary($model); ?>
+	
+	<?php echo $form->textFieldRow($model,'username') ?>
+	<?php echo $form->passwordFieldRow($model,'password') ?>
 
-<div class="form">
-<?php echo CHtml::beginForm(); ?>
-
-	<p class="note"><?php // echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
+	<p class="hint">
+    	<?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | 
+    	<?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
+	</p>
+    
+	<?php echo $form->checkBoxRow($model,'rememberMe'); ?>
 	
-	<?php echo CHtml::errorSummary($model); ?>
+    <br>
+	<?php $this->widget('bootstrap.widgets.TbButton',
+        array('buttonType'=>'submit',
+              'type'=>'primary',
+              'label'=>UserModule::t("Login"),
+              //'htmlOptions' => array('class' => 'btn btn-large btn-primary'),
+        )); 
+    ?>
 	
-		<?php echo CHtml::activeLabelEx($model,'username'); ?>
-		<?php echo CHtml::activeTextField($model,'username') ?>
-	
-		<?php echo CHtml::activeLabelEx($model,'password'); ?>
-		<?php echo CHtml::activePasswordField($model,'password') ?>
-	
-		<p class="hint">
-		<?php echo CHtml::link(UserModule::t("Register"),Yii::app()->getModule('user')->registrationUrl); ?> | <?php echo CHtml::link(UserModule::t("Lost Password?"),Yii::app()->getModule('user')->recoveryUrl); ?>
-		</p>
-	
-		<?php echo CHtml::activeCheckBox($model,'rememberMe'); ?>
-		<?php echo CHtml::activeLabelEx($model,'rememberMe'); ?>
-
-		<?php $this->widget('bootstrap.widgets.TbButton',
-            array('buttonType'=>'submit',
-                  'type'=>'primary',
-                  'label'=>UserModule::t("Login"),
-            )); 
-        ?>
-	
-<?php echo CHtml::endForm(); ?>
+<?php $this->endWidget(); ?>
 </div><!-- form -->
-
-
-<?php
-$form = new CForm(array(
-    'elements'=>array(
-        'username'=>array(
-            'type'=>'text',
-            'maxlength'=>32,
-        ),
-        'password'=>array(
-            'type'=>'password',
-            'maxlength'=>32,
-        ),
-        'rememberMe'=>array(
-            'type'=>'checkbox',
-        )
-    ),
-
-    'buttons'=>array(
-        'login'=>array(
-            'type'=>'submit',
-            'label'=>'Login',
-        ),
-    ),
-), $model);
-?>
