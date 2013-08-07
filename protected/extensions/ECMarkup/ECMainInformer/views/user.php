85c6481b1d6ea26c3a000f9820a0176fb087c7fc
uniconstructor <php1602agregator@gmail.com>
2013-08-07 11:05:00 +0400

    Исправлена верстка информера для участника

--- protected/extensions/ECMarkup/ECMainInformer/views/user.php
+++ protected/extensions/ECMarkup/ECMainInformer/views/user.php
@@ -3,23 +3,25 @@
  * Отображение информации для участника - новые сообщения и приглашения
  */
 ?>
-<div class="ec_user_info">
-    <h4>Заявки и приглашения</h4>
-    <div class="ec_user_info_block">
-        <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
-            Мои приглашения:&nbsp;<?= $invites; ?>
-        <!--/a-->
-    </div>
-    <div class="ec_user_info_block">
-        <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
-            Мои заявки:&nbsp;<?= $requests; ?>
-        <!--/a-->
-    </div>
-    <div class="ec_user_info_block">
-        <? if ( $events > 0 ) { ?>
-        <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
-            Предстоящие съемки:&nbsp;<?= $events; ?>
-        <!--/a-->
-        <? } ?>
+<div class="span2">
+    <div class="ec_user_info">
+        <h4 class="ec_informer_header">Уведомления</h4>
+        <div class="ec_user_info_block">
+            <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
+                Мои приглашения:&nbsp;<?= $invites; ?>
+            <!--/a-->
+        </div>
+        <div class="ec_user_info_block">
+            <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
+                Мои заявки:&nbsp;<?= $requests; ?>
+            <!--/a-->
+        </div>
+        <div class="ec_user_info_block">
+            <? if ( $events > 0 ) { ?>
+            <!--a href="<?= Yii::app()->createUrl('//catalog/catalog/myChoice'); ?>"-->
+                Мои съемки:&nbsp;<?= $events; ?>
+            <!--/a-->
+            <? } ?>
+        </div>
     </div>
 </div>
