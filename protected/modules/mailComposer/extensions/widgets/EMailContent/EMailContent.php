a2c168fca66d22ff5ca2e92d305c125a3f78086d
uniconstructor <php1602agregator@gmail.com>
2013-08-13 00:36:11 +0400

    Подготовлена структура для разбиения верстки письма на отдельные модульные фрагменты

---
+++ protected/modules/mailComposer/extensions/widgets/EMailContent/EMailContent.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Контейнер для блоков (абзацев) текста письма
+ * Получает массив настроек для каждого блока и в цикле создает их
+ */
+class EMailContent extends CWidget
+{
+    /**
+     * (non-PHPdoc)
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+    
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+    
+    }
+}
