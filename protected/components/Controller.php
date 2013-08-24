78100404d76659b7da4c1d8a7b11c3b7e7f09d45
uniconstructor <php1602agregator@gmail.com>
2013-08-24 16:35:43 +0400

    Закомментированы неиспользуемые поля и методы Они будут удалены при следующем тестировании

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -9,22 +9,26 @@ class Controller extends RController//CController
 	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
 	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
 	 */
-	public $layout='//layouts/column1';
+	//public $layout='//layouts/column1';
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
-	public $menu=array();
+	//public $menu=array();
 	/**
 	 * @var array the breadcrumbs of the current page. The value of this property will
 	 * be assigned to {@link CBreadcrumbs::links}. Please refer to {@link CBreadcrumbs::links}
 	 * for more details on how to specify this property.
 	 */
-	public $breadcrumbs=array();
+	//public $breadcrumbs=array();
 	
-	public function filters()
+	/**
+	 * (non-PHPdoc)
+	 * @see CController::filters()
+	 */
+	/*public function filters()
 	{
 	    return array(
 	        //'rights' => array('application.modules.rights.components.RightsFilter'),
 	    );
-	}
+	}*/
 }
