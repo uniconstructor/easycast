cdb7dd6db51bb7355f541bf5090e804d8f9975e3
uniconstructor <php1602agregator@gmail.com>
2013-08-26 01:54:44 +0400

    Форматирование кода, комментарии

--- themes/easycast/views/layouts/main.php
+++ themes/easycast/views/layouts/main.php
@@ -1,5 +1,4 @@
-<?php /* @var $this Controller */ ?>
-<!DOCTYPE html>
+<?php /* @var $this Controller */ ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -8,27 +7,27 @@
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	<?php Yii::app()->bootstrap->register(); ?>
 </head>
-
 <body>
-<?php // шапка страницы
-    $this->widget('ext.ECMarkup.ECHeader.ECHeader', $this->ecHeaderOptions);
-?>
-
-<div class="container" id="page">
-    <?php // Навигация (хлебные крошки) ?>
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-    <?php // Подвал страницы
-        $this->widget('ext.ECMarkup.ECFooter.ECFooter');
+    <?php // шапка страницы
+        $this->widget('ext.ECMarkup.ECHeader.ECHeader', $this->ecHeaderOptions);
     ?>
-</div><!-- page -->
+    <div class="container" id="page">
+        <?php // Верхняя навигация (хлебные крошки) 
+        if ( isset($this->breadcrumbs) )
+        {
+    	    $this->widget('bootstrap.widgets.TbBreadcrumbs', array(
+    			'links' => $this->breadcrumbs,
+    		)); 
+    	{
+    	?>
+    	<!-- breadcrumbs -->
+    	<?php // основное содержимое страницы 
+    	    echo $content;
+	    ?>
+    	<div class="clear"></div>
+        <?php // Подвал страницы
+            $this->widget('ext.ECMarkup.ECFooter.ECFooter');
+        ?>
+    </div><!-- page -->
 </body>
 </html>
