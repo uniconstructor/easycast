9635ff5e2547465c575063e97ee71b3a4cb7b311
uniconstructor <php1602agregator@gmail.com>
2013-09-08 18:13:48 +0400

    Выровнен код.

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,12 +4,12 @@ $basePath = dirname(__FILE__).DIRECTORY_SEPARATOR.'..';
 $consoleConfig = CMap::mergeArray(
     require(dirname(__FILE__).'/production.php'),
     array(
-        'basePath'=>$basePath,
+        'basePath' => $basePath,
         'language' => 'ru',
         'sourceLanguage' => 'en_us',
     
         // autoloading model and component classes
-        'import'=>array(
+        'import' => array(
             'application.models.*',
             'application.components.*',
             'application.modules.user.models.*',
@@ -18,9 +18,9 @@ $consoleConfig = CMap::mergeArray(
         ),
     
         // preloading 'log' component
-        'preload'=>array('log', 'messages'),
+        'preload' => array('log', 'messages'),
     
-        'modules'=>array(
+        /*'modules'=>array(
             'user'=>array(),
             'questionary' => array(),
             'catalog' => array(),
@@ -31,10 +31,10 @@ $consoleConfig = CMap::mergeArray(
             'rights'=>array(
                 'install'=>false,
             ),
-        ),
+        ),*/
     
         // application components
-        'components'=>array(
+        'components' => array(
             
             // устанавливаем пареметр hostInfo для того чтобы из консоли могли создаваться абсолютные ссылки
             // ( http://www.yiiframework.com/forum/index.php/topic/14825-problem-with-createurl-and-createabsoluteurl-in-console-application/ )
@@ -44,19 +44,19 @@ $consoleConfig = CMap::mergeArray(
                 'scriptUrl' => '',
             ),
             
-            'log'=>array(
-                'class'=>'CLogRouter',
-                'routes'=>array(
+            'log' => array(
+                'class'  => 'CLogRouter',
+                'routes' => array(
                     array(
-                        'class'=>'CDbLogRoute',
+                        'class'        => 'CDbLogRoute',
                         'connectionID' => 'db',
-                        'levels'=>'error, warning, info, AWS',
+                        'levels'       => 'error, warning, info, AWS',
                         'autoCreateLogTable' => true,
                     ),
                     array(
-                        'class'=>'CEmailLogRoute',
-                        'levels'=>'error, warning, AWS',
-                        'emails'=>'php1602agregator@gmail.com',
+                        'class'  => 'CEmailLogRoute',
+                        'levels' => 'error, warning, AWS',
+                        'emails' => 'php1602agregator@gmail.com',
                     ),
                 ),
             ),
