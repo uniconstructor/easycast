f89a4b9709c81044a2aae4319c567d61b01a12f7
uniconstructor <php1602agregator@gmail.com>
2013-09-29 02:29:12 +0400

    Добавлено поле "источник данных", стиль кодирования.

--- protected/modules/user/views/admin/_form.php
+++ protected/modules/user/views/admin/_form.php
@@ -1,31 +1,30 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-	'id'=>'user-form',
-	'enableAjaxValidation'=>true,
-	'htmlOptions' => array('enctype'=>'multipart/form-data'),
+<?php 
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+	'id'                   => 'user-form',
+	'enableAjaxValidation' => true,
+	'htmlOptions'          => array('enctype'=>'multipart/form-data'),
 ));
 ?>
-
-	<p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p>
-
+	<!--p class="note"><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></p-->
 	<?php echo $form->errorSummary(array($model)); ?>
-
-		<?php echo $form->textFieldRow($model,'email',array('size'=>60,'maxlength'=>128)); ?>
-
-		<?php echo $form->dropDownListRow($model,'superuser',User::itemAlias('AdminStatus')); ?>
-
-		<?php echo $form->dropDownListRow($model,'status',User::itemAlias('UserStatus')); ?>
-
-	<div class="form-actions">
-		<?php $this->widget('bootstrap.widgets.TbButton',
-            array('buttonType'=>'submit',
-                  'type'=>'primary',
-                  'label'=>$model->isNewRecord ? UserModule::t('Create') : UserModule::t('Save'),
-                  'htmlOptions'  => array('id' => 'save_questionary')
-            )); ?>
-	</div>
-
+	<?php echo $form->textFieldRow($model, 'email', array('autocomplete' => 'off', 'maxlength' => 255)); ?>
+	<?php echo $form->dropDownListRow($model, 'superuser', User::itemAlias('AdminStatus')); ?>
+	<?php echo $form->dropDownListRow($model, 'status', User::itemAlias('UserStatus')); ?>
+	<?php // источник данных (или "владелец анкеты"). Кого указать в качестве "автора" анкеты
+        // 823 - это номер пользователя Светланы (стоит здесь по умолчанию на время ввода ее базы)
+        echo CHtml::label('Источник данных:', 'ownerId');
+	    echo CHtml::dropDownList('ownerId', 823, UserModule::getAdminList());
+    ?>
+    <br>
+	<?php 
+	    $this->widget('bootstrap.widgets.TbButton',
+            array('buttonType'  => 'submit',
+                  'type'        => 'primary',
+                  'label'       => $model->isNewRecord ? UserModule::t('Create') : UserModule::t('Save'),
+                  'htmlOptions' => array('id' => 'save_questionary'),
+        ));
+    ?>
 <?php $this->endWidget(); ?>
-
 </div><!-- form -->
