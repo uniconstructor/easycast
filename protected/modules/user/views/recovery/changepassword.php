b26f7ad1770194eb3cf2f32a6103184399c7dd17
uniconstructor <php1602agregator@gmail.com>
2013-09-29 08:50:23 +0400

    В форме восстановления пароля отключено автозаполнение

--- protected/modules/user/views/recovery/changepassword.php
+++ protected/modules/user/views/recovery/changepassword.php
@@ -18,13 +18,13 @@ $this->breadcrumbs=array(
 	<?php echo CHtml::errorSummary($form); ?>
 	
 	<?php echo CHtml::activeLabelEx($form,'password'); ?>
-	<?php echo CHtml::activePasswordField($form,'password'); ?>
+	<?php echo CHtml::activePasswordField($form,'password', array('autocomplete' => 'off')); ?>
 	<p class="hint">
 	<?php echo UserModule::t("Minimal password length 4 symbols."); ?>
 	</p>
 	
 	<?php echo CHtml::activeLabelEx($form,'verifyPassword'); ?>
-	<?php echo CHtml::activePasswordField($form,'verifyPassword'); ?>
+	<?php echo CHtml::activePasswordField($form,'verifyPassword', array('autocomplete' => 'off')); ?>
 	
 	<div class="submit">
 	<?php echo CHtml::submitButton(UserModule::t("Save"), array('class' => 'btn btn-primary')); ?>
