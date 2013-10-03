b6928f4b3c921fb04f833e15b584af13aad46bb9
uniconstructor <php1602agregator@gmail.com>
2013-10-03 19:42:09 +0400

    Убрано отображение прошедших событий из раздела предстоящих съемок

--- protected/modules/questionary/extensions/widgets/QUserEvents/QUserEvents.php
+++ protected/modules/questionary/extensions/widgets/QUserEvents/QUserEvents.php
@@ -2,6 +2,7 @@
 
 // подключаем класс-родитель виджета
 Yii::import('application.modules.questionary.extensions.widgets.QUserApplications.QUserApplications');
+
 /**
  * Виджет "мои съемки" - выводит все мероприятия в которых участвует пользователь
  */
@@ -22,6 +23,27 @@ class QUserEvents extends QUserApplications
      */
     protected function initItems()
     {
-        $this->items = $this->questionary->activememberinstances;
+        $this->items = array();
+        foreach ( $this->questionary->activememberinstances as $key => $instance )
+        {// Убираем из предстоящих съемок прошедшие события
+            // @todo переписать при рефакторинге, после того как будет введено изменение статуса по крону 
+            if ( (time() < $instance->vacancy->event->timeend) OR ($instance->vacancy->event->nodates) )
+            {
+                $this->items[$key] = $instance;
+            }
+        }
+    }
+    
+    /**
+     * Отобразить сообщение о том, что заявок нет
+     * @param string $mode - какие заявки пытаемся вывести
+     * @return string
+     *
+     * @todo выводить разные сообщения в зависимости от режима отображения
+     */
+    protected function displayEmptyMessage($mode=null)
+    {
+        $text = 'Нет предстоящих съемок';
+        $this->render('message', array('text' => $text));
     }
 }
