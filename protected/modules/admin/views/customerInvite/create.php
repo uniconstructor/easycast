b9c24dccf645117ce281b10e66b8a6bff387e440
uniconstructor <php1602agregator@gmail.com>
2013-10-07 13:10:06 +0400

    Добавлена навигация и комментарии

--- protected/modules/admin/views/customerInvite/create.php
+++ protected/modules/admin/views/customerInvite/create.php
@@ -1,20 +1,42 @@
 <?php
 /**
  * Создание нового приглашения на отбор актеров
+ * @var CustomerInvite $model
  */
+
+// заголовок страницы
+$title = 'Предоставить доступ ';
 
 $this->breadcrumbs = array(
     'Администрирование' => array('/admin'),
-	'Приглашения для заказчиков' => array('/admin/customerInvite/admin'),
-	'Новое приглашение',
+	//'Приглашения для заказчиков' => array('/admin/customerInvite/admin'),
 );
 
-$this->menu = array(
-	//array('label'=>'List CustomerInvite','url'=>array('index')),
-	//array('label'=>'Manage CustomerInvite','url'=>array('admin')),
-);
+switch ( $model->objecttype )
+{
+    case 'project':
+        $this->breadcrumbs['Проекты'] = array('/admin/project/admin');
+        $this->breadcrumbs[$model->project->name] = array('/admin/project/view', 'id' => $model->project->id);
+        $title .= 'к отбору заявок на проект "'.CHtml::encode($model->project->name).'"' ;
+    break;
+    case 'event':
+        $this->breadcrumbs['Проекты'] = array('/admin/project/admin');
+        $this->breadcrumbs[$model->event->project->name] = array('/admin/project/view', 'id' => $model->event->project->id);
+        $this->breadcrumbs[$model->event->name] = array('/admin/projectEvent/view', 'id' => $model->event->id);
+        $title .= 'к отбору заявок на событие "'.CHtml::encode($model->event->name).'" '.$model->event->getFormattedTimePeriod();
+    break;
+    case 'vacancy':
+        $this->breadcrumbs['Проекты'] = array('/admin/project/admin');
+        $this->breadcrumbs[$model->vacancy->event->project->name] = array('/admin/project/view', 'id' => $model->vacancy->event->project->id);
+        $this->breadcrumbs[$model->vacancy->event->name] = array('/admin/projectEvent/view', 'id' => $model->vacancy->event->id);
+        $this->breadcrumbs[$model->vacancy->name] = array('/admin/eventVacancy/view', 'id' => $model->vacancy->id);
+        $title .= 'к отбору заявок на роль "'.CHtml::encode($model->vacancy->name).'" '.$model->vacancy->event->getFormattedTimePeriod();
+    break;
+    default: throw new CHttpException(400, 'Неизвестный тип приглашения ('.$model->objecttype.')');
+}
+$this->breadcrumbs[] = 'Предоставить доступ';
 ?>
 
-<h1>Новое приглашение на отбор актеров</h1>
+<h2><?= $title; ?></h2>
 
-<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+<?php echo $this->renderPartial('_form', array('model' => $model)); ?>
