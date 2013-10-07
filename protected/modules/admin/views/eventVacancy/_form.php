a8d2bc9d664b05bd7a311db21976469ae150aae5
uniconstructor <php1602agregator@gmail.com>
2013-10-07 11:01:07 +0400

    Стиль кодирования

--- protected/modules/admin/views/eventVacancy/_form.php
+++ protected/modules/admin/views/eventVacancy/_form.php
@@ -1,15 +1,15 @@
-<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+<?php 
+/**
+ * Форма создания вакансии (роли)
+ */
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id'=>'event-vacancy-form',
 	'enableAjaxValidation'=>false,
-)); ?>
-
-	<?php echo Yii::t('coreMessages','form_required_fields', array('{mark}' => '<span class="required">*</span>')); ?>
-
+)); 
+?>
 	<?php echo $form->errorSummary($model); ?>
-
-	<?php echo $form->textFieldRow($model,'name',array('class'=>'span5','maxlength'=>255)); ?>
-
-	<?php 
+	<?php echo $form->textFieldRow($model, 'name', array('class'=>'span5','maxlength'=>255)); ?>
+	<?php // описание роли
 	echo $form->labelEx($model,'description'); 
     $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
     	'model' => $model,
@@ -20,15 +20,15 @@
     ));
     echo $form->error($model,'description');
     ?>
-
-	<?php echo $form->textFieldRow($model,'limit',array('class'=>'span5','maxlength'=>6)); ?>
-	
-	<?php echo $form->textFieldRow($model,'salary',array('class'=>'span5','maxlength'=>7)); ?>
+	<?php echo $form->textFieldRow($model, 'limit', array('class'=>'span5','maxlength'=>6)); ?>
+	<?php echo $form->textFieldRow($model, 'salary', array('class'=>'span5','maxlength'=>7)); ?>
     <br>
-	<?php $this->widget('bootstrap.widgets.TbButton', array(
-		'buttonType'=>'submit',
-		'type'=>'primary',
-		'label'=>$model->isNewRecord ? 'Создать' : 'Сохранить',
-	)); ?>
+	<?php 
+	$this->widget('bootstrap.widgets.TbButton', array(
+		'buttonType' => 'submit',
+		'type'       => 'primary',
+		'label'      => $model->isNewRecord ? 'Создать' : 'Сохранить',
+	)); 
+	?>
 
 <?php $this->endWidget(); ?>
