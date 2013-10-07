f73ec88d520ada73e6b450e0ae7c3f3f1b79ed72
uniconstructor <php1602agregator@gmail.com>
2013-10-07 11:34:33 +0400

    Стиль кодирования, комментарии

--- protected/modules/admin/views/project/update.php
+++ protected/modules/admin/views/project/update.php
@@ -1,28 +1,29 @@
 <?php
-$this->breadcrumbs=array(
-    'Администрирование' =>array('/admin'),
-	'Проекты'=>array('/admin/project/admin'),
-	$model->name=>array('/admin/project/view','id'=>$model->id),
+/**
+ * Страница редактирования проекта в админке
+ */
+
+$this->breadcrumbs = array(
+    'Администрирование' => array('/admin'),
+	'Проекты' => array('/admin/project/admin'),
+	$model->name => array('/admin/project/view', 'id'=>$model->id),
 	'Редактировать',
 );
 
 $this->menu=array(
-	array('label'=>'Список проектов','url'=>array('/admin/project/admin')),
-	array('label'=>'Создать проект','url'=>array('/admin/project/create')),
-	array('label'=>'Просмотр проекта','url'=>array('/admin/project/view','id'=>$model->id)),
-	//array('label'=>'Manage Project','url'=>array('admin')),
+	array('label' => 'Список проектов', 'url' => array('/admin/project/admin')),
+	array('label' => 'Создать проект', 'url' => array('/admin/project/create')),
+	array('label' => 'Просмотр проекта', 'url' => array('/admin/project/view', 'id'=>$model->id)),
 );
 ?>
 
-<h1>Обновить проект "<?php echo $model->name; ?>"</h1>
+<h1>Редактировать проект "<?php echo $model->name; ?>"</h1>
 
 <?php 
-    echo $this->renderPartial('_form',
-                            array(
-                                'model'=>$model,
-
-                                'video'=>$video,
-                                'validatedVideos'=>$validatedVideos,
-                            )
-                        );
-?>
+echo $this->renderPartial('_form',
+    array(
+        'model' => $model,
+        'video' => $video,
+        'validatedVideos' => $validatedVideos,
+    )
+);
