2f065301430ee63cc18654a43d326e1abb14d7e2
uniconstructor <php1602agregator@gmail.com>
2013-10-07 13:09:30 +0400

    Добавлена AJAX-проверка формы

--- protected/modules/admin/views/customerInvite/_form.php
+++ protected/modules/admin/views/customerInvite/_form.php
@@ -6,15 +6,15 @@
 
 $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 	'id' => 'customer-invite-form',
-	'enableAjaxValidation' => false,
+	'enableAjaxValidation' => true,
 ));
 
 
 ?>
 	<?php echo $form->errorSummary($model); ?>
 
-	<?php echo $form->textFieldRow($model, 'email',array('class'=>'span5','maxlength'=>255)); ?>
-	<?php echo $form->textFieldRow($model, 'name',array('class'=>'span5','maxlength'=>255)); ?>
+	<?php echo $form->textFieldRow($model, 'email',array('class'=>'span5', 'maxlength' => 255)); ?>
+	<?php echo $form->textFieldRow($model, 'name',array('class'=>'span5', 'maxlength' => 255)); ?>
 	
 	<?php // Наш комментарий для заказчика
 	echo $form->labelEx($model, 'comment'); 
@@ -27,6 +27,10 @@ $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
     ));
     echo $form->error($model, 'comment');
     ?>
+    <div class="alert alert-info">
+        Этот комментарий добавится в письмо с приглашением, в самый конец. Используйте это поле
+        чтобы сообщить заказчику дополнительную информацию.
+    </div>
 
 	<div class="form-actions">
 		<?php
