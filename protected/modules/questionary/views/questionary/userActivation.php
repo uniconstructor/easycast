0d4fbe032c65f35b8a62ba6ef0c1d6e1c3f501d4
uniconstructor <php1602agregator@gmail.com>
2013-10-13 15:08:45 +0400

    Добавлена верстка страницы самостоятельной активации

---
+++ protected/modules/questionary/views/questionary/userActivation.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+
+<div class="row span8 offset2" style="text-align:center;">
+    <div class="alert alert-block alert-success">
+    Ваша учетная запись активирована.
+    Теперь вы сможете получать приглашения на съемки, выбрать проекты для участия, или отредактировать свою анкету.</div>
+    <a class="btn btn-large btn-primary" href="<?= $url; ?>">Перейти к анкете</a>
+</div>
