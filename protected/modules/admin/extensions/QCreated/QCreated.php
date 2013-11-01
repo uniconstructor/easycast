9f93008f8af65be1ecbd5d62017793938a3c0973
uniconstructor <php1602agregator@gmail.com>
2013-11-01 15:29:20 +0400

    Исправлено отображение введенных анкет

--- protected/modules/admin/extensions/QCreated/QCreated.php
+++ protected/modules/admin/extensions/QCreated/QCreated.php
@@ -16,7 +16,7 @@ class QCreated extends CWidget
     public function init()
     {
         $date = DateTime::createFromFormat('Y-m-d' , '2013-10-01');
-        //$this->startDate = 0;//$date->format('U');
+        $this->startDate = $date->format('U');
         if ( ! $this->userId )
         {
             $this->userId = Yii::app()->user->id;
@@ -34,6 +34,9 @@ class QCreated extends CWidget
         $criteria->compare('userid', $this->userId);
         $criteria->compare('timecreated', '>'.$this->startDate);
         $criteria->order = 'timecreated DESC';
+        $criteria->distinct = true;
+        $criteria->select = array('questionaryid');
+        //$criteria->index = 'questionaryid';
         
         $dataProvider = new CActiveDataProvider('QCreationHistory',
             array(
@@ -42,20 +45,15 @@ class QCreated extends CWidget
             )
         );
         
-        /*$actions = new TbButtonColumn;
-        $actions->template='{view} {update}';
-        $actions->viewButtonUrl   = 'Yii::app()->controller->createUrl("/questionary/questionary/view", array("id" => $data->questionaryid))';
-        $actions->updateButtonUrl = 'Yii::app()->controller->createUrl("/questionary/questionary/update", array("id" => $data->questionaryid))';
-        */
         $this->widget('bootstrap.widgets.TbGridView', array(
             'dataProvider' => $dataProvider,
-            'template'     => '{summary}{items}',
+            'template'     => '{summary}{items}{pager}',
             'columns' => array(
                 array( // ФИО
-                    'name'   => 'fullname',
+                    //'name'   => 'fullname',
                     'value'  => '($data->questionary ? CHtml::link($data->questionary->fullname, Yii::app()->createAbsoluteUrl("/questionary/questionary/view", array("id" => $data->questionaryid))) : "(Анкета удалена)" )',
                     'header' => '<b>ФИО</b>',
-                    'type'   => 'html',
+                    'type'   => 'raw',
                 ),
                 array( // display a column with "view", "update" and "delete" buttons
                     'class'           => 'bootstrap.widgets.TbButtonColumn',
