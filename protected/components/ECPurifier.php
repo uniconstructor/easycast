1756d19872c17f6a5badfa51b8f8b3d3ba084017
uniconstructor <php1602agregator@gmail.com>
2013-11-23 00:12:30 +0400

    Добавлен собственный класс фильтров для значений.

---
+++ protected/components/ECPurifier.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Наш собственный класс с дополнительными функциями очистки данных
+ */
+class ECPurifier extends CHtmlPurifier
+{
+    /**
+     * Обрезать все кавычки из названия
+     * @param unknown $string
+     * @return string
+     * @return void
+     */
+    public static function trimQuotes($string)
+    {
+        $string = trim($string);
+        if ( mb_eregi("^['\"«»„“]{1-2}.{1+}['\"«»„“]{1-2}$", $string) )
+        {// проверим, что строка начинается и заканчивается кавычками, чтобы не отрезать лишнееы
+            // в случаях типа: БАЛЕТ "ЛЕБЕДИНОЕ ОЗЕРО"
+            $string = trim($string, " \t\n\r\0\x0B'\"«»„“");
+        }
+        return $string;
+    }
+}
