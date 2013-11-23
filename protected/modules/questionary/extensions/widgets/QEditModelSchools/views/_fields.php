572fb024266ddb34acf7d24deab82e3dfccef09a
uniconstructor <php1602agregator@gmail.com>
2013-11-23 08:30:02 +0400

    Полностью переработаны виджеты редактирования модельных школ и показов

---
+++ protected/modules/questionary/extensions/widgets/QEditModelSchools/views/_fields.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Список полей формы во всплывающем modal-окне
+ * Структура своя для каждого виджета
+ */
+/* @var $form TbActiveForm */
+/* @var $this QEditFilms */
+/* @var $model QFilmInstance */
+
+// ошибки формы
+echo $form->errorSummary(array($model), null, null, array('id' => $this->formId.'_errors'));
+// название
+echo $form->textFieldRow($model, 'school');
+// год
+echo $form->datepickerRow($model, 'year', array(
+    'options' => $this->getYearPickerOptions(),
+));
