1622ff949252a61a13f67a65725216f4e7465337
uniconstructor <php1602agregator@gmail.com>
2013-12-06 11:18:29 +0400

    Улучшено отображение верхнего колонтитула всех писем

--- protected/modules/mailComposer/extensions/widgets/EMailTopBar/views/bar.php
+++ protected/modules/mailComposer/extensions/widgets/EMailTopBar/views/bar.php
@@ -6,56 +6,26 @@
 ?>
 <tr>
     <td class="w640" width="640">
-        <table id="top-bar" class="w640" width="640" bgcolor="#0099cc" style="border-radius:10px 10px 0px 0px;"
+        <table id="top-bar" class="w640" width="640" bgcolor="#09c" style="border-radius:10px 10px 0px 0px;"
             border="0" cellpadding="0" cellspacing="0">
             <tbody>
                 <tr>
                     <td class="w15" width="15"></td>
-                    <td class="w325" valign="middle" width="350"
-                        align="left">
-                        <table class="w325" width="350" border="0"
-                            cellpadding="0" cellspacing="0">
+                    <td class="w610" valign="middle" width="610" align="left">
+                        <table class="w610" width="610" border="0" cellpadding="0" cellspacing="0">
                             <tbody>
                                 <tr>
-                                    <td class="w325" height="8"
-                                        width="350">&nbsp;</td>
+                                    <td class="w610" height="8" width="610">&nbsp;</td>
                                 </tr>
                             </tbody>
                         </table>
                         <?php // выводим служебные ссылки 
                             $this->displayLinks();
                         ?>
-                        <table class="w325" width="350" border="0"
-                            cellpadding="0" cellspacing="0">
+                        <table class="w610" width="610" border="0" cellpadding="0" cellspacing="0">
                             <tbody>
                                 <tr>
-                                    <td class="w325" height="8"
-                                        width="350">&nbsp;</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </td>
-                    <td class="w30" width="30"></td>
-                    <td class="w255" valign="middle" width="255"
-                        align="right">
-                        <table class="w255" width="255" border="0"
-                            cellpadding="0" cellspacing="0">
-                            <tbody>
-                                <tr>
-                                    <td class="w255" height="8"
-                                        width="255"></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <?php // Выводим кнопки соцсетей
-                            $this->displaySocial();
-                        ?>
-                        <table class="w255" width="255" border="0"
-                            cellpadding="0" cellspacing="0">
-                            <tbody>
-                                <tr>
-                                    <td class="w255" height="8"
-                                        width="255">&nbsp;</td>
+                                    <td class="w610" height="8" width="610">&nbsp;</td>
                                 </tr>
                             </tbody>
                         </table>
