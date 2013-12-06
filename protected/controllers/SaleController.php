b29da137e1a5f46a5f6285000395a9f4195be40d
uniconstructor <php1602agregator@gmail.com>
2013-12-06 15:44:19 +0400

    Первая версия коммерческого предложения - без адаптации к верстке сайта

---
+++ protected/controllers/SaleController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Контроллер для работы с коммерческим предложением и всем что связано с продажами
+ */
+class SaleController extends Controller
+{
+    /**
+     * 
+     * @return void
+     */
+    public function actionIndex()
+    {
+        
+    }
+}
