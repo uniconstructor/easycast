c70399cd31ac7bbaab36b6f366c0c70220b54a87
uniconstructor <php1602agregator@gmail.com>
2013-12-09 16:41:37 +0400

    Изменены стили кнопки регистрации

--- protected/extensions/ECMarkup/ECMainInformer/views/main.php
+++ protected/extensions/ECMarkup/ECMainInformer/views/main.php
@@ -8,6 +8,6 @@
 $this->widget('ext.ECMarkup.ECLoginWidget.ECLoginWidget');
 ?>
 <div class="ec-join_but">
-    <a class="btn btn-success btn-large" href="/user/registration"
+    <a class="btn ec-btn-primary reg btn-lg" href="/user/registration"
         data-toggle="modal" data-target="#registration-modal">Регистрация</a>
 </div>
