d6b42c0d6444c579b5ca98a71b87ebd2ab128842
uniconstructor <php1602agregator@gmail.com>
2014-01-09 03:32:58 +0400

    Исправлено отображение поиска после правки параметров виджета CGridView в config.php

--- protected/modules/catalog/extensions/search/QSearchResults/views/_user.php
+++ protected/modules/catalog/extensions/search/QSearchResults/views/_user.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Отображает одну картинку пользователя
- * 
- * @var data $data 
+ * Отображает одну картинку пользователя в результатах поиска, с краткой информацией о нем
+ * @todo вынести всю логику из view
+ * @todo выводить краткую информацию по пользователю
  */
 
 // Проверка на случай если пользователь не загрузил изображение
@@ -14,10 +14,10 @@ if ( $cover = $data->getAvatarUrl('catalog') )
     $image = $data->getAvatarUrl();
 }
 
-if ( $data and isset($data->user) )
+if ( $data AND isset($data->user) )
 {
 ?>
-<li class="span2" style="margin-right:-20px;" rel="tooltip" title=""
+<li style="margin-right:0px;" rel="tooltip" title=""
     data-original-title="<?= implode(', ', array($data->user->fullname, $data->age)); ?>"  data-placement="top" 
     data-toggle="tooltip">
     <a href="<?= Yii::app()->createUrl(Yii::app()->getModule('questionary')->profileUrl, array('id' => $data->id));?>" 
