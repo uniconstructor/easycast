38a33457307c21c74073f239af049790e8b86ebb
uniconstructor <php1602agregator@gmail.com>
2014-01-17 18:44:37 +0400

    Добавлен виджет с краткой информацией о проекте (для получения данных через AJAX)

---
+++ protected/modules/projects/extensions/AjaxProjectInfo/views/onlyDescription.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Информация о проекте: только полное описание (без колонки с логотипом)
+ */
+/* @var $this AjaxProjectInfo */
+?>
+<div class="row-fluid">
+    <div class="span12"><?= $description; ?></div>
+</div>
