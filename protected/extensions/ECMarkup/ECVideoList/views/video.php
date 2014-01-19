927e244b1127e7f4a1e4263572de9bfdaa236305
uniconstructor <php1602agregator@gmail.com>
2014-01-19 09:37:57 +0400

    Убраны лишние параметры из ссылок

--- protected/extensions/ECMarkup/ECVideoList/views/video.php
+++ protected/extensions/ECMarkup/ECVideoList/views/video.php
@@ -19,7 +19,7 @@ if ( $data->type == 'youtube' AND $data->externalid )
 }else
 {
     $url = Sweeml::raiseOpenShadowboxUrl('#', array(
-        'player' => 'html',
+        'player'  => 'html',
         'content' => '<div style="background-color:#ffffff;text-align:center;height:240px;width:360px;"><br><br><br><br><br>'.
             CHtml::link('<h4>(Открыть видео в новом окне)</h4>', $data->link, array('target' => '_blank')).'</div>',
         'width'  => 360,
@@ -30,6 +30,6 @@ if ( $data->type == 'youtube' AND $data->externalid )
 ?>
 <li class="span4">
     <?= Sweeml::link($image, $url, array('class' => "thumbnail")); ?>
-    <?= Sweeml::link(CHtml::encode($data->name), $url, array()); ?>
+    <?= Sweeml::link(CHtml::encode($data->name), $url); ?>
 </li>
 
