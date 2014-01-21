21b86ff5f0bc0a23a615f1c2189929c43b5b2652
uniconstructor <php1602agregator@gmail.com>
2014-01-21 16:57:29 +0400

    Добавлен виджет "Список ролей для мероприятия".

---
+++ protected/modules/projects/extensions/VacancyList/VacancyList.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Список ролей для мероприятия, проекта или участника
+ * 
+ * @todo добавить проверку входных параметров
+ * @todo настройка: выводить ли сообщение если нет подходящих ролей
+ */
+class VacancyList extends CWidget
+{
+    /**
+     * @var string - тип объекта для которого отображается список ролей
+     *               event
+     *               project
+     *               questionary
+     */
+    public $objectType;
+    /**
+     * @var ProjectEvent
+     */
+    public $event;
+    /**
+     * @var Project
+     */
+    public $project;
+    /**
+     * @var Questionary - анкета просматривающего роль участника (только для $this->displayMode = 'user')
+     */
+    public $questionary;
+    
+    /**
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+        switch ( $this->objectType )
+        {
+            case 'event': 
+                if ( ! ($this->event instanceof ProjectEvent) )
+                {
+                    throw new CException('Не передано мероприятие для отображения списка ролей');
+                }
+            break;
+            default: throw new CException('Не передан тип отображения для списка ролей');
+        }
+        parent::init();
+    }
+    
+    /**
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+        if ( ! $vacancies = $this->getVacancies() )
+        {// @todo получать активные и завершенные роли (после написания условий с параметров в модели роли)
+            return;
+        }
+        foreach ( $vacancies as $vacancy )
+        {// по каждой роли выводим краткую информацию + кнопки действий
+            $this->widget('projects.extensions.VacancyInfo.VacancyInfo', array(
+                'vacancy'             => $vacancy,
+                'questionary'         => $this->questionary,
+                //'displayNotAvailable' => $displayNotAvailable,
+                'displayNotAvailable' => false,
+                'isAjaxRequest'       => true,
+                'isAvailable'         => $vacancy->isAvailableForUser($this->questionary->id, true)
+            ));
+        }
+    }
+    
+    /**
+     * Получить список ролей для отображения
+     * @return EventVacancy[]
+     */
+    protected function getVacancies()
+    {
+        switch ( $this->objectType )
+        {
+            case 'event': return $this->event->activevacancies; break;
+        }
+    }
+}
