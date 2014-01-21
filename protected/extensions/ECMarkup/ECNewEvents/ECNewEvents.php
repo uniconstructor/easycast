cf0f5edc106a844e5cac5cbc592369118ea2a684
uniconstructor <php1602agregator@gmail.com>
2014-01-21 20:26:24 +0400

    Черновой вариант виджета "наши события" на главной

---
+++ protected/extensions/ECMarkup/ECNewEvents/ECNewEvents.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Виджет "наши события" на главной странице (для участников)
+ */
+class ECNewEvents extends CWidget
+{
+    /**
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+        parent::init();
+    }
+    
+    /**
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+        
+    }
+}
