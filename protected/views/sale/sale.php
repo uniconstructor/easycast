b2c4ce5869f333f22072136616dc7717abd00720
uniconstructor <php1602agregator@gmail.com>
2014-02-10 09:35:36 +0400

    Временная версия коммерческого предложения

--- protected/views/sale/sale.php
+++ protected/views/sale/sale.php
@@ -39,7 +39,7 @@
 <!-- END HEADER headertop NAV -->
 
 <!-- TOP SECTION-->
-<section class="headertop needhead" id="header-section" style="margin-top:70px;">
+<section class="headertop needhead" id="header-section">
     
     <div class="container">
         <div class="row-fluid">
@@ -56,10 +56,58 @@
         
     </div>
     <!-- Список услуг -->
-    <div class="row-fluid">
-        <div id="cd-photo-letter-container" class="cd-photo-letter-container">
-    		<h2><a href="javascript: return false;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></h2>
-    	</div>
+    <div class="lp-static-photos">
+        <ul>
+			<li>
+				<a href="#"><img src="<?= Yii::app()->theme->baseUrl; ?>/images/services/01-Media.jpg" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px -16px -56px; z-index: 27; position: relative;">
+				<a href="#"><img src="<?= Yii::app()->theme->baseUrl; ?>/images/services/02-Services.jpg" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px 0px -52px;  z-index: 40; position: relative;">
+				<a href="#"><img src="<?= Yii::app()->theme->baseUrl; ?>/images/services/03-Models.jpg" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px 0px -50px;  z-index: 35; position: relative;">
+				<a href="#"><img src="<?= Yii::app()->theme->baseUrl; ?>/images/services/04-Children.jpg" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px 0px -52px;  z-index: 40; position: relative;">
+				<a href="#"><img src="<?= Yii::app()->theme->baseUrl; ?>/images/services/05-Castings.jpg" alt="" title=""/></a>
+			</li>
+		</ul>
+		<ul style="margin-top: -65px;">
+			<li style="margin:0px 0px 0px 5px;  z-index: 30; position: relative;">
+				<a href="#"><img src="images/s6.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px 0px -56px; z-index: 28; position: relative;">
+				<a href="#"><img src="images/s7.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:3px 0px 0px -63px;  z-index: 40; position: relative;">
+				<a href="#"><img src="images/s8.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px 0px -50px;  z-index: 34; position: relative;">
+				<a href="#"><img src="images/s9.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px -10px -67px;  z-index: 40; position: relative;">
+				<a href="#"><img src="images/s10.png" alt="" title=""/></a>
+			</li>
+		</ul> 
+		<ul style="margin-top: -63px;">
+			<li style="margin:0px 0px 0px -7px;  z-index: 27; position: relative;">
+				<a href="#"><img src="images/s11.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px -35px -48px; z-index: 28; position: relative;">
+				<a href="#"><img src="images/s12.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:3px 0px -11px -53px;  z-index: 40; position: relative;">
+				<a href="#"><img src="images/s13.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px -24px -57px;  z-index: 42; position: relative;">
+				<a href="#"><img src="images/s14.png" alt="" title=""/></a>
+			</li>
+			<li style="margin:0px 0px -40px -67px;  z-index: 40; position: relative;">
+				<a href="#"><img src="images/s15.png" alt="" title=""/></a>
+			</li>
+		</ul> 
 	</div>
 	
     <!-- Кнопки под списком услуг -->
@@ -122,8 +170,8 @@
                             <div class="span9">
                                 <div class="lp-service-text drop-shadow bottom">
                                 <h3 class="lp-service-header">Поиск</h3>
-                                <p class="lead">Найдите нужного артиста. Это быстро и легко - мы сами пользуемся.
-                                Вы вводите параметры и ресурс выдаст вам список реальных людей, 
+                                <p class="lead">Найдите нужного артиста. Это легко и быстро:
+                                вы вводите параметры и ресурс выдаст вам список реальных людей, 
                                 которые подходят под нужное описание. 
                                 Наш поиск содержит <b>25</b> критериев чтобы вы могли найти самый сложный типаж.
                                 </p>
