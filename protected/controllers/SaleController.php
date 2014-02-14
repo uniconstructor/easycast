4ac1bec85355e7a8bddc222f51ec73bdc43f116c
uniconstructor <php1602agregator@gmail.com>
2014-02-14 21:13:14 +0400

    Изменена тема оформления коммерческого предложения.

--- protected/controllers/SaleController.php
+++ protected/controllers/SaleController.php
@@ -2,16 +2,45 @@
 
 /**
  * Контроллер для работы с коммерческим предложением и всем что связано с продажами
- * @todo перенести сюда страницу коммерческого предложения
  */
 class SaleController extends Controller
 {
     /**
-     * 
+     * @var string
+     */
+    public $layout = 'main';
+    
+    /**
+     * @see CController::init()
+     */
+    public function init()
+    {
+        parent::init();
+        // требуется для вывода разделов каталога и результатов поиска при демонстрации
+        Yii::import('application.modules.catalog.models.*');
+        
+        // для коммерческого предложения используется специальная тема оформления
+        /* Yii::app()->theme = 'landing';
+        Yii::app()->bootstrap->packages = array(
+            'bootstrap.css' => array(
+                'basePath' => 'webroot.themes.responsive.bootstrap',
+                'baseUrl'  => null,
+                'css'      => array('css/bootstrap.css'),
+            )
+        );*/
+        
+        Yii::app()->theme = 'staticLanding';
+    }
+    
+    /**
+     * Коммерческое предложение для заказчиков
      * @return void
      */
     public function actionIndex()
     {
-        $this->redirect(Yii::app()->createAbsoluteUrl('/lp/sale/sale.html'));
+        // для коммерческого предложения режим просмотра всегда "заказчик"
+        Yii::app()->getModule('user')->setViewMode('customer');
+        
+        $this->render('sale');
     }
 }
