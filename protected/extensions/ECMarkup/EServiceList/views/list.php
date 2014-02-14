b0b9970550d96f133a7df63d493e5bd6be648307
uniconstructor <php1602agregator@gmail.com>
2014-02-14 21:00:13 +0400

    Добавлен (но не протестирован) виджет "список услуг": он представляет собой список фотографий, каждая из которых является ссылкой на соответствующий раздел каталога.

---
+++ protected/extensions/ECMarkup/EServiceList/views/list.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * 
+ */
+/* @var $this EServiceList */
+?>
+<div class="uslugi">
+    <ul>
+		<li>
+			<a href="#"><img src="<?= Yii::app()->baseUrl; ?>/images/services/01-Media.jpg" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px -16px -56px; z-index: 27; position: relative;">
+			<a href="#"><img src="<?= Yii::app()->baseUrl; ?>/images/services/02-Services.jpg" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px 0px -52px;  z-index: 40; position: relative;">
+			<a href="#"><img src="<?= Yii::app()->baseUrl; ?>/images/services/03-Models.jpg" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px 0px -50px;  z-index: 35; position: relative;">
+			<a href="#"><img src="<?= Yii::app()->baseUrl; ?>/images/services/04-Children.jpg" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px 0px -52px;  z-index: 40; position: relative;">
+			<a href="#"><img src="<?= Yii::app()->baseUrl; ?>/images/services/05-Castings.jpg" alt="" title=""/></a>
+		</li>
+	</ul>
+	<ul style="margin-top: -65px;">
+		<li style="margin:0px 0px 0px 5px;  z-index: 30; position: relative;">
+			<a href="#"><img src="images/s6.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px 0px -56px; z-index: 28; position: relative;">
+			<a href="#"><img src="images/s7.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:3px 0px 0px -63px;  z-index: 40; position: relative;">
+			<a href="#"><img src="images/s8.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px 0px -50px;  z-index: 34; position: relative;">
+			<a href="#"><img src="images/s9.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px -10px -67px;  z-index: 40; position: relative;">
+			<a href="#"><img src="images/s10.png" alt="" title=""/></a>
+		</li>
+	</ul> 
+	<ul style="margin-top: -63px;">
+		<li style="margin:0px 0px 0px -7px;  z-index: 27; position: relative;">
+			<a href="#"><img src="images/s11.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px -35px -48px; z-index: 28; position: relative;">
+			<a href="#"><img src="images/s12.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:3px 0px -11px -53px;  z-index: 40; position: relative;">
+			<a href="#"><img src="images/s13.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px -24px -57px;  z-index: 42; position: relative;">
+			<a href="#"><img src="images/s14.png" alt="" title=""/></a>
+		</li>
+		<li style="margin:0px 0px -40px -67px;  z-index: 40; position: relative;">
+			<a href="#"><img src="images/s15.png" alt="" title=""/></a>
+		</li>
+	</ul> 
+</div>
