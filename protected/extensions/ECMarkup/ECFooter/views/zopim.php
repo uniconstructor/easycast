480bacb02e74af5dc226a00e64a77796b5c2fe5e
uniconstructor <php1602agregator@gmail.com>
2014-03-19 21:21:19 +0400

    Теперь для авторизованных пользователей в онлайн-чат автоматически добавляется ФИО и email

--- protected/extensions/ECMarkup/ECFooter/views/zopim.php
+++ protected/extensions/ECMarkup/ECFooter/views/zopim.php
@@ -10,5 +10,24 @@ d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
 $.src='//v2.zopim.com/?1jwtx0mz7NF3bfar8I0vxNEn7iLRzlDC';z.t=+new Date;$.
 type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+<?php
+
+if ( ! Yii::app()->user->isGuest )
+{// если пользователь уже зашел на сайт - автоматически подставим его имя и email в онлайн-чат
+    $name  = Yii::app()->getModule('user')->user()->questionary->fullname;
+    $email = Yii::app()->getModule('user')->user()->email;
+    
+    echo "\$zopim(function() {";
+    if ( $email )
+    {
+        echo "\$zopim.livechat.setEmail('{$email}');";
+    }
+    if ( $name )
+    {
+        echo "\$zopim.livechat.setName('{$name}');";
+    }
+    echo "});";
+}
+?>
 </script>
 <!--End of Zopim Live Chat Script-->
