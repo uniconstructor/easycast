67c81c6a6c0b789e3813e9721e8c6b6b841f8dea
uniconstructor <php1602agregator@gmail.com>
2014-04-14 05:51:03 +0400

    Добавлен заголовок и пояснение для страницы событий

--- protected/views/agenda/index.php
+++ protected/views/agenda/index.php
@@ -10,6 +10,14 @@ $this->breadcrumbs = array(
 ?>
 <div class="page-alternate">
     <div class="container">
+        <div class="title-page">
+            <h1>Наши события</h1>
+            <h4 class="title-description">
+                Здесь перечислены все съемки и кастинги, которые планируются в ближайшее время. 
+                Вы можете подать заявку на участие в любом событии, в котором для вас есть подходящие роли.<br>
+                Если хотите больше узнать о каком-то проекте - нажмите на его иконку.
+            </h4>
+        </div>
         <?php 
         // все текущие события выводятся одним виджетом
         $this->widget('projects.extensions.EventsAgenda.EventsAgenda', array(
