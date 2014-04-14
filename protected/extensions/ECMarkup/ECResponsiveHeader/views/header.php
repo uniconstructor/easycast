06e73dcec9b76f06ba63708dcd098fb497506f77
uniconstructor <php1602agregator@gmail.com>
2014-04-14 21:06:24 +0400

    Исправлено отображение заголовка на телефоне

--- protected/extensions/ECMarkup/ECResponsiveHeader/views/header.php
+++ protected/extensions/ECMarkup/ECResponsiveHeader/views/header.php
@@ -2,29 +2,27 @@
 /**
  * Шапка страницы (для темы Maximal)
  * @todo сделать версию лого высотой 83px
- * @todo придумать как сделать резиновую шапку, чтобы на телефоне виджет входа не расползался
  */
 /* @var $this ECResponsiveHeader */
 ?>
 <!-- Header -->
-<header>
-    <div class="main-nav">
-        <div class="row-fluid">
-            <div class="span3 text-left">
-                <!--div class="pull-left main-contacts" style="text-align:left;">
-                    +7(495)227-5-226 (Заказчикам)<br>
-                    +7(968)590-88-00 (Пользователям)<br>
-                </div-->
+<header class="row-fluid">
+    <div class="main-nav row-fluid">
+        <div class="span3 text-center">
+            <div class="main-contacts" style="text-align:left;">
+                &nbsp;<i class="icon-phone"></i>&nbsp;+7(495)227-5-226 (Заказчикам)<br>
+                &nbsp;<i class="icon-phone"></i>&nbsp;+7(968)590-88-00 (Пользователям)<br>
+                &nbsp;<i class="icon-envelope"></i>&nbsp;mail@easycast.ru
             </div>
-            <div class="span6 text-center">
-                <a href="<?= Yii::app()->createAbsoluteUrl('//'); ?>" title="easyCast">
-                    <?= CHtml::image(Yii::app()->createAbsoluteUrl('//images/logo.png'), 'easyCast', array('style' => 'max-height:83px;')); ?>
-                </a>
-            </div>
-            <div class="span3 text-right">
-                <div class="ec-user-informer">
-                    <?php $this->widget('ext.ECMarkup.ECUserInformer.ECUserInformer'); ?>
-                </div>
+        </div>
+        <div class="span6 text-center">
+            <a href="<?= Yii::app()->createAbsoluteUrl('//'); ?>" title="easyCast">
+                <?= CHtml::image(Yii::app()->createAbsoluteUrl('//images/logo.png'), 'easyCast', array('style' => 'max-height:83px;')); ?>
+            </a>
+        </div>
+        <div class="span3 text-right">
+            <div class="ec-user-informer">
+                <?php $this->widget('ext.ECMarkup.ECUserInformer.ECUserInformer'); ?>
             </div>
         </div>
     </div>
