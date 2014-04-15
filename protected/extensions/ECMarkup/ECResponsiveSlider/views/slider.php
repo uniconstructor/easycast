a91623102a65307488a7509bc7618074f1d2cc9a
uniconstructor <php1602agregator@gmail.com>
2014-04-15 16:54:50 +0400

    Добавлен виджет "Revolution Slider" вместе с описанием всех сервисов

---
+++ protected/extensions/ECMarkup/ECResponsiveSlider/views/slider.php
@@ -0,0 +1,200 @@
+<?php
+/**
+ * 
+ */
+/* @var $this ECResponsiveSlider */
+?>
+<!-- Homepage Slider -->
+<section id="slider">
+    <div class="fullwidthbanner-container">
+        <div class="fullwidthbanner">
+            <ul>
+                <!-- THE FIRST SLIDE -->
+                <li data-transition="3dcurtain-vertical" data-slotamount="8" data-masterspeed="300" >
+                        <img src="">
+                                
+                <div class="caption lfl"  
+                     data-x="23" 
+                     data-y="90" 
+                     data-speed="800" 
+                     data-start="500" 
+                     data-easing="easeOutExpo"><img src="<?= Yii::app()->baseUrl;?>/images/howitworks/search-example1.png"></div>
+                                
+                <div class="caption lfl"  
+                     data-x="247" 
+                     data-y="267" 
+                     data-speed="800" 
+                     data-start="700" 
+                     data-easing="easeOutExpo"><img src="<?= Yii::app()->baseUrl;?>/images/howitworks/search-example2.png"></div>
+                                
+                <div class="caption very_big_black lfr"  
+                     data-x="619" 
+                     data-y="130" 
+                     data-speed="300" 
+                     data-start="1000" 
+                     data-easing="easeOutExpo">Поиск</div>
+    
+                <div class="caption medium_red lfr"  
+                     data-x="619" 
+                     data-y="238" 
+                     data-speed="300" 
+                     data-start="1200" 
+                     data-easing="easeOutExpo">По 25 критериям и 12 разделам</div>
+                                
+                <div class="caption medium_black lfb"  
+                     data-x="619" 
+                     data-y="337" 
+                     data-speed="300" 
+                     data-start="1400" 
+                     data-easing="easeOutExpo">
+                        Ежедневно в нашей системе регистрируется множество актеров, моделей, <br>
+                        артистов, ведущих, танцоров, музыкантов и вокалистов, ведь мы предлагаем <br>
+                        действительно удобный и надежный сервис. Для удобства поиска все анкеты <br>
+                        автоматически распределяются по 15 разделам с вкладками и фильтрами. <br>
+                        Автоматически обновляемый расширенный поиск быстро и легко найдет <br>
+                        нужного артиста с вашей помощью: просто укажите от 1 до 25 критериев <br>
+                        поиска и найти самый сложный типаж станет легко! 
+                     </div>
+                </li>
+                
+                <li data-transition="3dcurtain-vertical" data-slotamount="8" data-masterspeed="300" >
+                        <img src="_include/img/work/thumbs/image-02.jpg" >
+                                
+                <div class="caption lfl"  
+                     data-x="23" 
+                     data-y="90" 
+                     data-speed="800" 
+                     data-start="500" 
+                     data-easing="easeOutExpo"><img src="<?= Yii::app()->baseUrl;?>/images/howitworks/team-example.png"></div>
+                                
+                <div class="caption lfl"  
+                     data-x="247" 
+                     data-y="267" 
+                     data-speed="800" 
+                     data-start="700" 
+                     data-easing="easeOutExpo"></div>
+                                
+                <div class="caption very_big_black lfr"  
+                     data-x="619" 
+                     data-y="130" 
+                     data-speed="300" 
+                     data-start="1000" 
+                     data-easing="easeOutExpo">Заказ</div>
+    
+                <div class="caption medium_red lfr"  
+                     data-x="619" 
+                     data-y="238" 
+                     data-speed="300" 
+                     data-start="1200" 
+                     data-easing="easeOutExpo">Через персонального менеджера</div>
+                                
+                <div class="caption medium_black lfb"  
+                     data-x="619" 
+                     data-y="337" 
+                     data-speed="300" 
+                     data-start="1400" 
+                     data-easing="easeOutExpo">
+                        Этот сервис создан специально для режиссеров, продюсеров и <br>
+                        кастинг-директоров, которые предпочитают делегировать все <br>
+                        кастинг-задачи и контролировать лишь конечный результат работы <br> 
+                        в своем личном кабинете на нашем сайте. Для вашего удобства и спокойствия -<br> 
+                        наши лучшие руководители проектов. Просто выбираете для своего проекта <br>
+                        персонального менеджера и заполняете короткую форму - остальное сделаем мы! 
+                     </div>
+                </li>
+                <li data-transition="3dcurtain-vertical" data-slotamount="8" data-masterspeed="300" >
+                        <img src="_include/img/work/thumbs/image-02.jpg" >
+                                
+                <div class="caption lfl"  
+                     data-x="23" 
+                     data-y="10" 
+                     data-speed="800" 
+                     data-start="500" 
+                     data-easing="easeOutExpo"><img src="<?= Yii::app()->baseUrl;?>/images/howitworks/b4.jpg"></div>
+                                
+                <div class="caption lfl"  
+                     data-x="247" 
+                     data-y="267" 
+                     data-speed="800" 
+                     data-start="700" 
+                     data-easing="easeOutExpo"></div>
+                                
+                <div class="caption very_big_black lfr"  
+                     data-x="619" 
+                     data-y="130" 
+                     data-speed="300" 
+                     data-start="1000" 
+                     data-easing="easeOutExpo">Кастинг</div>
+    
+                <div class="caption medium_red lfr"  
+                     data-x="619" 
+                     data-y="238" 
+                     data-speed="300" 
+                     data-start="1200" 
+                     data-easing="easeOutExpo">В режиме онлайн</div>
+                                
+                <div class="caption medium_black lfb"  
+                     data-x="619" 
+                     data-y="337" 
+                     data-speed="300" 
+                     data-start="1400" 
+                     data-easing="easeOutExpo" style="background-color:#fff;">
+                        Это новый, удобный и современный формат проведения кастинга. <br>
+                        Если вы цените свое время, то онлайн кастинг станет для вас лучшим помощником. <br>
+                        Вы можете провести полноценный кастинг не выходя из дома, офиса или стоя в пробке. <br>
+                        Нужно заполнить заявку на проведение онлайн-кастинга и ввести все <br>
+                        необходимые сведения о проекте. Система автоматически оповестит всех <br>
+                        подходящих по параметрам пользователей, получит от каждого видеоролик, и <br>
+                        после этого предложит вам просмотреть и отобрать заявки. <br>
+                     </div>
+                </li>
+                <li data-transition="3dcurtain-vertical" data-slotamount="8" data-masterspeed="300" >
+                        <img src="_include/img/work/thumbs/image-02.jpg" >
+                                
+                <div class="caption lfl"  
+                     data-x="23" 
+                     data-y="90" 
+                     data-speed="800" 
+                     data-start="500" 
+                     data-easing="easeOutExpo"><img src="<?= Yii::app()->baseUrl;?>/images/howitworks/docs-example.png"></div>
+                                
+                <div class="caption lfl"  
+                     data-x="247" 
+                     data-y="267" 
+                     data-speed="800" 
+                     data-start="700" 
+                     data-easing="easeOutExpo"></div>
+                                
+                <div class="caption very_big_black lfr"  
+                     data-x="619" 
+                     data-y="130" 
+                     data-speed="300" 
+                     data-start="1000" 
+                     data-easing="easeOutExpo">Автоматизация</div>
+    
+                <div class="caption medium_red lfr"  
+                     data-x="619" 
+                     data-y="238" 
+                     data-speed="300" 
+                     data-start="1200" 
+                     data-easing="easeOutExpo">Документооборота</div>
+                                
+                <div class="caption medium_black lfb"  
+                     data-x="619" 
+                     data-y="337" 
+                     data-speed="300" 
+                     data-start="1400" 
+                     data-easing="easeOutExpo">
+                        Наша система на основе сформированного заказа система автоматически <br>
+                        генерирует весь пакет документов, включая договор, ведомости, <br>
+                        смету и даже фотовызывной, содержащий всю необходимую информацию <br>
+                        об актерах, утвержденных заказчиком. 
+                     </div>
+                </li>
+                        
+            </ul>
+            <div class="tp-bannertimer tp-bottom"></div>
+        </div>
+    </div>
+</section><!--End slider -->
+<script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/theme.js" type="text/javascript"></script>
