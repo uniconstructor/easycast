316c86d40b6ffd69620b70b32ab98faa7b2e8784
uniconstructor <php1602agregator@gmail.com>
2014-04-16 22:28:53 +0400

    Исправлена верстка страницы приглашения участников.

--- protected/modules/projects/views/invite/tokenInvite.php
+++ protected/modules/projects/views/invite/tokenInvite.php
@@ -8,8 +8,25 @@
 $this->breadcrumbs = array(
 	'Приглашение',
 );
-// виджет со списком приглашений
-$this->widget('application.modules.projects.extensions.TokenInvite.TokenInvite', array(
-    'key'    => $key,
-    'invite' => $invite
-));
+?>
+<div class="page-alternate">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <div class="title-page">
+                    <h1 class="title">Приглашение</h1>
+                    <h4 class="intro-description">Если ролей несколько - подать заявку можно на каждую.</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row-fluid">
+            <?php 
+            // виджет со списком приглашений
+            $this->widget('application.modules.projects.extensions.TokenInvite.TokenInvite', array(
+                'key'    => $key,
+                'invite' => $invite
+            ));
+            ?>
+        </div>
+    </div>
+</div>
