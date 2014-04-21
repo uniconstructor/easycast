ecaddcc8e73810650d344ef0e2474f70def4caee
uniconstructor <php1602agregator@gmail.com>
2014-04-21 18:25:36 +0400

    Стиль кодирования, комментарии

--- protected/modules/admin/views/projectMember/index.php
+++ protected/modules/admin/views/projectMember/index.php
@@ -13,36 +13,36 @@ if ( $type == 'applications' )
 }
 if ( $projectid )
 {
-    $project = Project::model()->findByPk($projectid);
+    $project     = Project::model()->findByPk($projectid);
     $breadcrumbs = array(
-        'Администрирование' =>array('/admin'),
-        'Проекты'=>array('/admin/project'),
-        $project->name=>array('/admin/project/view','id'=>$project->id),
+        'Администрирование' => array('/admin'),
+        'Проекты'           => array('/admin/project'),
+        $project->name      => array('/admin/project/view','id'=>$project->id),
     );
     $viewMode = 'project';
     $objectid = $projectid;
 }
 if ( $eventid )
 {
-    $event = ProjectEvent::model()->findByPk($eventid);
+    $event       = ProjectEvent::model()->findByPk($eventid);
     $breadcrumbs = array(
-        'Администрирование' =>array('/admin'),
-        'Проекты'=>array('/admin/project'),
-        $event->project->name=>array('/admin/project/view','id'=>$event->project->id),
-        $event->name=>array('/admin/projectEvent/view','id'=>$event->id),
+        'Администрирование'   => array('/admin'),
+        'Проекты'             => array('/admin/project'),
+        $event->project->name => array('/admin/project/view', 'id' => $event->project->id),
+        $event->name          => array('/admin/projectEvent/view','id' => $event->id),
     );
     $viewMode = 'event';
     $objectid = $eventid;
 }
 if ( $vacancyid )
 {
-    $vacancy = EventVacancy::model()->findByPk($vacancyid);
+    $vacancy     = EventVacancy::model()->findByPk($vacancyid);
     $breadcrumbs = array(
-        'Администрирование' =>array('/admin'),
-        'Проекты'=>array('/admin/project'),
-        $vacancy->event->project->name=>array('/admin/project/view','id'=>$vacancy->event->project->id),
-        $vacancy->event->name=>array('/admin/projectEvent/view','id'=>$vacancy->event->id),
-        $vacancy->name=>array('/admin/eventVacancy/view','id'=>$vacancy->id),
+        'Администрирование'            => array('/admin'),
+        'Проекты'                      => array('/admin/project'),
+        $vacancy->event->project->name => array('/admin/project/view', 'id' => $vacancy->event->project->id),
+        $vacancy->event->name => array('/admin/projectEvent/view','id' => $vacancy->event->id),
+        $vacancy->name => array('/admin/eventVacancy/view', 'id' => $vacancy->id),
     );
     $viewMode = 'vacancy';
     $objectid = $vacancyid;
@@ -56,12 +56,10 @@ $this->breadcrumbs=$breadcrumbs;
 <h1><?= $titleString ?></h1>
 
 <?php
-
+// отображаем список участников события
 $this->widget('admin.extensions.ProjectMembers.ProjectMembers',array(
-	'objectType'  => $viewMode,
-	'objectId'    => $objectid,
-    'displayType' => $type,
+	'objectType'      => $viewMode,
+	'objectId'        => $objectid,
+    'displayType'     => $type,
     'displayFullInfo' => true,
 )); 
-
-?>
