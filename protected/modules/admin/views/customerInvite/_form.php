52091c333ed75c94c7cdbe28b9accbc4566605cf
uniconstructor <php1602agregator@gmail.com>
2014-04-21 18:26:26 +0400

    В форму отправки одноразовой ссылки добавлены галочки для статусов заявок.

--- protected/modules/admin/views/customerInvite/_form.php
+++ protected/modules/admin/views/customerInvite/_form.php
@@ -3,45 +3,59 @@
  * Форма создания/редактирования приглашения для заказчика
  * @todo сделать 3 кнопки "создать", "сохранить" и "отправить"
  */
+/* @var $form TbActiveForm */
+/* @var $model CustomerInvite */
 
 $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 	'id' => 'customer-invite-form',
 	'enableAjaxValidation' => true,
 ));
-
-
 ?>
-	<?php echo $form->errorSummary($model); ?>
-	<?php echo $form->textFieldRow($model, 'email', array('class'=>'span5', 'maxlength' => 255)); ?>
-	<?php echo $form->textFieldRow($model, 'name',  array('class'=>'span5', 'maxlength' => 255)); ?>
-	
-	<?php // Наш комментарий для заказчика
-	echo $form->labelEx($model, 'comment'); 
-    $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
-    	'model' => $model,
-    	'attribute' => 'comment',
-    	'options' => array(
-    		'lang' => 'ru',
-            ),
-    ));
-    echo $form->error($model, 'comment');
-    ?>
-    <div class="alert alert-info">
-        Этот комментарий добавится в письмо с приглашением, в самый конец. Используйте это поле
-        чтобы сообщить заказчику дополнительную информацию.
-    </div>
-
-	<div class="form-actions">
-		<?php
-		if ( $model->isNewRecord )
-		{
-		    $this->widget('bootstrap.widgets.TbButton', array(
-		        'buttonType' => 'submit',
-		        'type'  => 'primary',
-		        'label' => 'Отправить',
-		    ));
-		} 
+<div class="page-alternate">
+    <div class="container">
+        <?php
+        echo $form->errorSummary($model);
+        echo $form->textFieldRow($model, 'email', array('class'=>'span5', 'maxlength' => 255));
+        echo $form->textFieldRow($model, 'name',  array('class'=>'span5', 'maxlength' => 255));
+        // Наш комментарий для заказчика
+    	echo $form->labelEx($model, 'comment'); 
+        $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
+        	'model'     => $model,
+        	'attribute' => 'comment',
+        	'options'   => array('lang' => 'ru'),
+        ));
+        echo $form->error($model, 'comment');
         ?>
-	</div>
-
-<?php $this->endWidget(); ?>
+        <div class="alert alert-info">
+            Этот комментарий добавится в письмо с приглашением, в самый конец. Используйте это поле
+            чтобы сообщить заказчику дополнительную информацию.
+        </div>
+        <?php 
+        // галочки для выбора статусов заявок, которые будут включены в фотовызывной
+        echo CHtml::label('Статусы заявок', 'lang_0');
+        echo CHtml::checkBoxList('statuses', array('draft', 'pending'), array(
+                ProjectMember::STATUS_DRAFT   => 'На рассмотрении',
+                ProjectMember::STATUS_PENDING => 'Предварительно одобренные',
+                ProjectMember::STATUS_ACTIVE  => 'Утвержденные',
+            ),
+            array(
+                'labelOptions' => array('style' => 'display:inline;'),
+            )
+        );
+        ?>
+    	<div class="form-actions">
+    		<?php
+    		if ( $model->isNewRecord )
+    		{
+    		    $this->widget('bootstrap.widgets.TbButton', array(
+    		        'buttonType' => 'submit',
+    		        'type'       => 'primary',
+                    'size'       => 'large',
+    		        'label'      => 'Отправить',
+    		    ));
+    		} 
+            ?>
+    	</div>
+    <?php $this->endWidget(); ?>
+    </div>
+</div>
