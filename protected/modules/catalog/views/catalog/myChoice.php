b22dacd146598c3712538e460ff03ca4470ef606
uniconstructor <php1602agregator@gmail.com>
2014-04-28 16:08:09 +0400

    Исправлена форма на странице "Мой выбор".

--- protected/modules/catalog/views/catalog/myChoice.php
+++ protected/modules/catalog/views/catalog/myChoice.php
@@ -1,92 +1,115 @@
 <?php
 /**
- * отображение страницы "мой выбор"
- * @var TbActiveForm $form
+ * Разметка страницы "мой выбор"
+ * @todo подключить проверку телефонного момера через LPNValidator
  */
+/* @var $form  TbActiveForm */
+/* @var $order FastOrder */
+/* @var $this  CatalogController */
+
 $this->breadcrumbs = array(
     "Мой выбор",
 );
 
 ?>
-
-<h1>Мой выбор</h1>
-
-<div class="alert alert-block">На этой странице вы можете еще раз просмотреть всю информацию о 
-выбранных вами участниках и оформить заказ.
-</div>
-
-<?php
-// Выводим сообщения или предупреждения
-$this->widget('bootstrap.widgets.TbAlert', array(
-    'block' => true, // display a larger alert block?
-    'fade'  => false, // use transitions?
-    'closeText' => false, // close link text - if set to false, no close link is displayed
-));
-
-
-// @todo разместить по центру дополнительные комментарии и кнопку оформления заказа
-if ( ! FastOrder::orderIsEmpty() )
-{// В заказе есть хотя бы один актер
-
-    // Выводим всех приглашенных заказчиком актеров одним списком
-    $this->widget('application.modules.catalog.extensions.MyChoice.MyChoice', array(
-        'questionaries' => $questionaries,
-    ));
-    
-    // добавляем форму с дополнительными комментариями к заказу и кнопкой "подтвердить"
-    $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
-        'id' => 'order-form',
-        'enableAjaxValidation' => false,
-        'action' => '/site/placeOrder',
-    ));
-    ?>
-    <div class="span2">&nbsp;</div>
-    <div class="span6">
-        <h2>Ваши контактные данные</h2>
-        <div class="alert alert-block">
-            Пожалуйста заполните форму, для того чтобы мы могли связаться с вами и подтвердить заказ.<br> 
-            Как только ваш заказ будет подтвержден мы свяжемся с каждым участником и сообщим ему всю необходимую информацию.
+<div class="page-alternate">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <div class="title-page">
+                    <h1 class="title">Мой выбор</h1>
+                    <h3 class="title-description">
+                        На этой странице вы можете еще раз просмотреть всю информацию о 
+                        выбранных вами участниках и оформить заказ.
+                    </h3>
+                </div>
+            </div>
         </div>
-        <?php 
-            echo Yii::t('coreMessages','form_required_fields', array('{mark}' => '<span class="required">*</span>'));
-            // ФИО
-            echo $form->textFieldRow($order, 'name');
-        ?>
-        <?php 
-            // телефон
-            echo $form->textFieldRow($order, 'phone');
-        ?>
-        <?php 
-            // email
-            echo $form->labelEx($order,'email');
-            echo $form->emailField($order, 'email');
-            echo $form->error($order,'email');
-        ?>
-        <?php 
-            // поле с дополнительной информацией о заказе
-            echo $form->textAreaRow($order, 'comment', array('style' => 'width:100%;height:100px;'));
-        ?>
-        <small>Мы обязуемся использовать ваш телефон и email только для связи с вами и уточнения деталей заказа.
-    	Мы никогда не будем присылать вам рекламу или раскрывать ваши данные третьим лицам.</small>
-        <div>
-        <?php 
-        // галочка, которая ни на что не влияет :)
-        //echo '<div style="margin-bottom:15px;">';
-        //echo CHtml::checkBox('cool', false, array('title' => 'Эта галочка ни на что не влияет'));
-        //echo '<small style="margin-top:10px;" title="Эта галочка ни на что не влияет">&nbsp;&nbsp;Здорово, я согласен!</small><br></div>';
-        
-        $this->widget('bootstrap.widgets.TbButton',
-            array(
-                    'buttonType'  => 'submit',
-                    'type'        => 'success',
-                    'label'       => 'Оформить заказ',
-                    'htmlOptions' => array('id'=>'place_order', 'class' => 'btn btn-large btn-success',),
-                )); 
-        ?>
-    	</div>
+        <div class="row">
+            <?php
+            // Выводим сообщения или предупреждения
+            $this->widget('bootstrap.widgets.TbAlert');
+            if ( ! FastOrder::orderIsEmpty() )
+            {// В заказе есть хотя бы один актер
+                // Выводим всех приглашенных заказчиком актеров одним списком
+                $this->widget('application.modules.catalog.extensions.MyChoice.MyChoice', array(
+                    'questionaries' => $questionaries,
+                ));
+                
+                // добавляем форму с дополнительными комментариями к заказу и кнопкой "подтвердить"
+                $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+                    'id'                   => 'order-form',
+                    'enableAjaxValidation' => false,
+                    'action'               => '/site/placeOrder',
+                ));
+                ?>
+                <div class="title-page">
+                    <h4 class="title">Ваши контактные данные</h4>
+                    <h4 class="title-description">
+                        Пожалуйста заполните форму, для того чтобы мы могли связаться с вами и подтвердить заказ.<br> 
+                        Как только ваш заказ будет подтвержден мы свяжемся с каждым участником и сообщим ему всю необходимую информацию.
+                    </h4>
+                </div>
+                <div class="span3">&nbsp;</div>
+                <div class="span6">
+                    <?php 
+                    echo Yii::t('coreMessages','form_required_fields', array('{mark}' => '<span class="required">*</span>'));
+                    // ФИО
+                    echo $form->textFieldRow($order, 'name',
+                        array(
+                            'placeholder' => 'Имя'
+                        ),
+                        array(
+                            'prepend'     => '<i class="icon icon-user"></i>',
+                        )
+                    );
+                    // телефон
+                    echo $form->textFieldRow($order, 'phone', 
+                        array(
+                            'placeholder' => '(987)654-32-10'
+                        ), 
+                        array(
+                            'hint'        => '<small>В номере должно быть 10 цифр. Мы обязуемся использовать ваш 
+                                телефон и email только для связи с вами и уточнения деталей заказа.
+                	            Мы никогда не будем присылать вам рекламу или раскрывать ваши данные 
+                                третьим лицам.</small>',
+                            'prepend'     => '+7',
+                        )
+                    );
+                    // email
+                    echo $form->textFieldRow($order, 'email', array(
+                            'size'        => 60,
+                            'maxlength'   => 255,
+                            'placeholder' => 'mail@example.com',
+                        ),
+                        array(
+                            'prepend'     => '<i class="icon icon-envelope"></i>',
+                        )
+                    );
+                    // поле с дополнительной информацией о заказе
+                    echo $form->textAreaRow($order, 'comment', array('style' => 'width:100%;height:100px;'));
+                    ?>
+                    <div>
+                    <?php 
+                    // и главная кнопка :)
+                    $form->widget('bootstrap.widgets.TbButton', array(
+                        'buttonType'  => 'submit',
+                        'type'        => 'success',
+                        'label'       => 'Оформить заказ',
+                        'htmlOptions' => array(
+                            'id'    => 'place_order',
+                            'class' => 'btn btn-large btn-success',
+                        ),
+                    )); 
+                    ?>
+                	</div>
+                </div>
+                <div class="span3">&nbsp;</div>
+                <?php 
+                // конец формы
+                $this->endWidget(); 
+            }// конец условия "в заказе есть хотя бы один актер"
+            ?>
+        </div>
     </div>
-    <div class="span2">&nbsp;</div>
-    <?php 
-    // конец формы
-    $this->endWidget(); 
-}// конец условия "в заказе есть хотя бы один актер"
+</div>
