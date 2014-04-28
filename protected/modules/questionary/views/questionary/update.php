40bcd4d2a9ed055011c68f459a4bfc4e85414f3d
uniconstructor <php1602agregator@gmail.com>
2014-04-28 19:15:45 +0400

    Страница анкеты адаптирована под новую верстку.

--- protected/modules/questionary/views/questionary/update.php
+++ protected/modules/questionary/views/questionary/update.php
@@ -18,34 +18,36 @@ $this->menu=array(
 ?>
 
 <h1><?php echo QuestionaryModule::t('update_questionary'); ?></h1>
-
-<?php echo $this->renderPartial('_form', array(
-          'questionary'         => $questionary,
-          'address'             => $address,
-          'user'                => $user,
-          'recordingConditions' => $recordingConditions,
-
-          // сложные значения
-          // Телеведущий
-          'tvShow'           => $tvShow,
-          'validatedTvShows' => $validatedTvShows,
-          // Работа фотомоделью
-          'photoModelJob'           => $photoModelJob,
-          'validatedPhotoModelJobs' => $validatedPhotoModelJobs,
-          // Работа промо-моделью
-          'promoModelJob'           => $promoModelJob,
-          'validatedPromoModelJobs' => $validatedPromoModelJobs,
-          // Стили танца
-          'danceType'           => $danceType,
-          'validatedDanceTypes' => $validatedDanceTypes,
-          // Музыкальные инструменты
-          'instrument'           => $instrument,
-          'validatedInstruments' => $validatedInstruments,
-          // Звания, призы и награды
-          'award'           => $award,
-          'validatedAwards' => $validatedAwards,
-          // Работа в театре
-          'actorTheatre'           => $actorTheatre,
-          'validatedActorTheatres' => $validatedActorTheatres,
-     ));
-?>
+<div class="container">
+    <?php 
+    echo $this->renderPartial('_form', array(
+        'questionary'         => $questionary,
+        'address'             => $address,
+        'user'                => $user,
+        'recordingConditions' => $recordingConditions,
+        
+        // сложные значения
+        // Телеведущий
+        'tvShow'           => $tvShow,
+        'validatedTvShows' => $validatedTvShows,
+        // Работа фотомоделью
+        'photoModelJob'           => $photoModelJob,
+        'validatedPhotoModelJobs' => $validatedPhotoModelJobs,
+        // Работа промо-моделью
+        'promoModelJob'           => $promoModelJob,
+        'validatedPromoModelJobs' => $validatedPromoModelJobs,
+        // Стили танца
+        'danceType'           => $danceType,
+        'validatedDanceTypes' => $validatedDanceTypes,
+        // Музыкальные инструменты
+        'instrument'           => $instrument,
+        'validatedInstruments' => $validatedInstruments,
+        // Звания, призы и награды
+        'award'           => $award,
+        'validatedAwards' => $validatedAwards,
+        // Работа в театре
+        'actorTheatre'           => $actorTheatre,
+        'validatedActorTheatres' => $validatedActorTheatres,
+    ));
+    ?>
+</div>
