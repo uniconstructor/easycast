f39be9705fafe85992d3b75047188d8c46eed676
uniconstructor <php1602agregator@gmail.com>
2014-05-15 17:21:34 +0400

    Из КП убран онлайн-чат, слияние веток dynamicForm и Master

--- themes/staticLanding/views/layouts/main.php
+++ themes/staticLanding/views/layouts/main.php
@@ -26,30 +26,5 @@
         ?>
         </div>
     </div>
-<!--Start of Zopim Live Chat Script-->
-<script type="text/javascript">
-window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-$.src='//v2.zopim.com/?1jwtx0mz7NF3bfar8I0vxNEn7iLRzlDC';z.t=+new Date;$.
-type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-<?php 
-// Подставляем имя и email заказчика в онлайн-чат, если он зашел по ссылке из письма
-if ( $offer = Yii::app()->session->get('activeOffer') )
-{/* @var $offer CustomerOffer */
-    echo "\$zopim(function() {";
-    if ( $offer->email )
-    {
-        echo "\$zopim.livechat.setEmail('{$offer->email}');";
-    }
-    if ( $offer->name )
-    {
-        echo "\$zopim.livechat.setName('{$offer->name}');";
-    }
-    echo "});";
-}
-?>
-</script>
-<!--End of Zopim Live Chat Script-->
 </body>
 </html> 
