87e73ad1b44d7fad6389343c5b4143c80fe9c8f4
uniconstructor <php1602agregator@gmail.com>
2014-05-18 03:44:55 +0400

    Добавлены миграции: 1) Для работы в театре и работы телеведущим добавлен пункт "по настоящее время" 2) Добавлены недостающие поля "время создания и изменения". Стандартизирован способ хранения данных в таких полях.

---
+++ protected/modules/questionary/migrations/m141518_001800_fixTimestamps.php
@@ -0,0 +1,50 @@
+<?php
+
+class m141518_001800_fixTimestamps extends CDbMigration
+{
+    /**
+     * @see CDbMigration::safeUp()
+     */
+    public function safeUp()
+    {
+        $table = '{{q_activities}}';
+        $this->addColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timemodified', $table, 'timemodified');
+        $this->alterColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        
+        $table = '{{q_activity_types}}';
+        $this->addColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timecreated', $table, 'timecreated');
+        $this->addColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timemodified', $table, 'timemodified');
+        
+        $table = '{{q_awards}}';
+        $this->alterColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->alterColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        
+        $table = '{{q_films}}';
+        $this->alterColumn($table, 'date', 'int(11) UNSIGNED DEFAULT NULL');
+        
+        $table = '{{q_film_instances}}';
+        $this->alterColumn($table, 'date', 'int(11) UNSIGNED DEFAULT NULL');
+        
+        $table = '{{q_recording_conditions}}';
+        $this->addColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timecreated', $table, 'timecreated');
+        $this->addColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timemodified', $table, 'timemodified');
+        
+        $table = '{{q_theatre_instances}}';
+        $this->alterColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->alterColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        
+        $table = '{{q_tvshow_instances}}';
+        $this->alterColumn($table, 'timestart', 'int(11) UNSIGNED DEFAULT NULL');
+        $this->alterColumn($table, 'timeend', 'int(11) UNSIGNED DEFAULT NULL');
+        
+        $table = '{{q_university_instances}}';
+        $this->addColumn($table, 'timemodified', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+        $this->createIndex('idx_timemodified', $table, 'timemodified');
+        $this->alterColumn($table, 'timecreated', 'int(11) UNSIGNED NOT NULL DEFAULT 0');
+    }
+}
