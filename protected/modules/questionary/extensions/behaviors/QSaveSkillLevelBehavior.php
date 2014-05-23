316436093e34a45bf5c573965a8d3f242457f5a3
uniconstructor <php1602agregator@gmail.com>
2014-05-24 02:49:53 +0400

    Исправлена ошибка отображения уровня вледения умением/навыком

--- protected/modules/questionary/extensions/behaviors/QSaveSkillLevelBehavior.php
+++ protected/modules/questionary/extensions/behaviors/QSaveSkillLevelBehavior.php
@@ -8,9 +8,8 @@
 class QSaveSkillLevelBehavior extends CActiveRecordBehavior
 {
     /**
-     * Получить уровня владения навыком (любитель/профессионал)
+     * Получить возможные уровни владения навыком (любитель/профессионал)
      * Используется во всех выпадающих меню, где нужно указать уровень владения чем-либо
-     *
      * @return array
      */
     public function levelList()
@@ -22,25 +21,24 @@ class QSaveSkillLevelBehavior extends CActiveRecordBehavior
     }
     
     /**
-     * Получить уровень владения для отображения пользователю 
+     * Получить уровень владения для отображения пользователю (используется текущий язык сайта)
+     * @return void
      */
     public function getSkilllevel()
     {
-        if ( $this->owner->scenario == 'view' )
-        {
-            return $this->owner->getDefaultValueForDisplay('level', $this->owner->level);
-        }
-        
-        return $this->owner->level;
+        return $this->owner->getDefaultValueForDisplay('level', $this->owner->level);
     }
     
+    /**
+     * Получить уровень текущий владения для навыка 
+     * @return void
+     */
     public function getLevel()
     {
-        if ( $this->owner->scenario == 'view' )
+        if ( $this->owner->scenario === 'view' )
         {
             return $this->owner->getDefaultValueForDisplay('level', $this->owner->level);
         }
-        
         return $this->owner->level;
     }
 }
