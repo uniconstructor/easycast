0a4eff1b810741ccd77df607f13a41133399dfa0
uniconstructor <php1602agregator@gmail.com>
2014-05-26 08:04:07 +0400

    Исправлен поиск по имени и фамилии

--- protected/modules/catalog/extensions/search/handlers/QSearchHandlerName.php
+++ protected/modules/catalog/extensions/search/handlers/QSearchHandlerName.php
@@ -17,21 +17,19 @@ class QSearchHandlerName extends QSearchHandlerBase
         $name = $data['name'];
         
         if ( mb_ereg(' ', $name) )
-        {// Если в поле введены и имя и фамилия - то
-            // разбиваем строку по пробелам для того чтобы отделить имя от фамилии при дальнейшем поиске
+        {// Если в поле введены и имя и фамилия -
+            // то разбиваем строку по пробелам для того чтобы отделить имя от фамилии при дальнейшем поиске
             $parts = explode(' ', $name);
             
             // Поскольку мы не знаем, что введено сначала: имя или фамилия - то будем искать по всем полям
             $criteria->addInCondition('firstname', $parts);
-            $criteria->addInCondition('lastname', $parts);
+            $criteria->addInCondition('lastname', $parts, 'OR');
         }else
-       {// в поле введено одно слово
+        {// в поле введено одно слово
            // Поскольку мы не знаем, что введено сначала: имя или фамилия - то будем искать по всем полям
            $criteria->compare('firstname', $name);
            $criteria->compare('lastname', $name, false, 'OR');
         }
-        
-        
         return $criteria;
     }
 }
