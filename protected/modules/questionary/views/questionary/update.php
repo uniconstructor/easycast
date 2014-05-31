3cc93e05bb1d9af2df1b7d9f83eea28485210e92
uniconstructor <php1602agregator@gmail.com>
2014-05-31 11:32:47 +0400

    Убран неиспользуемый код

--- protected/modules/questionary/views/questionary/update.php
+++ protected/modules/questionary/views/questionary/update.php
@@ -9,7 +9,7 @@
 $this->breadcrumbs = array(
     CatalogModule::t('catalog') => array('/catalog'),
 	$user->fullname  => array(Yii::app()->getModule('questionary')->profileUrl, 'id' => $questionary->id),
-	Yii::t('coreMessages','edit'),
+	Yii::t('coreMessages', 'edit'),
 );
 
 ?>
@@ -22,29 +22,6 @@ $this->breadcrumbs = array(
         'address'             => $address,
         'user'                => $user,
         'recordingConditions' => $recordingConditions,
-        
-        // сложные значения
-        // Телеведущий
-        'tvShow'           => $tvShow,
-        'validatedTvShows' => $validatedTvShows,
-        // Работа фотомоделью
-        'photoModelJob'           => $photoModelJob,
-        'validatedPhotoModelJobs' => $validatedPhotoModelJobs,
-        // Работа промо-моделью
-        'promoModelJob'           => $promoModelJob,
-        'validatedPromoModelJobs' => $validatedPromoModelJobs,
-        // Стили танца
-        'danceType'           => $danceType,
-        'validatedDanceTypes' => $validatedDanceTypes,
-        // Музыкальные инструменты
-        'instrument'           => $instrument,
-        'validatedInstruments' => $validatedInstruments,
-        // Звания, призы и награды
-        'award'           => $award,
-        'validatedAwards' => $validatedAwards,
-        // Работа в театре
-        'actorTheatre'           => $actorTheatre,
-        'validatedActorTheatres' => $validatedActorTheatres,
     ));
     ?>
 </div>
