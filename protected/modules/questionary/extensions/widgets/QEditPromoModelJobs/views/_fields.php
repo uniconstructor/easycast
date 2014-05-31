04243f001c14622f893dfcebad9600c2717ad1fc
uniconstructor <php1602agregator@gmail.com>
2014-05-31 11:20:39 +0400

    Стандартизирован ввод поля name, переименованы поля формы

--- protected/modules/questionary/extensions/widgets/QEditPromoModelJobs/views/_fields.php
+++ protected/modules/questionary/extensions/widgets/QEditPromoModelJobs/views/_fields.php
@@ -10,7 +10,7 @@
 // ошибки формы
 echo $form->errorSummary(array($model), null, null, array('id' => $this->formId.'_errors'));
 // название
-echo $form->textFieldRow($model, 'job');
+echo $form->textFieldRow($model, 'name');
 // год
 echo $form->datepickerRow($model, 'year', array(
     'options' => $this->getYearPickerOptions(),
