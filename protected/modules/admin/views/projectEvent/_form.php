1d5bba205fac11c6e95e6049694ae54f5f57882c
uniconstructor <php1602agregator@gmail.com>
2014-06-02 05:40:43 +0400

    Исправлена работа элементов "копировать/вставить" в текстовых полях ввода

--- protected/modules/admin/views/projectEvent/_form.php
+++ protected/modules/admin/views/projectEvent/_form.php
@@ -1,29 +1,31 @@
 <?php 
 /**
  * Форма редактирования мероприятия в админке
+ * 
+ * @todo указывать адрес вместо текстового поля "место проведения"
  */
+/* @var $this  ProjectEventController */
+/* @var $model ProjectEvent */
+/* @var $form  TbActiveForm */
 
 $form = $this->beginWidget('bootstrap.widgets.TbActiveForm',array(
 	'id' => 'project-event-form',
 	'enableAjaxValidation' => false,
 )); 
-
 $dateFormatter = new CDateFormatter('ru');
 ?>
-
-	<?php echo Yii::t('coreMessages','form_required_fields', array('{mark}' => '<span class="required">*</span>')); ?>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<?php echo $form->textFieldRow($model, 'name',array('class'=>'span5', 'maxlength'=>255)); ?>
+	<?php 
+	// ошибки формы
+	echo Yii::t('coreMessages','form_required_fields', array('{mark}' => '<span class="required">*</span>'));
+	echo $form->errorSummary($model);
 	
-	<?php
+	// название события
+	echo $form->textFieldRow($model, 'name', array('class' => 'span5', 'maxlength' => 255)); 
+	
 	// тип мероприятия
 	$eventTypes = $model->getTypes();
 	echo $form->dropDownListRow($model, 'type', $eventTypes);
-	?>
 	
-	<?php 
 	// группа мероприятия
 	$groups = $model->getOpenGroups($project->id);
 	if ( $model->isNewRecord AND $groupid )
@@ -34,123 +36,92 @@ $dateFormatter = new CDateFormatter('ru');
 	{// нельзя перемещать активные мероприятия между группами
 	    echo $form->dropDownListRow($model, 'parentid', $groups, array('disabled' => 'disabled'));
 	}else
-	{
+	{// черновики можно
 	    echo $form->dropDownListRow($model, 'parentid', $groups);
 	}
-	?>
-
-	<?php // описание мероприятия
-	echo $form->labelEx($model, 'description'); 
-        $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
-    	'model' => $model,
-    	'attribute' => 'description',
-    	'options' => array(
-    		'lang'    => 'ru',
-            ),
-        ));
-    echo $form->error($model,'description');
-    echo '<div class="alert">Видно всем</div>';
-    ?>
+	
+	// описание мероприятия
+	echo $form->redactorRow($model, 'description', array(), array(
+        'hint' => 'Видно всем',
+    ));
+    //echo '<div class="alert">Видно всем</div>';
     
-    <?php // Дополнительная информация для участников
-	echo $form->labelEx($model,'memberinfo'); 
-        $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
-    	'model' => $model,
-    	'attribute' => 'memberinfo',
-    	'options' => array(
-    		'lang'    => 'ru',
-            ),
-        ));
-    echo $form->error($model,'memberinfo');
-    echo '<div class="alert">Отображается только подтвержденным участникам</div>';
-    ?>
+    // Дополнительная информация для участников
+    echo $form->redactorRow($model, 'memberinfo', array(), array(
+        'hint' => 'Отображается только подтвержденным участникам',
+    ));
+    //echo '<div class="alert">Отображается только подтвержденным участникам</div>';
     
-    <?php // нужно создать мероприятие без даты (она пока неизвестна)
+    // нужно создать мероприятие без даты (она пока неизвестна)
     // @todo выключать даты начала и окончания при установке этой галочки
     echo $form->checkBoxRow($model, 'nodates');
-    ?>
-    
-	<?php // время начала
-        // @todo заменить даты начала и окончания на более удобный виджет (на вариант с диапазоном): 
-        //       http://jonthornton.github.io/jquery-timepicker/
-	    echo $form->labelEx($model, 'timestart');
-	    $defaultStart = time();
-	    if ( $model->timestart )
-	    {
-	        $defaultStart = $model->timestart;
-	    }
-	    $this->widget('ext.CJuiDateTimePicker.CJuiDateTimePicker',array(
-	        //'model'=>$model,
-            //'flat' => true,
-	        //'attribute'=>'timestart',
-            'name' => 'ProjectEvent[timestart]',
-            'value' => $dateFormatter->format('dd.MM.yyyy HH:mm', $defaultStart),
-	        'options'=>array(
-	            'showAnim'=>'fold',
-	        ),
-	    ));
-        echo $form->error($model,'timestart');
-	?>
 
-	<?php // время окончания
-	    echo $form->labelEx($model, 'timeend');
-	    $defaultEnd = time();
-	    if ( $model->timeend )
-	    {
-	        $defaultEnd = $model->timeend;
-	    }
-	    $this->widget('ext.CJuiDateTimePicker.CJuiDateTimePicker',array(
-	        //'model'=>$model,
-            //'flat' => true,
-	        //'attribute'=>'timeend',
-            'name' => 'ProjectEvent[timeend]',
-            'value' => $dateFormatter->format('dd.MM.yyyy HH:mm', $defaultEnd),
-	        'options'=>array(
-	            'showAnim'=>'fold',
-	        ),
-	    ));
-        echo $form->error($model,'timeend');
-	?>
-	
-	<?php // время сбора
-	    // @todo заменить этот виджет на более удобный: http://amsul.ca/pickadate.js/time.htm#formats
-	    echo $form->labelEx($model, 'eta');
-	    $this->widget('ext.ETinyTimePicker.ETinyTimePicker', array(
-	        'model'     => $model,
-	        'attribute' => 'eta',
-	    ));
-        echo $form->error($model,'eta');
-	?>
-	
-	<?php // Описание места встречи
-	echo $form->labelEx($model,'meetingplace'); 
-        $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
-    	'model' => $model,
-    	'attribute' => 'meetingplace',
-    	'options' => array(
-    		'lang'    => 'ru',
-            ),
-        ));
-    echo $form->error($model, 'meetingplace');
-    echo '<div class="alert">Отображается только подтвержденным участникам</div>';
-    ?>
-	
-	<?php // показывать ли время начала съемок?
-	    $showTimeStartOptions = array();
-	    if ( $model->isNewRecord )
-	    {
-	        $showTimeStartOptions['checked'] = false;
-	    }
-	    echo $form->checkBoxRow($model, 'showtimestart');
-	    echo '<div class="alert">Если галочка поставлена - участникам покажется и время встречи, и время съемок.
+    // время начала
+    // @todo заменить даты начала и окончания на более удобный виджет (на вариант с диапазоном): 
+    //       http://jonthornton.github.io/jquery-timepicker/
+    echo $form->labelEx($model, 'timestart');
+    $defaultStart = time();
+    if ( $model->timestart )
+    {
+        $defaultStart = $model->timestart;
+    }
+    $this->widget('ext.CJuiDateTimePicker.CJuiDateTimePicker',array(
+        //'model'=>$model,
+        //'flat' => true,
+        //'attribute'=>'timestart',
+        'name' => 'ProjectEvent[timestart]',
+        'value' => $dateFormatter->format('dd.MM.yyyy HH:mm', $defaultStart),
+        'options'=>array(
+            'showAnim'=>'fold',
+        ),
+    ));
+    echo $form->error($model,'timestart');
+
+	// время окончания
+    echo $form->labelEx($model, 'timeend');
+    $defaultEnd = time();
+    if ( $model->timeend )
+    {
+        $defaultEnd = $model->timeend;
+    }
+    $this->widget('ext.CJuiDateTimePicker.CJuiDateTimePicker',array(
+        //'model'=>$model,
+        //'flat' => true,
+        //'attribute'=>'timeend',
+        'name' => 'ProjectEvent[timeend]',
+        'value' => $dateFormatter->format('dd.MM.yyyy HH:mm', $defaultEnd),
+        'options'=>array(
+            'showAnim'=>'fold',
+        ),
+    ));
+    echo $form->error($model,'timeend');
+
+	// время сбора
+    // @todo заменить этот виджет на более удобный: http://amsul.ca/pickadate.js/time.htm#formats
+    echo $form->labelEx($model, 'eta');
+    $this->widget('ext.ETinyTimePicker.ETinyTimePicker', array(
+        'model'     => $model,
+        'attribute' => 'eta',
+    ));
+    echo $form->error($model,'eta');
+
+	// Описание места встречи
+	echo $form->redactorRow($model, 'meetingplace', array(), array(
+	    'hint' => 'Отображается только подтвержденным участникам',
+	));
+    //echo '<div class="alert">Отображается только подтвержденным участникам</div>';
+
+    // показывать ли время начала съемок?
+    $showTimeStartOptions = array();
+    if ( $model->isNewRecord )
+    {
+        $showTimeStartOptions['checked'] = false;
+    }
+    echo $form->checkBoxRow($model, 'showtimestart');
+    echo '<div class="alert">Если галочка поставлена - участникам покажется и время встречи, и время съемок.
                 Если снята - только время сбора.</div>';
-	?>
 	
-	<?php // @todo убрать размер оплаты
-	    //echo $form->textFieldRow($model, 'salary', array('class'=>'span5','maxlength'=>32));
-	?>
-	
-	<?php
+    // Фотографии
     echo '<div>Фотогалерея</div>';
     if ( $model->photoGalleryBehavior->getGallery() === null )
     {
@@ -162,13 +133,13 @@ $dateFormatter = new CDateFormatter('ru');
              'controllerRoute' => '/admin/gallery'
         ));
     }
-    ?>
-
-	<?php // echo $form->textFieldRow($model,'addressid',array('class'=>'span5','maxlength'=>11)); ?>
-	<?php $this->widget('bootstrap.widgets.TbButton', array(
+    
+    // кнопка сохранения данных
+	$this->widget('bootstrap.widgets.TbButton', array(
 		'buttonType'=>'submit',
 		'type'=>'primary',
 		'label'=>$model->isNewRecord ? 'Создать' : 'Сохранить',
-	)); ?>
+	));
+    ?>
 
 <?php $this->endWidget(); ?>
