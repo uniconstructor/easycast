3927acf959f439b54c8a60035ef4f76189ac6d71
uniconstructor <php1602agregator@gmail.com>
2014-06-02 04:30:01 +0400

    Добавлена настройка "использовать cron"

--- protected/config/dev.php
+++ protected/config/dev.php
@@ -15,14 +15,14 @@ return CMap::mergeArray(
                     'ext.simpleWorkflow.command.gii',
                 ),
             ),
-            'log' => array(
+            /*'log' => array(
                 'routes' => array(
                     array(
                         'class'     => 'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
                         'ipFilters' => array('127.0.0.1'),
                     ),
                 ),
-            ),
+            ),*/
             // бонус: мой маленький робот для торговли на бирже :)
             // Он вообще к сайту никак не относится, просто воткнуть его сюда было быстрее чем
             // создавать новый проект :)
@@ -78,8 +78,8 @@ return CMap::mergeArray(
             // Использовать хостинг amazon s3 для хранения картинок
             'useAmazonS3'   => false,
             'AWSBucket'     => 'test.easycast.ru',
-            'AWSBucketPath' => 'http://bglance',
-            //'AWSBucketPath' => 'http://test.easycast.ru.s3.amazonaws.com',
+            //'AWSBucketPath' => 'http://bglance',
+            'AWSBucketPath' => 'http://test.easycast.ru.s3.amazonaws.com',
             
             // SES
             // использовать сервис Amazon SES для отправки почты на реальные адреса
@@ -90,16 +90,20 @@ return CMap::mergeArray(
             
             // SQS
             // использовать Amazon SQS для отправки большого количества почты (через очередь)
-            'useAmazonSQS'      => true,
+            'useAmazonSQS'      => false,
             'AWSEmailQueueUrl'  => 'https://sqs.us-east-1.amazonaws.com/507109426938/test_easycast_mail',
             'AWSEmailQueueName' => 'test_easycast_mail',
             
             // Для отладки: отсылать ли вообще хоть какие-нибудь сообщения, даже на тестовые адреса?
-            'AWSSendMessages' => true,
+            'AWSSendMessages' => false,
             
             // использовать ли прокси сервера google для отображения картинок в письмах
             // (должно быть включено на production и выключено на машине разработчика)
             'useGoogleImageProxy' => false,
+            
+            // cron: на dev-системе сильно замедляет работу, поэтому отключен
+            // включается только для целей тестирования
+            'useCron' => false,
         ),
     )
 );
