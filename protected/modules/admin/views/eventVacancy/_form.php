ad669720e2baa64c953e6ca551fc2defe0251c40
uniconstructor <php1602agregator@gmail.com>
2014-06-03 22:49:13 +0400

    Переписан процесс создания роли, убран неиспользуемый код

--- protected/modules/admin/views/eventVacancy/_form.php
+++ protected/modules/admin/views/eventVacancy/_form.php
@@ -2,31 +2,27 @@
 /**
  * Форма создания вакансии (роли)
  */
-$form = $this->beginWidget('bootstrap.widgets.TbActiveForm',array(
-	'id'                   => 'event-vacancy-form',
-	'enableAjaxValidation' => false,
-)); 
-?>
-	<?php echo $form->errorSummary($model); ?>
-	<?php echo $form->textFieldRow($model, 'name', array('maxlength'=>255)); ?>
-	<?php // описание роли
-	echo $form->labelEx($model,'description'); 
-    $this->widget('ext.imperavi-redactor-widget.ImperaviRedactorWidget', array(
-    	'model'     => $model,
-    	'attribute' => 'description',
-    	'options'   => array('lang' => 'ru'),
-    ));
-    echo $form->error($model,'description');
-    ?>
-	<?php echo $form->textFieldRow($model, 'limit', array('maxlength'=>6)); ?>
-	<?php echo $form->textFieldRow($model, 'salary', array('maxlength'=>7)); ?>
-    <br>
-	<?php 
-	$this->widget('bootstrap.widgets.TbButton', array(
-		'buttonType' => 'submit',
-		'type'       => 'primary',
-		'label'      => $model->isNewRecord ? 'Создать' : 'Сохранить',
-	)); 
-	?>
 
-<?php $this->endWidget(); ?>
+$form = $this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+    'id' => 'event-vacancy-form',
+    'enableAjaxValidation' => false,
+));
+
+// название роли
+echo $form->textFieldRow($model, 'name', array('maxlength' => 255));
+// описание роли
+echo $form->redactorRow($model, 'description'); 
+// необходимое количество человек
+echo $form->textFieldRow($model, 'limit', array('maxlength' => 6));
+// оплата за день
+echo $form->textFieldRow($model, 'salary', array('maxlength' => 7));
+
+echo '<br>';
+$this->widget('bootstrap.widgets.TbButton', array(
+	'buttonType' => 'submit',
+	'type'       => 'primary',
+	'size'       => 'large',
+	'label'      => $model->isNewRecord ? 'Создать' : 'Сохранить',
+)); 
+
+$this->endWidget();
--- protected/modules/admin/views/eventVacancy/view.php
+++ protected/modules/admin/views/eventVacancy/view.php
@@ -1,8 +1,9 @@
 <?php
 /**
- * Страница просмотра вакансии
+ * Страница просмотра роли
  */
-/* @var $this EventVacancyController */
+/* @var $this  EventVacancyController */
+/* @var $model EventVacancy */
 
 $this->breadcrumbs = array(
 	'Администрирование'          => array('/admin'),
@@ -14,23 +15,28 @@ $this->breadcrumbs = array(
 
 $this->menu = array(
 	array(
-	    'label' => 'Страница мероприятия', 
-	    'url'   => array('/admin/projectEvent/view', 'id' => $model->event->id)),
+	    'label' => 'Страница события', 
+	    'url'   => array('/admin/projectEvent/view', 'id' => $model->event->id),
+    ),
 	array(
 	    'label' => 'Добавить роль',
-	    'url'   => array('/admin/eventVacancy/create', 'eventid' => $model->event->id)),
+	    'url'   => array('/admin/eventVacancy/create', 'eventid' => $model->event->id),
+    ),
 	array(
 	    'label' => 'Редактировать роль',
-	    'url'   => array('/admin/eventVacancy/update', 'id' => $model->id)),
+	    'url'   => array('/admin/eventVacancy/update', 'id' => $model->id),
+    ),
     array(
         'label' => 'Заявки',
-        'url'   => array('/admin/projectMember/index', 'vacancyid' => $model->id, 'type' => 'applications')),
+        'url'   => array('/admin/projectMember/index', 'vacancyid' => $model->id, 'type' => 'applications'),
+    ),
     array(
         'label' => 'Подтвержденные участники',
-        'url'   => array('/admin/projectMember/index', 'vacancyid' => $model->id, 'type' => 'members')),
+        'url'   => array('/admin/projectMember/index', 'vacancyid' => $model->id, 'type' => 'members'),
+    ),
 );
 
-if ( $model->status == EventVacancy::STATUS_DRAFT )
+if ( $model->status === EventVacancy::STATUS_DRAFT )
 {// разрешаем удалять вакансию-черновик
     $this->menu[] = array(
         'label'       => 'Удалить роль', 
@@ -44,11 +50,13 @@ if ( $model->status == EventVacancy::STATUS_DRAFT )
 }
 if ( in_array('active', $model->getAllowedStatuses()) )
 {// ссылка на активацию вакансии
-    $this->menu[] = array('label' => 'Опубликовать роль',
-        'url' => array('/admin/eventVacancy/setStatus', 'id' => $model->id, 'status' => 'active'),
+    $this->menu[] = array(
+        'label' => 'Опубликовать роль',
+        'url'   => array('/admin/eventVacancy/setStatus', 'id' => $model->id, 'status' => 'active'),
         'linkOptions' => array(
             'confirm' => 'Это действие оповестит всех подходящих участников о начале съемок.
-                ВНИМАНИЕ: после публикации роли редактировать критерии поиска будет нельзя. Опубликовать роль "'.$model->name.'"?',
+                ВНИМАНИЕ: после публикации роли редактировать критерии поиска будет нельзя.
+                Опубликовать роль "'.$model->name.'"?',
         ),
     );
 }
@@ -56,55 +64,82 @@ if ( in_array('finished', $model->getAllowedStatuses()) )
 {// Ссылка на закрытие вакансии
     $this->menu[] = array(
         'label' => 'Закрыть роль',
-        'url'   => array('/admin/eventVacancy/setStatus', 'id' => $model->id, 'status' => 'finished'),
+        'url'   => array('/admin/eventVacancy/setStatus',
+            'id'     => $model->id,
+            'status' => 'finished',
+        ),
         'linkOptions' => array(
             'confirm' => 'Закрыть роль "'.$model->name.'"?',
         ),
     );
 }
-if ( $model->status == EventVacancy::STATUS_ACTIVE )
+if ( $model->status === EventVacancy::STATUS_ACTIVE )
 {// предоставить доступ
-    $this->menu[] = array('label' => 'Предоставить доступ',
-        'url' => array('/admin/customerInvite/create', 'objectId' => $model->id, 'objectType' => 'vacancy'),
+    $this->menu[] = array(
+        'label' => 'Предоставить доступ',
+        'url'   => array('/admin/customerInvite/create',
+            'objectType' => 'vacancy',
+            'objectId'   => $model->id, 
+        ),
     );
 }
 
 // отображение оповещений о смене статуса
 $this->widget('bootstrap.widgets.TbAlert');
-
 ?>
 
-<div class="row">
-    <div class="span6">
-        <h1>Вакансия "<?php echo $model->name; ?>"</h1>
-        <?php
-        // описание самой вакансии
-        $this->widget('bootstrap.widgets.TbDetailView', array(
-        	'data'       => $model,
-        	'attributes' => array(
-        		'name',
-        		'description:html',
-        		array(
-                    'label' => 'Заполнение',
-                    'value' => '('.$model->membersCount.' из '.$model->limit.') [Заявки: '.$model->requestsCount.']',
-                ),
-        		array(
-                    'label' => ProjectsModule::t('status'),
-                    'value' => $model->statustext,
-                ),
-                'salary',
-        	),
-        ));
-        ?>
-    </div>
-    <div class="span3">
-        <?php
-        // Критерии отбора участников для вакансии
-        $this->widget('admin.extensions.VacancyFilters.VacancyFilters', array(
-            'mode'    => 'vacancy',
-            'vacancy' => $model,
-            //'filterInstances' => $model->filterinstances,
-        ));
-        ?>
-    </div>
+<div class="row-fluid">
+    <h1>Роль "<?php echo $model->name; ?>"</h1>
+    <?php
+    // описание самой роли
+    $this->widget('bootstrap.widgets.TbDetailView', array(
+    	'data'       => $model,
+    	'attributes' => array(
+    		'name',
+    		'description:html',
+    		array(
+                'label' => 'Заполнение',
+                'value' => '('.$model->membersCount.' из '.$model->limit.') [Заявки: '.$model->requestsCount.']',
+            ),
+    		array(
+                'label' => ProjectsModule::t('status'),
+                'value' => $model->statustext,
+            ),
+            'salary',
+    	),
+    ));
+    ?>
 </div>
+<?php 
+
+// виджет расширенной формы поиска (по всей базе)
+$this->widget('catalog.extensions.search.QSearchForm.QSearchForm', array(
+    'searchObject' => $model,
+    'mode'         => 'vacancy',
+    'dataSource'   => 'db',
+    'searchUrl'    => '/admin/eventVacancy/setSearchData',
+    'clearUrl'     => '/admin/eventVacancy/clearFilterSearchData',
+    'countUrl'     => '/admin/eventVacancy/setSearchData',
+    'countResultPosition' => 'bottom',
+    'refreshDataOnChange' => true,
+    //'refreshDataOnChange' => false,
+    'searchButtonTitle'      => 'Сохранить',
+    'clearButtonHtmlOptions' => array(
+        'class' => 'btn btn-danger btn-large',
+        'id'    => 'clear_search',
+    ),
+    'countContainerHtmlOptions' => array(
+        'class' => 'well text-center',
+    ),
+));
+
+$this->widget('admin.extensions.ExtraFieldsManager.ExtraFieldsManager', array(
+    'vacancy' => $model,
+));
+
+$clips = Yii::app()->getModule('admin')->formClips;
+foreach ( $clips as $clip )
+{
+    echo $this->clips[$clip];
+}
+?>
