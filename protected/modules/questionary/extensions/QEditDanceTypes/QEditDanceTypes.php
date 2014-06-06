abb960a4ecfe18412d541caaa93cac45b179a5a5
uniconstructor <php1602agregator@gmail.com>
2014-06-06 22:04:39 +0400

    Удалены все старые виджеты ввода списков значений анкеты

--- protected/modules/questionary/extensions/QEditDanceTypes/QEditDanceTypes.php
+++
@@ -1,8 +0,0 @@
-<?php
-Yii::import('application.modules.questionary.extensions.QEditSimpleActivity.QEditSimpleActivity');
-
-class QEditDanceTypes extends QEditSimpleActivity
-{
-    public $fieldName = 'dancetype';
-    public $modelName = 'Questionary';
-}
