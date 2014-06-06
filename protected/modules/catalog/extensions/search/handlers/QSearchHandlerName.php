32f8363d1cf8297cfb01ed798fde1fd3ce11210c
uniconstructor <php1602agregator@gmail.com>
2014-06-06 22:24:18 +0400

    Улучшен поиск по ФИО

--- protected/modules/catalog/extensions/search/handlers/QSearchHandlerName.php
+++ protected/modules/catalog/extensions/search/handlers/QSearchHandlerName.php
@@ -14,21 +14,20 @@ class QSearchHandlerName extends QSearchHandlerBase
     {
         $criteria = new CDbCriteria();
         $data = $this->getFilterData();
-        $name = $data['name'];
+        $name = trim($data['name']);
         
         if ( mb_ereg(' ', $name) )
         {// Если в поле введены и имя и фамилия -
             // то разбиваем строку по пробелам для того чтобы отделить имя от фамилии при дальнейшем поиске
             $parts = explode(' ', $name);
-            
             // Поскольку мы не знаем, что введено сначала: имя или фамилия - то будем искать по всем полям
             $criteria->addInCondition('firstname', $parts);
             $criteria->addInCondition('lastname', $parts, 'OR');
         }else
         {// в поле введено одно слово
-           // Поскольку мы не знаем, что введено сначала: имя или фамилия - то будем искать по всем полям
-           $criteria->compare('firstname', $name);
-           $criteria->compare('lastname', $name, false, 'OR');
+            // Поскольку мы не знаем, что введено сначала: имя или фамилия - то будем искать по всем полям
+            $criteria->compare('firstname', $name);
+            $criteria->compare('lastname', $name, false, 'OR');
         }
         return $criteria;
     }
