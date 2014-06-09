905738d03269fb06a780c60b955300850803dbd2
uniconstructor <php1602agregator@gmail.com>
2014-06-09 08:06:38 +0400

    Отлажен процесс сохранения динамической формы

--- protected/modules/projects/controllers/VacancyController.php
+++ protected/modules/projects/controllers/VacancyController.php
@@ -88,11 +88,86 @@ class VacancyController extends Controller
     }
     
     /**
-     * Создать заявку на участии в мероприятии
-     * (выполняется после всех проверок)
+     * Отобразить страницу с динамической формой анкеты, которая 
+     * одновременно регистрирует пользователя в системе и подает от его имени заявку на интересующую роль
+     * 
+     * @return void
+     */
+    public function actionRegistration()
+    {
+        // id роли
+        // @todo добавить проверку статуса роли
+        $vid     = Yii::app()->request->getParam('vid', 0);
+        $vacancy = $this->loadModel($vid);
+        
+        // id анкеты: разрешаем только регистрацию новых пользователей или подачу заявки от своего имени
+        // подача заявки от имени другого участника доступна только в админке
+        if ( $qid = Yii::app()->getModule('questionary')->getCurrentQuestionaryId() )
+        {// заявку подает существующий участник 
+            $questionary = Questionary::model()->findByPk($qid);
+            $model = new QDynamicFormModel('application');
+            $model->displayFilled = false;
+        }else
+        {// участник регистрируется через подачу заявки
+            $questionary = new Questionary;
+            $model = new QDynamicFormModel('registration');
+        }
+        
+        // запоминаем роль и анкету, от имени которой будет подаваться заявка
+        $model->vacancy     = $vacancy;
+        $model->questionary = $questionary;
+        
+        if ( $formData = Yii::app()->request->getParam('QDynamicFormModel') )
+        {// форма заполнена: пришли данные из формы регистрации пользователя
+            $model->attributes = $formData;
+            $model->galleryid  = $formData['galleryid'];
+            // Проверка данных формы по AJAX
+            $this->performAjaxValidation($model);
+            
+            if ( $model->validate() )
+            {// все данные формы верны
+                if ( $user = $model->save() )
+                {/* @var $user User */
+                    // Вместе с сохранением данных участника
+                    // сразу же происходит его авторизация на сайте
+                    Yii::app()->getModule('user')->forceLogin($user);
+                    // добавляем flash-сообщение об успешной регистрации
+                    Yii::app()->user->setFlash('success', 'Регистрация завершена');
+                    // и перенаправляем его на страницу 
+                    $this->redirect('finish');
+                }
+            }
+        }elseif ( $questionary->isNewRecord OR $model->scenario === 'registration' )
+        {// это регистрация и у пользователя нет аккаунта:
+            // cоздаем пустую галерею чтобы было куда загружать изображения
+            $gallery = new Gallery();
+            // Определяем в каких размерах создавать миниатюры изображений в галерее
+            $gallery->versions    = Yii::app()->getModule('questionary')->gallerySettings['versions'];
+            $gallery->limit       = 40;
+            $gallery->name        = 1;
+            $gallery->description = 1;
+            $gallery->save();
+            
+            if ( ! $gallery->subfolder )
+            {// @todo beforeSave не может знать id для записи в subfolder до сохранения записи
+                // поэтому загрузка изображений происходила в неправильные директории
+                // этот код можно будет убрать после того как будет переписан класс gallery
+                $gallery->subfolder = $gallery->id;
+            }
+            $model->galleryid = $gallery->id;
+        }
+        
+        // Отображаем страницу формы с регистрацией на роль
+        $this->render('registration', array(
+            'model' => $model,
+        ));
+    }
+    
+    /**
+     * Создать заявку на участии в мероприятии (выполняется после всех проверок)
      *
      * @param EventVacancy $vacancy
-     * @return null
+     * @return bool
      *
      * @todo сделать проверку - не является ли текущий участник гостем, если questionaryid не указан
      */
@@ -127,5 +202,18 @@ class VacancyController extends Controller
             throw new CHttpException(404, 'Роль не найдена id='.$id);
         }
         return $model;
+    }
+    
+    /**
+     * Performs the AJAX validation.
+     * @param CModel the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if ( isset($_POST['ajax']) && $_POST['ajax'] === 'dynamic-registration-form' )
+        {
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
     }
 }
