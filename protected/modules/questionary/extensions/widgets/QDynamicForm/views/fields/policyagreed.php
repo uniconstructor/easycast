197ee44c2083780952370106624296bf52213395
uniconstructor <php1602agregator@gmail.com>
2014-06-09 07:58:04 +0400

    Исправлена верстка полей динамической формы

--- protected/modules/questionary/extensions/widgets/QDynamicForm/views/fields/policyagreed.php
+++ protected/modules/questionary/extensions/widgets/QDynamicForm/views/fields/policyagreed.php
@@ -7,4 +7,6 @@
 /* @var $model QDynamicFormModel */
 
 // согласие с условиями использования
+// @todo придумать как можно переместить галочку в произвольное место формы
+$model->policyagreed = 1;
 echo $form->checkBoxRow($model, 'policyagreed');
