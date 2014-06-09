9fc42654932327a1bd1ebf8d12e167b5cf4ef9b7
uniconstructor <php1602agregator@gmail.com>
2014-06-09 18:18:40 +0400

    Изменено название контроллера

--- protected/controllers/TopmodelController.php
+++ protected/controllers/TopmodelController.php
@@ -8,9 +8,7 @@
  */
 class TopModelController extends Controller
 {
-
     /**
-     *
      * @see CController::init()
      */
     public function init()
@@ -31,7 +29,7 @@ class TopModelController extends Controller
     {
         // id роли
         // @todo добавить проверку статуса роли
-        $vid = Yii::app()->request->getParam('vid', 0);
+        $vid = Yii::app()->request->getParam('vid', 600);
         $vacancy = $this->loadModel($vid);
         
         // id анкеты: разрешаем только регистрацию новых пользователей или подачу заявки от своего имени
@@ -48,7 +46,7 @@ class TopModelController extends Controller
         }
         
         // запоминаем роль и анкету, от имени которой будет подаваться заявка
-        $model->vacancy = $vacancy;
+        $model->vacancy     = $vacancy;
         $model->questionary = $questionary;
         
         if ( $formData = Yii::app()->request->getParam('QDynamicFormModel') )
@@ -66,9 +64,9 @@ class TopModelController extends Controller
                     // сразу же происходит его авторизация на сайте
                     Yii::app()->getModule('user')->forceLogin($user);
                     // добавляем flash-сообщение об успешной регистрации
-                    Yii::app()->user->setFlash('success', 'Регистрация завершена');
+                    //Yii::app()->user->setFlash('success', 'Регистрация завершена');
                     // и перенаправляем его на страницу завершения
-                    $this->redirect('//topModel/finish');
+                    $this->redirect('//topmodel/finish');
                 }
             }
         }elseif ( $questionary->isNewRecord or $model->scenario === 'registration' )
@@ -77,12 +75,12 @@ class TopModelController extends Controller
             $gallery = new Gallery();
             // Определяем в каких размерах создавать миниатюры изображений в галерее
             $gallery->versions = Yii::app()->getModule('questionary')->gallerySettings['versions'];
-            $gallery->limit = 40;
-            $gallery->name = 1;
+            $gallery->limit       = 40;
+            $gallery->name        = 1;
             $gallery->description = 1;
             $gallery->save();
             
-            if ( !$gallery->subfolder )
+            if ( ! $gallery->subfolder )
             {// @todo beforeSave не может знать id для записи в subfolder до сохранения записи
                 // поэтому загрузка изображений происходила в неправильные директории
                 // этот код можно будет убрать после того как будет переписан класс gallery
@@ -92,8 +90,7 @@ class TopModelController extends Controller
         }
         
         // Отображаем страницу формы с регистрацией на роль
-        $this->render('index', array(
-            'model' => $model));
+        $this->render('index', array('model' => $model));
     }
 
     /**
