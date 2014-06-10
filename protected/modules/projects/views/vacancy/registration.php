0cee0600095930c5c8cdcfc6c513cf76a116e634
uniconstructor <php1602agregator@gmail.com>
2014-06-10 12:54:12 +0400

    Закончена страница регистрации участника с заполнением доп. полей.

--- protected/modules/projects/views/vacancy/registration.php
+++ protected/modules/projects/views/vacancy/registration.php
@@ -3,25 +3,31 @@
  * Подача заявки на событие через динамическую форму анкеты
  */
 /* @var $this VacancyController */
+/* @var $model QDynamicFormModel */
 ?>
 <div class="page-alternate">
     <div class="container">
         <div class="row">
-            <div class="span12">
-                <div class="title-page">
-                    <h4 class="intro-description">
-                        Чтобы подать заявку на эту роль необходходимо указать дополнительные данные. <br>
-                        <b>Ваши контакты не будут видны на сайте</b> никому кроме администраторов 
-                        и будут использоваться только для связи с вами. 
-                    </h4>
-                </div>
-            </div>
+            <?php 
+            // информация о событии на которое подается заявка
+            $this->widget('projects.extensions.ProjectInfo.ProjectInfo', array(
+                'eventId'     => $model->vacancy->event->id,
+                'displayTabs' => array('main'),
+            ));
+            ?>
         </div>
         <div class="row-fluid">
             <?php 
             // виджет динамической формы: он сам выбирает нужные поля в зависимости от настроек роли
             $this->widget('questionary.extensions.widgets.QDynamicForm.QDynamicForm', array(
                 'model' => $model
+            ));
+            ?>
+        </div>
+        <div class="row">
+            <?php
+            $this->widget('questionary.extensions.widgets.QUserInvites.QUserInvites', array(
+                'questionary' => $model->questionary,
             ));
             ?>
         </div>
