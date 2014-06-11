247a30248a4c6f05e7555d8a238db50edc9ac950
uniconstructor <php1602agregator@gmail.com>
2014-06-12 03:18:17 +0400

    Исправлена работа переключателя разделов

--- protected/modules/catalog/extensions/search/QSectionHelper/views/sections.php
+++ protected/modules/catalog/extensions/search/QSectionHelper/views/sections.php
@@ -9,9 +9,10 @@
     // верхняя полоска со списком разделов
     $filter = CatalogFilter::model()->find("`shortname` = 'iconlist'");
     $this->widget('catalog.extensions.search.filters.'.$filter->widgetclass.'.'.$filter->widgetclass, array(
-        'display'      => 'form',
-        'filter'       => $filter,
-        'searchObject' => $this->searchObject,
+        'display'             => 'form',
+        'dataSource'          => $this->dataSource,
+        'filter'              => $filter,
+        'searchObject'        => $this->searchObject,
         // обновляем результаты поиска каждый раз при изменении списка разделов
         'refreshDataOnChange' => true,
     ));
