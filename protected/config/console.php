fbaae8cbdea0e2ed0a2033cf3bc61dbcfc20d4d6
uniconstructor <php1602agregator@gmail.com>
2014-06-13 03:18:42 +0400

    Убраны лишние параметры из конфигурации консольного приложения

--- protected/config/console.php
+++ protected/config/console.php
@@ -14,26 +14,15 @@ $consoleConfig = CMap::mergeArray(
             'application.extensions.*'
         ),
         // preloading components
-        'preload' => array('log', 'messages'),
+        //'preload' => array('log', 'messages'),
         // application components
         'components' => array(
             // устанавливаем параметр hostInfo для того чтобы из консоли могли создаваться абсолютные ссылки
-            // ( http://www.yiiframework.com/forum/index.php/topic/14825-problem-with-createurl-and-createabsoluteurl-in-console-application/ )
+            // @see http://www.yiiframework.com/forum/index.php/topic/14825-problem-with-createurl-and-createabsoluteurl-in-console-application/
             'request' => array(
                 'hostInfo'  => 'http://easycast.ru',
                 'baseUrl'   => '',
                 'scriptUrl' => '',
-            ),
-            'log' => array(
-                'class'  => 'CLogRouter',
-                'routes' => array(
-                    array(
-                        'class'        => 'CDbLogRoute',
-                        'connectionID' => 'db',
-                        'levels'       => 'error, warning, info, AWS',
-                        'autoCreateLogTable' => false,
-                    ),
-                ),
             ),
         ),
     )
