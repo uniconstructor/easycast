8f419a5d745e8fe19988b585e28a4f1a64db9e5c
uniconstructor <php1602agregator@gmail.com>
2014-06-12 11:31:42 +0400

    Добавлено безусловное использование фильтра поиска по статусу

--- protected/modules/catalog/extensions/search/handlers/QSearchHandlerStatus.php
+++ protected/modules/catalog/extensions/search/handlers/QSearchHandlerStatus.php
@@ -11,12 +11,27 @@ class QSearchHandlerStatus extends QSearchHandlerBase
      */
     protected function createCriteria()
     {
-        $data   = $this->getFilterData();
-        $values = implode("', '", CHtmlPurifier::purify($data['status']));
-    
         $criteria = new CDbCriteria();
-        $criteria->addCondition("`t`.`status` IN ('{$values}')");
-    
+        $data = $this->getFilterData();
+        
+        if ( isset($data['status']) AND ! empty($data['status']) )
+        {// если задан хотя бы один статус - ищем по нему
+            $values = implode("', '", $data['status']);
+            $criteria->addCondition("`t`.`status` IN ('{$values}')");
+        }else
+        {// на всякий случай всегда категорически исключим статусы отложенных и неподтвержденных анкет
+            $values = implode("', '", array('draft', 'unconfirmed', 'delayed'));
+            $criteria->addCondition("`t`.`status` NOT IN ('{$values}')");
+        }
         return $criteria;
     }
+    
+    /**
+     * Фильтр по поиску всегда используется
+     * @see QSearchHandlerBase::filterIsUsed()
+     */
+    /*protected function filterIsUsed()
+    {
+        return true;
+    }*/
 }
