d631ac391c2248328c4bf8b7bc5bfab1a121aa3f
uniconstructor <php1602agregator@gmail.com>
2014-06-14 17:49:12 +0400

    Исправлено затемнение при загрузке фотографий

--- protected/views/topmodel/index.php
+++ protected/views/topmodel/index.php
@@ -4,30 +4,28 @@
  */
 /* @var $this TopModelController */
 ?>
-<div class="page-alternate">
-    <div class="container">
-        <div class="row text-center">
-            <?= CHtml::image('/images/tm_form_header.jpg') ?>
-        </div>
-        <div class="row">
-            <div class="span12">
-                <div class="title-page">
-                    <h4 class="intro-description">
-                        Чтобы подать заявку на участие в кастинге необходходимо указать дополнительные данные.<br>
-                        (Личная информация из вашей анкеты не будет видна никому кроме организаторов кастинга. 
-                        Любые указанные контакты также будут использоваться только для уточнения 
-                        информации и оповещения вас о дальшейших этапах отбора)
-                    </h4>
-                </div>
+<div class="container">
+    <div class="row text-center">
+        <?= CHtml::image('/images/tm_form_header.jpg') ?>
+    </div>
+    <div class="row">
+        <div class="span12">
+            <div class="title-page">
+                <h4 class="intro-description">
+                    Чтобы подать заявку на участие в кастинге необходходимо указать дополнительные данные.<br>
+                    (Личная информация из вашей анкеты не будет видна никому кроме организаторов кастинга. 
+                    Любые указанные контакты также будут использоваться только для уточнения 
+                    информации и оповещения вас о дальшейших этапах отбора)
+                </h4>
             </div>
         </div>
-        <div class="row-fluid">
-            <?php 
-            // виджет динамической формы: он сам выбирает нужные поля в зависимости от настроек роли
-            $this->widget('questionary.extensions.widgets.QDynamicForm.QDynamicForm', array(
-                'model' => $model
-            ));
-            ?>
-        </div>
+    </div>
+    <div class="row-fluid">
+        <?php 
+        // виджет динамической формы: он сам выбирает нужные поля в зависимости от настроек роли
+        $this->widget('questionary.extensions.widgets.QDynamicForm.QDynamicForm', array(
+            'model' => $model
+        ));
+        ?>
     </div>
 </div>
