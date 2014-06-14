88afa0c2b1a42b0db0c5b11adf556bbb3724c17c
uniconstructor <php1602agregator@gmail.com>
2014-06-14 15:14:48 +0400

    Добавлены модели тегов

---
+++ protected/models/Tag.php
@@ -0,0 +1,127 @@
+<?php
+
+/**
+ * This is the model class for table "{{tags}}".
+ *
+ * The followings are the available columns in table '{{tags}}':
+ * @property integer $id
+ * @property string $objecttype
+ * @property string $objectid
+ * @property string $sourcetype
+ * @property string $sourceid
+ * @property string $name
+ * @property string $rawname
+ * @property string $description
+ * @property string $type
+ * @property integer $official
+ * @property string $timecreated
+ * @property string $timemodified
+ */
+class Tag extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return '{{tags}}';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('name, rawname', 'required'),
+			array('official', 'numerical', 'integerOnly'=>true),
+			array('objecttype, sourcetype, type', 'length', 'max'=>50),
+			array('objectid, sourceid, timecreated, timemodified', 'length', 'max'=>11),
+			array('name, rawname, description', 'length', 'max'=>255),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, objecttype, objectid, sourcetype, sourceid, name, rawname, description, type, official, timecreated, timemodified', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'objecttype' => 'Objecttype',
+			'objectid' => 'Objectid',
+			'sourcetype' => 'Sourcetype',
+			'sourceid' => 'Sourceid',
+			'name' => 'Name',
+			'rawname' => 'Rawname',
+			'description' => 'Description',
+			'type' => 'Type',
+			'official' => 'Official',
+			'timecreated' => 'Timecreated',
+			'timemodified' => 'Timemodified',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('objecttype',$this->objecttype,true);
+		$criteria->compare('objectid',$this->objectid,true);
+		$criteria->compare('sourcetype',$this->sourcetype,true);
+		$criteria->compare('sourceid',$this->sourceid,true);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('rawname',$this->rawname,true);
+		$criteria->compare('description',$this->description,true);
+		$criteria->compare('type',$this->type,true);
+		$criteria->compare('official',$this->official);
+		$criteria->compare('timecreated',$this->timecreated,true);
+		$criteria->compare('timemodified',$this->timemodified,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Tag the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
