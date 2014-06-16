837094f839f58f71edcf106d9cced8436aa0b112
uniconstructor <php1602agregator@gmail.com>
2014-06-16 08:53:14 +0400

    Изменена структура навигации в админке

--- protected/modules/admin/views/questionary/index.php
+++ protected/modules/admin/views/questionary/index.php
@@ -9,7 +9,6 @@ $this->menu=array(
     array('label'=>'Анкеты на проверку','url'=>array('/admin/questionary/requests')),
     array('label'=>'Отложенные анкеты','url'=>array('/admin/questionary/delayed')),
     array('label'=>'Созданные анкеты','url'=>array('/admin/questionary/created')),
-    array('label'=>'Разделы каталога','url'=>array('/admin/catalogSection/admin')),
     array('label'=>'Редактор стандартных значений','url'=>array('/admin/standartValue/')),
     //array('label'=>'','url'=>array('/admin/projectMember/index', 'projectid'=>$model->id, 'type' => 'applications')),
 );
