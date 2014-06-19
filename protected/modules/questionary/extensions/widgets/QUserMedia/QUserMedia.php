723cb43da4c4a26e4983edbe7cfd00f8c6ebf148
uniconstructor <php1602agregator@gmail.com>
2014-06-19 11:39:38 +0400

    Полностью переписан виджет отображения видео

--- protected/modules/questionary/extensions/widgets/QUserMedia/QUserMedia.php
+++ protected/modules/questionary/extensions/widgets/QUserMedia/QUserMedia.php
@@ -28,7 +28,7 @@ class QUserMedia extends CWidget
                 'active'  => true,
             );
         }
-        if ( $this->questionary->video )
+        /*if ( $this->questionary->video )
         {// есть хотя бы одно видео - отобразим его
             $tabs[] = array(
                 'label'   => 'Видео',
@@ -36,7 +36,7 @@ class QUserMedia extends CWidget
                 // делаем изначально активной вкладку видео только когда нет фото
                 'active'  => empty($tabs),
             );
-        }
+        }*/
         $this->widget('bootstrap.widgets.TbTabs', array(
                 'type' => 'pills',
                 'tabs' => $tabs,
@@ -51,23 +51,53 @@ class QUserMedia extends CWidget
     protected function getPhotoTab()
     {
         $result = '';
-        // Список фотографий пользователя
-        $result .= $this->widget('ext.ECMarkup.EThumbCarousel.EThumbCarousel', array(
-            'previews'    => $this->questionary->getBootstrapPhotos('small'),
-            'photos'      => $this->questionary->getBootstrapPhotos('medium'),
-            'largePhotos' => $this->questionary->getBootstrapPhotos('large'),
-            'emptyText'   => $this->render('_message', null, true),
-            'echoScripts' => $this->echoScripts,
-            'id'          => 'ethumbcarousel'.$this->questionary->id,
+        $items = array();
+        
+        $videoCriteria = new CDbCriteria();
+        $videoCriteria->compare('objecttype', 'questionary');
+        $videoCriteria->compare('objectid', $this->questionary->id);
+        $videoCriteria->addInCondition('type', array('youtube', 'vimeo'));
+        if ( $records = Video::model()->findAll($videoCriteria) )
+        {
+            foreach ( $records  as $record )
+            {
+                $items[] = array('video' => $record->link);
+            }
+        }
+        
+        $criteria = new CDbCriteria();
+        $criteria->compare('gallery_id', $this->questionary->galleryid);
+        if ( $records = GalleryPhoto::model()->findAll($criteria) )
+        {
+            foreach ( $records  as $record )
+            {
+                $items[] = array(
+                    'image'       => $record->getUrl('medium'),
+                    'thumb'       => $record->getUrl('small'),
+                    'big'         => $record->getUrl(''),
+                    'title'       => $record->name,
+                    'description' => $record->description,
+                    //'link' => 'http://domain.com',
+                );
+            }
+        }
+        $result .= $this->widget('Galleria', array(
+            'options' => array(
+                'transition' => 'fade',
+                'responsive' => true,
+                'lightbox'   => true,
+                'dataSource' => $items,
+                'keepSource' => true,
+            ),
         ), true);
+        
         if ( ! $this->questionary->getGalleryPhotos() AND $this->canEditUser() )
         {
             $result .= $this->widget('GalleryManager', array(
-                'gallery' => $questionary->galleryBehavior->getGallery(),
+                'gallery' => $this->questionary->galleryBehavior->getGallery(),
                 'controllerRoute' => '/questionary/gallery'
             ), true);
         }
-        
         return $result;
     }
     
@@ -77,14 +107,7 @@ class QUserMedia extends CWidget
      */
     protected function getVideoTab()
     {
-        $result = '';
-        
-        $result .= $this->widget('ext.ECMarkup.ECVideoList.ECVideoList', array(
-            'objectType' => 'questionary',
-            'objectId'   => $this->questionary->id,
-        ), true);
-        
-        return $result;
+        return '';
     }
     
     /**
