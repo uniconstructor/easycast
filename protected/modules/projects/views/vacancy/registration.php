45304fec9946041c642a55ff1546c8a192351bfa
uniconstructor <php1602agregator@gmail.com>
2014-06-19 04:20:59 +0400

    Исправлена верстка

--- protected/modules/projects/views/vacancy/registration.php
+++ protected/modules/projects/views/vacancy/registration.php
@@ -2,35 +2,33 @@
 /**
  * Подача заявки на событие через динамическую форму анкеты
  */
-/* @var $this VacancyController */
+/* @var $this  VacancyController */
 /* @var $model QDynamicFormModel */
 ?>
-<div class="page-alternate">
-    <div class="container">
-        <div class="row">
-            <?php 
-            // информация о событии на которое подается заявка
-            $this->widget('projects.extensions.ProjectInfo.ProjectInfo', array(
-                'eventId'     => $model->vacancy->event->id,
-                'displayTabs' => array('main'),
-            ));
-            ?>
-        </div>
-        <div class="row-fluid">
-            <?php 
-            // виджет динамической формы: он сам выбирает нужные поля в зависимости от настроек роли
-            $this->widget('questionary.extensions.widgets.QDynamicForm.QDynamicForm', array(
-                'model' => $model
-            ));
-            ?>
-        </div>
-        <div class="row">
-            <?php
-            // @todo включить после запуска топ-модели
-            /*$this->widget('questionary.extensions.widgets.QUserInvites.QUserInvites', array(
-                'questionary' => $model->questionary,
-            ));*/
-            ?>
-        </div>
+<div class="container">
+    <div class="row">
+        <?php 
+        // информация о событии на которое подается заявка
+        $this->widget('projects.extensions.ProjectInfo.ProjectInfo', array(
+            'eventId'     => $model->vacancy->event->id,
+            'displayTabs' => array('main'),
+        ));
+        ?>
+    </div>
+    <div class="row-fluid">
+        <?php 
+        // виджет динамической формы: он сам выбирает нужные поля в зависимости от настроек роли
+        $this->widget('questionary.extensions.widgets.QDynamicForm.QDynamicForm', array(
+            'model' => $model
+        ));
+        ?>
+    </div>
+    <div class="row">
+        <?php
+        // @todo включить после запуска топ-модели
+        /*$this->widget('questionary.extensions.widgets.QUserInvites.QUserInvites', array(
+            'questionary' => $model->questionary,
+        ));*/
+        ?>
     </div>
 </div>
