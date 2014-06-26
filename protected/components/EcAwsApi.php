1e4c796912c076f660fa96f658f40de699a0c9d0
uniconstructor <php1602agregator@gmail.com>
2014-06-26 16:08:46 +0400

    Упорядочены функции: сначала public, потом protected

--- protected/components/EcAwsApi.php
+++ protected/components/EcAwsApi.php
@@ -209,40 +209,6 @@ class EcAwsApi extends CApplicationComponent
     }
     
     /**
-     * Получить массив с параметрами для запроса к сервису Amazon SES через Amazon PHP API 2
-     * 
-     * @param string $email - адрес получателя
-     * @param string $subject - тема письма
-     * @param string $message - текст сообщения
-     * 
-     * @return array
-     * 
-     * @todo добавить настройку "присылать письма как текст или как HTML"
-     * @todo делать striptags для сообщений длиннее 64Кб 
-     */
-    protected function createSESEmail($email, $subject, $message, $from)
-    {
-        return array(
-            'Source' => $from,
-            'Destination' => array(
-                'ToAddresses' => array($email),
-            ),
-            'Message' => array(
-                'Subject' => array(
-                    'Data'    => $subject,
-                    'Charset' => 'utf-8',
-                ),
-                'Body' => array(
-                    'Html' => array(
-                        'Data'    => $message,
-                        'Charset' => 'utf-8',
-                    ),
-                ),
-            ),
-        );
-    }
-    
-    /**
      * Поставить письмо в очередь отправки.
      * Эта функция используется для массовой рассылки сообщений. Она работает быстро и позволяет 
      * обойти ограничения Amazon на количество отправляемых писем в секунду.
@@ -410,6 +376,95 @@ class EcAwsApi extends CApplicationComponent
     }
     
     /**
+     * Получить информацию об очереди email-сообщений
+     *
+     * @return null
+     */
+    public function getEmailQueryInfo()
+    {
+        $url = $this->getEmailQueueUrl();
+        $args = array(
+            'QueueUrl'       => $url,
+            'AttributeNames' => array(
+                'ApproximateNumberOfMessages',
+                'ApproximateNumberOfMessagesNotVisible',
+                'ApproximateNumberOfMessagesDelayed',
+            ),
+        );
+        // отправляем запрос для получения информации об очереди
+        $result = $this->getSqs()->getQueueAttributes($args);
+    
+        return $result['Attributes'];
+    }
+    
+    /**
+     * Вывести в консоль информацию о состоянии очереди email-сообщений
+     *
+     * @return null
+     */
+    public function showEmailQueryInfo()
+    {
+        $info = $this->getEmailQueryInfo();
+    
+        $this->trace('');
+        $this->trace('[Email queue info]');
+        $this->trace('Total messages: '.$info['ApproximateNumberOfMessages']);
+        $this->trace('Not visible:    '.$info['ApproximateNumberOfMessagesNotVisible']);
+        $this->trace('Delayed:        '.$info['ApproximateNumberOfMessagesDelayed']);
+        $this->trace('');
+    }
+    
+    /**
+     * Проверить, есть ли email-сообщения, ждущие отправки в очереди
+     *
+     * @return null
+     */
+    public function emailQueueIsEmpty()
+    {
+        $info = $this->getEmailQueryInfo();
+    
+        if ( $info['ApproximateNumberOfMessages'] == 0 )
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Получить массив с параметрами для запроса к сервису Amazon SES через Amazon PHP API 2
+     *
+     * @param string $email - адрес получателя
+     * @param string $subject - тема письма
+     * @param string $message - текст сообщения
+     *
+     * @return array
+     *
+     * @todo добавить настройку "присылать письма как текст или как HTML"
+     * @todo делать striptags для сообщений длиннее 64Кб
+     */
+    protected function createSESEmail($email, $subject, $message, $from)
+    {
+        return array(
+            'Source' => $from,
+            'Destination' => array(
+                'ToAddresses' => array($email),
+            ),
+            'Message' => array(
+                'Subject' => array(
+                    'Data'    => $subject,
+                    'Charset' => 'utf-8',
+                ),
+                'Body' => array(
+                    'Html' => array(
+                        'Data'    => $message,
+                        'Charset' => 'utf-8',
+                    ),
+                ),
+            ),
+        );
+    }
+    
+    /**
      * Создать массив с параметрами для функции добавления сообщения в очередь
      * 
      * @param string $message - email для отправки в формате JSON
@@ -440,8 +495,7 @@ class EcAwsApi extends CApplicationComponent
         return array(
             'QueueUrl'            => $url,
             'MaxNumberOfMessages' => $count,
-            // не позволяем никому видеть полученные нами сообщения еще как минимум 2 минуты
-            // (пока они не удалятся)
+            // не позволяем никому видеть полученные нами сообщения еще как минимум 2 минуты (пока они не удалятся)
             'VisibilityTimeout'   => self::HIDE_READ_MESSAGES_TIMEOUT,
         );
     }
@@ -475,7 +529,6 @@ class EcAwsApi extends CApplicationComponent
                 }
             }
         }
-        
         return $this->queueUrl;
     }
     
@@ -498,61 +551,6 @@ class EcAwsApi extends CApplicationComponent
             'from'    => $from,
         );
         return CJSON::encode($data);
-    }
-    
-    /**
-     * Получить информацию об очереди email-сообщений
-     * 
-     * @return null
-     */
-    public function getEmailQueryInfo()
-    {
-        $url = $this->getEmailQueueUrl();
-        $args = array(
-            'QueueUrl'       => $url,
-            'AttributeNames' => array(
-                'ApproximateNumberOfMessages',
-                'ApproximateNumberOfMessagesNotVisible',
-                'ApproximateNumberOfMessagesDelayed',
-            ),
-        );
-        // отправляем запрос для получения информации об очереди
-        $result = $this->getSqs()->getQueueAttributes($args);
-        
-        return $result['Attributes'];
-    }
-    
-    /**
-     * Вывести в консоль информацию о состоянии очереди email-сообщений
-     * 
-     * @return null
-     */
-    public function showEmailQueryInfo()
-    {
-        $info = $this->getEmailQueryInfo();
-        
-        $this->trace('');
-        $this->trace('[Email queue info]');
-        $this->trace('Total messages: '.$info['ApproximateNumberOfMessages']);
-        $this->trace('Not visible:    '.$info['ApproximateNumberOfMessagesNotVisible']);
-        $this->trace('Delayed:        '.$info['ApproximateNumberOfMessagesDelayed']);
-        $this->trace('');
-    }
-    
-    /**
-     * Проверить, есть ли email-сообщения, ждущие отправки в очереди
-     * 
-     * @return null
-     */
-    public function emailQueueIsEmpty()
-    {
-        $info = $this->getEmailQueryInfo();
-        
-        if ( $info['ApproximateNumberOfMessages'] == 0 )
-        {
-            return true;
-        }
-        return false;
     }
     
     /**
