418d89886fab8003174173c00fe869623b4db01d
frost <frost@easycast.ru>
2014-07-23 02:25:51 +0400

    Для админов добавлена возможность редактировать введенные участником значен6ия доп. полей

--- protected/modules/admin/extensions/ExtraFieldsList/ExtraFieldsList.php
+++ protected/modules/admin/extensions/ExtraFieldsList/ExtraFieldsList.php
@@ -9,6 +9,22 @@ class ExtraFieldsList extends CWidget
      * @var ProjectMember
      */
     public $member;
+    /**
+     * @var string - адрес по которому происходит обработка AJAX-редактирования дополнительных полей
+     */
+    public $editUrl;
+    
+    /**
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+        if ( ! $this->editUrl )
+        {
+            $this->editUrl = Yii::app()->createUrl('/admin/extraFieldValue/update');
+        }
+        parent::init();
+    }
     
     /**
      * @see CWidget::run()
@@ -21,18 +37,43 @@ class ExtraFieldsList extends CWidget
         {
             return;
         }
+        
         foreach ( $this->member->vacancy->extraFields as $extraField )
         {/* @var $extraField ExtraField */
             $value = ExtraFieldValue::model()->forField($extraField->id)->
                 forVacancy($this->member->vacancy->id)->forQuestionary($this->member->memberid)->find();
             if ( $value AND $extraField->type != 'checkbox' )
             {
-                $data[$extraField->name] = $value->value;
-                $attributes[] = array(
+                $attribute = array(
                     'name'  => $extraField->name,
                     'label' => $extraField->label,
                 );
-                $data[$extraField->name] = $value->value;
+                if ( Yii::app()->user->checkAccess('Admin') )
+                {// админам разрешаем редактировать ответы участников
+                    $data[$extraField->name] = $this->widget('bootstrap.widgets.TbEditableField', array(
+                        'type'      => 'textarea',
+                        'model'     => $value,
+                        'attribute' => 'value',
+                        'url'       => $this->editUrl,
+                        'params'    => array(
+                            Yii::app()->request->csrfTokenName => Yii::app()->request->csrfToken,
+                        ),
+                        // отображаем форму редактирования внутри ячейки чтобы не глючило наложение
+                        'mode'    => 'inline',
+                        'options' => array(
+                            'onblur' => 'submit',
+                        ),
+                    ), true);
+                    // разрешаем в столбце ответа html для того чтобы сработал 
+                    // админский элемент редактирования ответов
+                    $attribute['type'] = 'raw';
+                }else
+                {// всем остальным только смотреть
+                    $data[$extraField->name] = $value->value;
+                }
+                
+                $attributes[] = $attribute;
+                unset($attribute);
             }
         }
         
