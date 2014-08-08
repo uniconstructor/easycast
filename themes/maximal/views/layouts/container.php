86538d58bb7f168865c6b86f3e79ee47c991d26e
frost <frost@easycast.ru>
2014-08-09 00:16:45 +0400

    Добавлена дополнительная разметка для верстки контейнера (чтобы не писать его каждый раз вручную)

---
+++ themes/maximal/views/layouts/container.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * 
+ */
+/* @var $this Controller */
+
+// head-раздел и мета-теги
+$this->beginContent('//layouts/main');
+?>
+<div class="page">
+    <div class="container">
+        <?= $content; ?>
+    </div>
+</div>
+<?php
+// все скрипты, которые должны быть подключены внизу страницы CClientScript::POS_END
+$this->endContent();
