76c45b5f2c50d232d33488cd7953bc918a4e1b3c
frost <frost@easycast.ru>
2014-08-10 06:07:29 +0400

    Добавлен промещуточный вариант дополнительных пояснений к фото

--- protected/modules/questionary/extensions/widgets/QDynamicForm/views/fields/photo.php
+++ protected/modules/questionary/extensions/widgets/QDynamicForm/views/fields/photo.php
@@ -16,3 +16,20 @@ $form->widget('GalleryManager', array(
 ));
 //echo $form->error($model, 'galleryid');
 echo $form->error($model, 'photo');
+
+// FIXME заменить hardcoded-значение на настройку
+if ( $this->vacancy->id === 749 OR YII_DEBUG )
+{
+?>
+    <div style="padding-top: 20px;">
+        <h3 class="spec" style="text-transform: none;">
+        Для участния в кастинге необходимо загрузить минимум 
+        3 фотографии:</h3>
+        <ol>
+            <li>Ваше портретное фото
+            <li>Ваше фото по пояс
+            <li>Фотография приготовленного вами на видео блюда
+        </ol>
+    </div>
+<?php
+}
