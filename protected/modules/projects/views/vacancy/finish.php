ad8a56b49bb27b30b178b9ba46826cb188a931a8
frost <frost@easycast.ru>
2014-08-14 03:10:11 +0400

    Исправлены ошибки при перенаправлении участника обратно на сайт телеканала

--- protected/modules/projects/views/vacancy/finish.php
+++ protected/modules/projects/views/vacancy/finish.php
@@ -7,6 +7,7 @@
  * @todo Выводить здесь список всех активных подходящих ролей на которые идет набор сейчас
  */
 /* @var $this VacancyController */
+/* @var $redirectUrl string */
 ?>
 <div class="page-alternate">
     <div class="container">
@@ -14,10 +15,19 @@
             <div class="span12">
                 <div class="title-page">
                     <h1 class="title">Заявка принята</h1>
-                    <h4 class="intro-description">
                         <?php 
-                        $this->widget('bootstrap.widgets.TbAlert');
+                        // FIXME значение в настройку
+                        if ( $vacancy->id == 749 )
+                        {
+                            $redirectUrl = 'http://ctc.ru/rus/projects/show/76527/';
+                            $image = CHtml::image('/masterchief/size_3_baner-master-chef-3.jpg', '', array(
+                                'style' => 'max-width: 100%;',
+                            ));
+                            echo CHtml::link($image, $redirectUrl);
+                        }
                         ?>
+                    <h4 class="intro-description">
+                        <?php $this->widget('bootstrap.widgets.TbAlert'); ?>
                     </h4>
                 </div>
             </div>
@@ -26,11 +36,35 @@
             <?php 
             // предлагаем посмотреть другие события
             // @todo вывести первые 5 доступных подходящих событий
-            $url = Yii::app()->createUrl('//agenda', array('newMode' => 'user'));
-            echo CHtml::link('Вернуться к списку событий', $url, array(
+            //$redirecrUrl = Yii::app()->createUrl('//agenda', array('newMode' => 'user'));
+            echo CHtml::link('Вернуться обратно', $redirectUrl, array(
                 'class' => 'btn btn-large btn-primary',
             ));
             ?>
         </div>
     </div>
 </div>
+<!--div class="page-alternate">
+    <div class="container">
+        <div class="row text-center">
+            <div class="alert alert-success alert-block">
+                Заявка успешно отправлена. Вы можете перейти обратно на сайт проекта.
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="title-page">
+                <?php
+                //$image = CHtml::image('size_3_baner-master-chef-2.jpg');
+                //echo CHtml::link($image, $redirecrUrl);
+                ?>
+            </div>
+        </div>
+        <div class="row text-center">
+            <?php 
+            /*echo CHtml::link('Вернуться на сайт проекта', $redirecrUrl, array(
+                'class' => 'btn btn-large btn-primary',
+            ));*/
+            ?>
+        </div>
+    </div>
+</div-->
