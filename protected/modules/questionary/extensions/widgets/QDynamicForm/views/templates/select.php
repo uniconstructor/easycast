955afd90fdafa198b90cf9a2d31f64f08917ae85
frost <frost@easycast.ru>
2014-08-14 03:06:00 +0400

    Исправлено сохранение стандартных значений анкеты

--- protected/modules/questionary/extensions/widgets/QDynamicForm/views/templates/select.php
+++ protected/modules/questionary/extensions/widgets/QDynamicForm/views/templates/select.php
@@ -7,6 +7,9 @@
 /* @var $model QDynamicFormModel */
 
 // select
-$models = QActivityType::model()->forActivity($attribute)->findAll();
-$options = CHtml::listData($models, 'id', 'translation');
+//$models = QActivityType::model()->forActivity($attribute)->findAll();
+//$options = CHtml::listData($models, 'id', 'translation');
+$default = array('' => Yii::t('coreMessages', 'not_set') );
+$options = QActivityType::model()->activityVariants($attribute);
+$options = CMap::mergeArray($default, $options);
 echo $form->dropDownListRow($model, $attribute, $options);
