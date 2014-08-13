3dedafa7266ddb68972ccb11a00610f5d2ee198f
frost <frost@easycast.ru>
2014-08-14 03:45:37 +0400

    Добавлен обработчик редактирования шагов регистрации

---
+++ protected/modules/admin/controllers/WizardStepController.php
@@ -0,0 +1,48 @@
+<?php
+
+Yii::import('ext.EditableGrid.EditableGridController');
+
+/**
+ * Контроллер для работы со списком шагов регистрации
+ */
+class WizardStepController extends EditableGridController
+{
+    /**
+     * @var string - класс модели сложного значения
+     */
+    protected $modelClass = 'WizardStep';
+    
+    /**
+     * @see EditableGridController::actionCreate()
+     */
+    public function actionCreate()
+    {
+        $step = parent::actionCreate();
+        
+        // чаще всего при создании нового шага регистрации мы сразу же привязываем его к роли
+        $objectType = Yii::app()->request->getParam('objectType');
+        $objectId   = Yii::app()->request->getParam('objectId', 0);
+        if ( $objectType AND $objectId )
+        {
+            $instance = new WizardStepInstance();
+            $instance->objecttype   = $objectType;
+            $instance->objectid     = $objectId;
+            $instance->wizardstepid = $step->id;
+            $instance->save();
+        }
+    }
+    
+    /**
+     * Проверить, есть ли у пользователя доступ к добавлению, редактированию или удалению объекта
+     * @param CActiveRecord $item
+     * @return void
+     */
+    protected function checkAccess($item)
+    {
+        if ( ! Yii::app()->user->checkAccess('Admin') )
+        {
+            throw new CHttpException(400, 'Ошибка при изменении записи');
+        }
+        return true;
+    }
+}
