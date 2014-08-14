e5b1b97cf5d202c6c41754ac33a13952552f2dbf
frost <frost@easycast.ru>
2014-08-14 08:09:33 +0400

    Добавлена очистка для поля "город"

--- protected/modules/questionary/extensions/behaviors/QScalarRules.php
+++ protected/modules/questionary/extensions/behaviors/QScalarRules.php
@@ -22,12 +22,13 @@ class QScalarRules extends CActiveRecordBehavior
                         ismusician, issportsman, isextremal, isathlete, hasskills, hastricks,
                         haslanuages, hasinshurancecard, countryid, nativecountryid,
                         shoessize, rating, hastatoo, playagemin, playagemax, 
-                        istheatreactor, ismediaactor, ownerid, currentcountryid, cityid, visible',
+                        istheatreactor, ismediaactor, ownerid, currentcountryid, visible',
                 'numerical', 'integerOnly' => true),
             
-            array('firstname, lastname, middlename, city, inshurancecardnum', 'length', 'max' => 128),
+            array('firstname, lastname, middlename, city, cityid, inshurancecardnum', 'length', 'max' => 128),
         
             array('mobilephone, homephone, addphone, inn', 'length', 'max' => 32),
+            array('status', 'length', 'max' => 50),
             array('hairlength, wearsize', 'length', 'max' => 16),
             array('vkprofile, fbprofile, okprofile, passportorg', 'length', 'max' => 255),
             array('privatecomment, admincomment, moderationcomment', 'length', 'max' => 4095),
@@ -36,12 +37,12 @@ class QScalarRules extends CActiveRecordBehavior
             // @todo добавить индивидуальную проверку для каждого типа поля
             array('passportdate, gender, height, weight, wearsize, looktype, haircolor, eyecolor,
                     physiquetype, titsize, chestsize, waistsize, hipsize, striptype
-                    striplevel, singlevel, wearsize, status', 'safe'),
+                    striplevel, singlevel, wearsize', 'safe'),
             // @todo добавить проверку сложных значений или проводить ее асинхронно при работе
             //       с изменяемой grid-таблицей
             array('voicetimbre, addchar, parodist, twin, vocaltype, sporttype, extremaltype, trick, skill', 'safe'),
             // @todo пропустить через trim все остальные поля
-            array('formattedBirthDate, firstname, lastname, birthdate, gender, galleryid', 
+            array('formattedBirthDate, firstname, lastname, birthdate, gender, galleryid, city', 
                 'filter', 'filter' => 'trim'),
         );
     }
@@ -68,7 +69,7 @@ class QScalarRules extends CActiveRecordBehavior
      */
     public function checkBirthDate($date)
     {
-        if ( $this->owner->scenario != 'update' )
+        if ( strpos($date, '.') === false OR $this->owner->scenario != 'update' )
         {
             return $date;
         }
