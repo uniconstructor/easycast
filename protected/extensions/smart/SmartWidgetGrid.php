cb3c931daf9a48c0505dd9867f2ddc17c6c66a49
frost <frost@easycast.ru>
2014-08-15 22:58:27 +0400

    Доработана верстка сетки виджетов

--- protected/extensions/smart/SmartWidgetGrid.php
+++ protected/extensions/smart/SmartWidgetGrid.php
@@ -6,9 +6,15 @@
 class SmartWidgetGrid extends CWidget
 {
     /**
-     * @var массив виджетов или настроек виджетов Jarvis
+     * @var array массив виджетов или настроек виджетов Jarvis
      */
     public $widgets = array();
+    /**
+     * @var array массив колонок или подгрупп виджетов внутри сетки
+     *      каждый раздел может содержать несколько виджетов, сетка
+     *      верстки может содержать несколько разделов
+     */
+    public $sections = array();
     
     /**
      * @see CWidget::init()
