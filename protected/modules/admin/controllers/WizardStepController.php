2b4a92b61619f838a51c5a70a66b4daee7a83c91
frost <frost@easycast.ru>
2014-08-19 23:43:35 +0400

    Убрана устаревшая связь с несуществующей больше моделью экземпляра шага регистрации

--- protected/modules/admin/controllers/WizardStepController.php
+++ protected/modules/admin/controllers/WizardStepController.php
@@ -13,26 +13,6 @@ class WizardStepController extends EditableGridController
     protected $modelClass = 'WizardStep';
     
     /**
-     * @see EditableGridController::actionCreate()
-     */
-    public function actionCreate()
-    {
-        $step = parent::actionCreate();
-        
-        // чаще всего при создании нового шага регистрации мы сразу же привязываем его к роли
-        $objectType = Yii::app()->request->getParam('objectType');
-        $objectId   = Yii::app()->request->getParam('objectId', 0);
-        if ( $objectType AND $objectId )
-        {
-            $instance = new WizardStepInstance();
-            $instance->objecttype   = $objectType;
-            $instance->objectid     = $objectId;
-            $instance->wizardstepid = $step->id;
-            $instance->save();
-        }
-    }
-    
-    /**
      * Проверить, есть ли у пользователя доступ к добавлению, редактированию или удалению объекта
      * @param CActiveRecord $item
      * @return void
