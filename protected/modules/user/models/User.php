b876b53421918d99a980a965c694a06bd60ece30
frost <frost@easycast.ru>
2014-08-19 17:42:50 +0400

    Исправлена проблема с проверкой имени пользователя и сохранением модели пользователя вместе с анкетой

--- protected/modules/user/models/User.php
+++ protected/modules/user/models/User.php
@@ -53,7 +53,7 @@ class User extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.CConsoleApplication
 		return ((get_class(Yii::app())=='CConsoleApplication' || (get_class(Yii::app())!='CConsoleApplication' && Yii::app()->user->isSuperuser))?array(
-			array('username', 'length', 'max'=>255, 'min' => 2,'message' => UserModule::t("Incorrect username (length between 3 and 20 characters).")),
+			array('username', 'length', 'max'=>255, 'min' => 1,'message' => UserModule::t("Incorrect username (length between 3 and 20 characters).")),
 			array('password', 'length', 'max'=>128, 'min' => 4,'message' => UserModule::t("Incorrect password (minimal length 4 symbols).")),
 			array('email', 'email'),
 			array('username', 'unique', 'message' => UserModule::t("This user's name already exists.")),
@@ -70,7 +70,7 @@ class User extends CActiveRecord
 			array('id, username, password, email, activkey, create_at, lastaccess, superuser, status', 'safe', 'on'=>'search'),
 		):((Yii::app()->user->id==$this->id)?array(
 			array('email', 'required'),
-			array('username', 'length', 'max'=>255, 'min' => 3,'message' => UserModule::t("Incorrect username (length between 3 and 20 characters).")),
+			array('username', 'length', 'max'=>255, 'min' => 1,'message' => UserModule::t("Incorrect username (length between 3 and 20 characters).")),
 			array('email', 'email'),
 			array('username', 'unique', 'message' => UserModule::t("This user's name already exists.")),
 			array('username', 'match', 'pattern' => '/^[A-Za-z0-9_\.()-]+$/u','message' => UserModule::t("Incorrect symbols (A-z0-9).")),
