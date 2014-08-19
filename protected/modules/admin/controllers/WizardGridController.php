4fa135ac52234d1ca1e9111d3edeeaa4a618e0c2
frost <frost@easycast.ru>
2014-08-19 22:43:42 +0400

    Добавлен обработчик списков пошаговых форм

---
+++ protected/modules/admin/controllers/WizardGridController.php
@@ -0,0 +1,28 @@
+<?php
+
+Yii::import('ext.EditableGrid.EditableGridController');
+
+/**
+ * Обработчик для виджета редактирования списка пошаговых форм
+ */
+class WizardGridController extends EditableGridController
+{
+    /**
+     * @var string - класс модели сложного значения
+     */
+    protected $modelClass = 'Wizard';
+    
+    /**
+     * Проверить, есть ли у пользователя доступ к добавлению, редактированию или удалению объекта
+     * @param CActiveRecord $item
+     * @return void
+     */
+    protected function checkAccess($item)
+    {
+        if ( ! Yii::app()->user->checkAccess('Admin') )
+        {
+            throw new CHttpException(400, 'Ошибка при изменении записи');
+        }
+        return true;
+    }
+}
