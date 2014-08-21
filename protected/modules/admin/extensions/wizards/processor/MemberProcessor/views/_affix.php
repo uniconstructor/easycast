ec6ba17ebdb70a2727f5b7f2358b96a812c2fbfe
frost <frost@easycast.ru>
2014-08-22 01:02:46 +0400

    Исправлена верстка старого виджета просмотра всех заявок.

--- protected/modules/admin/extensions/wizards/processor/MemberProcessor/views/_affix.php
+++ protected/modules/admin/extensions/wizards/processor/MemberProcessor/views/_affix.php
@@ -5,11 +5,11 @@
  */
 /* @var $this MemberProcessor */
 ?>
-<ul class="nav nav-list nav-stacked dropdown dropdown-bootstrap-fix affix" style="background-color: #eee;">
+<ul class="nav nav-list nav-stacked dropdown dropdown-bootstrap-fix" style="background-color:#eee;max-width:210px;">
     <?php 
-    // сверху всегжа добавляем вкладку "все"
+    // сверху всегда добавляем вкладку "все"
     $url = Yii::app()->createUrl($this->widgetRoute, $this->getLinkOptions(array('siid' => -1)));
-    echo '<li class=""><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;padding:10px;">
+    echo '<li><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;font-size:10px;padding:10px;">
             <i class="icon-chevron-right"></i>Все &nbsp;
             <span class="badge badge-info pull-right">'.$this->getMemberCount(-1).'</span></a></li>';
     foreach ( $this->vacancy->catalogSectionInstances as $instance )
@@ -25,13 +25,13 @@
             $active = 'active';
         }
         $url = Yii::app()->createUrl($this->widgetRoute, $this->getLinkOptions(array('siid' => $instance->id)));
-        echo '<li class="'.$active.'"><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;padding:10px;">
+        echo '<li class="'.$active.'"><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;font-size:10px;padding:10px;">
             <i class="icon-chevron-right"></i>'.$instance->section->name.'&nbsp;
             <span class="badge badge-info pull-right">'.$this->getMemberCount($instance->id).'</span></a></li>';
     }
     // внизу всегда добавляем вкладку с нераробранными
     $url = Yii::app()->createUrl($this->widgetRoute, $this->getLinkOptions(array('siid' => 0)));
-    echo '<li><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;padding:10px;">
+    echo '<li><a href="'.$url.'" style="text-transform:capitalize;font-weight:normal;font-size:10px;padding:10px;">
             <i class="icon-chevron-right"></i> Без категории&nbsp;<span class="badge badge-info pull-right">'.
             $this->getMemberCount(0).'</span></a></li>';
     ?>
