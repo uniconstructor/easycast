3570f268c9209fb8aba4c2637205a4e3a8b2bad0
frost <frost@easycast.ru>
2014-08-25 20:23:05 +0400

    Стандарт кодирования, комментарии.

--- protected/modules/admin/controllers/ProjectMemberController.php
+++ protected/modules/admin/controllers/ProjectMemberController.php
@@ -1,12 +1,15 @@
 <?php
 
+/**
+ * Контроллер для работы с заявками участников в админке
+ */
 class ProjectMemberController extends Controller
 {
 	/**
-	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
-	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 * @var string - the default layout for the views. Defaults to '//layouts/column2', meaning
+	 *               using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
-	public $layout='//layouts/column2';
+	public $layout = '//layouts/column2';
 
 	/**
 	 * @return array action filters
@@ -29,15 +32,15 @@ class ProjectMemberController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index', 'view'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array('create', 'update'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete','setStatus'),
+				'actions'=>array('admin', 'delete', 'setStatus'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
@@ -63,20 +66,20 @@ class ProjectMemberController extends Controller
 	 */
 	public function actionCreate()
 	{
-		$model=new ProjectMember;
+		$model = new ProjectMember;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['ProjectMember']))
+		if ( isset($_POST['ProjectMember']) )
 		{
-			$model->attributes=$_POST['ProjectMember'];
+			$model->attributes = $_POST['ProjectMember'];
 			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+				$this->redirect(array('view', 'id'=>$model->id));
 		}
 
-		$this->render('create',array(
-			'model'=>$model,
+		$this->render('create', array(
+			'model' => $model,
 		));
 	}
 
@@ -87,16 +90,18 @@ class ProjectMemberController extends Controller
 	 */
 	public function actionUpdate($id)
 	{
-		$model=$this->loadModel($id);
+		$model = $this->loadModel($id);
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['ProjectMember']))
+		if ( isset($_POST['ProjectMember']) )
 		{
-			$model->attributes=$_POST['ProjectMember'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			$model->attributes = $_POST['ProjectMember'];
+			if( $model->save() )
+			{
+			    $this->redirect(array('view', 'id' => $model->id));
+			}
 		}
 
 		$this->render('update',array(
@@ -117,11 +122,14 @@ class ProjectMemberController extends Controller
 			$this->loadModel($id)->delete();
 
 			// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-			if(!isset($_GET['ajax']))
-				$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			if( ! isset($_GET['ajax']) )
+			{
+			    $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			}
+		}else
+		{
+		    throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
 		}
-		else
-			throw new CHttpException(400,'Invalid request. Please do not repeat this request again.');
 	}
 
 	/**
@@ -129,9 +137,9 @@ class ProjectMemberController extends Controller
 	 */
 	public function actionIndex()
 	{
-	    $this->layout='//layouts/column1';
-	    $type      = Yii::app()->request->getParam('type');
+	    $this->layout = '//layouts/column1';
 	    
+	    $type      = Yii::app()->request->getParam('type');
 	    $projectid = Yii::app()->request->getParam('projectid');
 	    $eventid   = Yii::app()->request->getParam('eventid');
 	    $vacancyid = Yii::app()->request->getParam('vacancyid');
@@ -151,13 +159,16 @@ class ProjectMemberController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new ProjectMember('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['ProjectMember']))
-			$model->attributes=$_GET['ProjectMember'];
+		$model = new ProjectMember('search');
+		// clear any default values
+		$model->unsetAttributes();
+		if( isset($_GET['ProjectMember']) )
+		{
+		    $model->attributes = $_GET['ProjectMember'];
+		}
 
-		$this->render('admin',array(
-			'model'=>$model,
+		$this->render('admin', array(
+			'model' => $model,
 		));
 	}
 
@@ -168,9 +179,11 @@ class ProjectMemberController extends Controller
 	 */
 	public function loadModel($id)
 	{
-		$model=ProjectMember::model()->findByPk($id);
-		if($model===null)
-			throw new CHttpException(404,'The requested page does not exist.');
+		$model = ProjectMember::model()->findByPk($id);
+		if( $model === null )
+		{
+		    throw new CHttpException(404, 'The requested page does not exist.');
+		}
 		return $model;
 	}
 
@@ -180,7 +193,7 @@ class ProjectMemberController extends Controller
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='project-member-form')
+		if ( isset($_POST['ajax']) AND $_POST['ajax'] === 'project-member-form' )
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
@@ -200,7 +213,7 @@ class ProjectMemberController extends Controller
 	    $model = $this->loadModel($id);
 	    if ( ! $status = Yii::app()->request->getParam('status') )
 	    {
-	        throw new CHttpException(404,'Необходимо указать статус');
+	        throw new CHttpException(404, 'Необходимо указать статус');
 	    }
 	    $model->setStatus($status);
 	    
@@ -214,7 +227,7 @@ class ProjectMemberController extends Controller
 	
 	    $url = Yii::app()->createUrl('/admin/projectMember/index/', array(
 	        'projectid' => $model->vacancy->event->project->id,
-	        'type' => 'applications',
+	        'type'      => 'applications',
 	    ));
 	    $this->redirect($url);
 	}
