8238f76a4efde3471ee2b1dafdf91cf444be35b9
frost <frost@easycast.ru>
2014-08-26 21:23:07 +0400

    Исправлены id элемент ов для сворачивающегося виджета, добавлено отображение времени подачи заявки

--- protected/modules/admin/extensions/wizards/processor/MpMemberData/views/main.php
+++ protected/modules/admin/extensions/wizards/processor/MpMemberData/views/main.php
@@ -3,6 +3,7 @@
  * Полная разметка одной заявки
  */
 /* @var $this MpMemberData */
+
 $collapseExtraStyle    = '';
 $collapseSectionsStyle = '';
 if ( ! $this->collapseExtra )
@@ -13,18 +14,26 @@ if ( ! $this->collapseSections )
 {
     $collapseSectionsStyle = ' in';
 }
+$collapseContainerOptions = array(
+    'id' => 'collapse_accordion_wrapper_'.$this->id,
+);
 ?>
 <div id="<?= $this->wrapperId; ?>">
     <div class="row-fluid">
         <?php
         // сначала выводим свернутый блок с краткой информацией
         $this->render('_summary');
+        
+        echo 'Дата создания заявки: '.date('Y-m-d H:i', $this->member->timecreated);
         ?>
     </div>
-        <?php $collapse = $this->beginWidget('bootstrap.widgets.TbCollapse'); ?>
+    <?php
+    // виджет для сворачивающихся блоков 
+    //$collapse = $this->beginWidget('bootstrap.widgets.TbCollapse', $collapseContainerOptions);
+    ?>
     <div class="accordion-group">
         <div class="accordion-heading">
-            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse_extra_<?= $this->id; ?>">
+            <a class="accordion-toggle" data-toggle="collapse" href="#collapse_extra_<?= $this->id; ?>">
                 <b>Анкета участника</b>
             </a>
         </div>
@@ -39,7 +48,7 @@ if ( ! $this->collapseSections )
     </div>
     <div class="accordion-group">
         <div class="accordion-heading">
-            <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse_sections_<?= $this->id; ?>">
+            <a class="accordion-toggle" data-toggle="collapse" href="#collapse_sections_<?= $this->id; ?>">
                 <b>В каких разделах эта заявка?</b>
             </a>
         </div>
@@ -52,5 +61,8 @@ if ( ! $this->collapseSections )
             </div>
         </div>
     </div>
-    <?php $this->endWidget(); ?>
+    <?php
+    // конец виджета со сворачивающимися полями 
+    //$this->endWidget();
+    ?>
 </div>
