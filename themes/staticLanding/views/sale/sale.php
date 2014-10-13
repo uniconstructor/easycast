78be25d37d048bcff8f16c0449509ee044def65e
frost <frost@easycast.ru>
2014-10-13 23:05:46 +0400

    Исправлена проблема с modal-окном в коммерческом после обновления YiiBooster

--- themes/staticLanding/views/sale/sale.php
+++ themes/staticLanding/views/sale/sale.php
@@ -38,7 +38,7 @@ $serviceLinks['media_actors'] = Yii::app()->createAbsoluteUrl('//catalog/', arra
 $this->widget('ext.ECMarkup.ECFastOrder.ECFastOrder');
 // modal-окно с гарантиями
 // @todo разобраться с лишним вызовом endWinget()
-//$this->widget('ext.ECMarkup.ECIGuaranteeIt.ECIGuaranteeIt');
+$this->widget('ext.ECMarkup.ECIGuaranteeIt.ECIGuaranteeIt');
 ?>
 <div class="section_1" id="section1">
 	<div class="ec-wrapper add_padding">
@@ -144,11 +144,11 @@ $this->widget('ext.ECMarkup.ECFastOrder.ECFastOrder');
             </div>
 		</div>
 		<a name="reviews-section"></a>
-		<!--div class="row-fluid">
+		<div class="row-fluid">
 		    <div class="quality">
-                <a href="#IGuaranteeItModal" style="font-size: 28px;" data-toggle="modal">Почему именно мы&nbsp;?</a>
+                <a href="#IGuaranteeItModal" style="font-size:28px;" data-toggle="modal">Почему именно мы&nbsp;?</a>
             </div>
-		</div-->
+		</div>
 		<!-- отзывы -->
 		<h2 style="font-size:42px;text-align:center;text-transform:uppercase;margin-bottom:40px;font-weight:200;margin-top:50px;">Отзывы</h2>
     	<div class="container3">
