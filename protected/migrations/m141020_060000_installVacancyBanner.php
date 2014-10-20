e13e6e1ff3f5cee886f931dbb04819e2722da4cb
frost <frost@easycast.ru>
2014-10-20 23:09:42 +0400

    Исправлены комментарии

--- protected/migrations/m141020_060000_installVacancyBanner.php
+++ protected/migrations/m141020_060000_installVacancyBanner.php
@@ -10,7 +10,7 @@ class m141020_060000_installVacancyBanner extends EcMigration
         // добавляем настройку "баннер" к проекту
         $config = array(
             'name'         => 'banner',
-            'title'        => 'Баннер проекта',
+            'title'        => 'Баннер проекта на странице формой подачи заявки',
             'description'  => 'Рекомендуемый размер баннера 3200x482',
             'type'         => 'file',
             'minvalues'    => 0,
