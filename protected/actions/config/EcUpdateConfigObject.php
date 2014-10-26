52a8da546a9a6209e65cc6fb571af490ac11f7b1
frost <frost@easycast.ru>
2014-10-26 11:24:38 +0300

    Создано действие для редактирования модели настройки (поля не содержащие значения)

---
+++ protected/actions/config/EcUpdateConfigObject.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Действие контроллера для редактирования объекта настройки
+ * 
+ * @todo права доступа при редактировании системных настроек
+ */
+class EcUpdateConfigObject extends EcUpdateConfigValue
+{
+    /**
+     * @see EcUpdateConfigValue::run()
+     */
+    public function run()
+    {
+        // id настройки
+        $id      = Yii::app()->request->getParam('id');
+        // поле модели
+        $attribute = Yii::app()->request->getParam('attribute');
+        // обновляемое значение
+        $value     = Yii::app()->request->getParam('value');
+        // массив с обновляемыми значениями (если есть)
+        $modelData = Yii::app()->request->getPost('Config');
+        
+        if ( Yii::app()->getRequest()->isPostRequest )
+        {
+            // загружаем модель настройки
+            $config  = $this->loadConfigModel($id);
+            if ( is_array($modelData) AND ! empty($modelData) )
+            {
+                $model->attributes = $modelData;
+                $success = $model->save();
+            }else
+            {
+                $model->$attribute = $value;
+                $success = $model->save(false, array($attribute));
+            }
+        
+            if ( Yii::app()->getRequest()->isAjaxRequest )
+            {
+                echo $success ? $this->ajaxResponseOnSuccess : $this->ajaxResponseOnFailed;
+                exit(0);
+            }
+            if ( $this->redirectRoute !== null )
+            {
+                $this->getController()->redirect($this->redirectRoute);
+            }
+        }else
+        {
+            throw new CHttpException(Yii::t('zii', 'Invalid request'));
+        }
+    }
+}
