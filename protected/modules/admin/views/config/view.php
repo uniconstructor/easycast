6dc250ad974b373d55cf7e3ceb2ee568de2efa69
frost <frost@easycast.ru>
2014-10-28 07:47:03 +0300

    Добавлены заготовки страниц настроек системы

---
+++ protected/modules/admin/views/config/view.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Все настройки одной модели
+ */
+/* @var $this ConfigController */
+
+// верхняя навигация
+$this->breadcrumbs = array(
+    'Администрирование' => array('/admin'),
+    'Настройки'         => array('/admin/config'),
+);
+
+// общий список настроек
+$this->widget('bootstrap.widgets.TbNavbar', array(
+    'brand'    => 'Системные настройки',
+    'fixed'    => false,
+    'brandUrl' => array('/admin/config'),
+    'items' => array(
+        array(
+            'class' => 'bootstrap.widgets.TbMenu',
+            'items' => array(
+                array(
+                    'label' => 'Анкета',
+                    'url'   => array('/admin/config/view', 'type' => 'Questionary'),
+                ),
+                array(
+                    'label' => 'Проект',
+                    'url'   => array('/admin/config/view', 'type' => 'Project'),
+                ),
+                array(
+                    'label' => 'Мероприятие',
+                    'url'   => array('/admin/config/view', 'type' => 'ProjectEvent'),
+                ),
+                array(
+                    'label' => 'Роль',
+                    'url'   => array('/admin/config/view', 'type' => 'EventVacancy'),
+                ),
+            )
+        )
+    )
+));
+
+$this->widget('ext.EditableConfig.EditableConfig', array(
+    'objectType'      => $type,
+    'objectId'        => $id,
+    'createUrl'       => Yii::app()->createUrl('admin/config/createValue'),
+    'updateUrl'       => Yii::app()->createUrl('admin/config/updateValue'),
+    'deleteUrl'       => Yii::app()->createUrl('admin/config/deleteValue'),
+    'updateObjectUrl' => Yii::app()->createUrl('admin/config/updateConfig'),
+));
+?>
