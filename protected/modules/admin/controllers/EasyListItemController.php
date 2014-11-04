66a7e8caa71e38304d9d39871b0e11ddc471f31c
frost <frost@easycast.ru>
2014-11-04 05:29:04 +0300

    Добавлены методы для работы с элементами списков в общем виде.

--- protected/modules/admin/controllers/EasyListItemController.php
+++ protected/modules/admin/controllers/EasyListItemController.php
@@ -8,9 +8,58 @@ Yii::import('ext.EditableGrid.EditableGridController');
 class EasyListItemController extends EditableGridController
 {
     /**
-     * @var string - класс модели сложного значения
+     * @var string
      */
     protected $modelClass = 'EasyListItem';
+    
+    /**
+     * Preview для письма с оповещением
+     * 
+     * @return void
+     */
+    public function actionView()
+    {
+        $id = Yii::app()->request->getParam('id');
+        $item = $this->loadModel($id);
+        
+        $message = Yii::app()->curl->get($item->value);
+        
+        // @todo временно размещаем тут тест отправки оповещения
+        /* @var $s3 Aws\S3\S3Client */
+        $s3   = Yii::app()->getComponent('ecawsapi')->getS3();
+        $user = Yii::app()->getModule('user')->user();
+        
+        Yii::app()->getComponent('ecawsapi')->sendMail($user->email, $item->description, $message);
+        
+        echo $message;
+    }
+    
+    /**
+     * Создать запись
+     * 
+     * @return void
+     */
+    public function actionCreate()
+    {
+        // создаем модель для добавления
+        $instance = $this->initModel();
+        // ajax-проверка введенных данных
+        $this->performAjaxValidation($instance);
+    
+        if ( $instanceData = Yii::app()->request->getPost($this->modelClass) )
+        {// проверяем права на добавление данных
+            $this->checkAccess($instance);
+            $instance->attributes = $instanceData;
+            
+            if ( ! $instance->save() )
+            {
+                throw new CHttpException(500, 'Ошибка при сохранении данных');
+            }else
+            {
+                echo CJSON::encode($instance->getAttributes());
+            }
+        }
+    }
     
     /**
      * Обновить запись
@@ -28,7 +77,8 @@ class EasyListItemController extends EditableGridController
         $item = $this->loadModel($id);
         $this->checkAccess($item);
         // обновляем привязанный элемент
-        $item->updateProxy($field, $value);
+        //$item->updateProxy($field, $value);
+        $item->setAttribute($field, $value);
     
         if ( ! $item->save() )
         {// не удалось обновить запись в поле
