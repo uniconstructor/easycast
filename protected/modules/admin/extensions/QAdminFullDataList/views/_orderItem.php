e1a0da9b63e49b82eb27b616a170ebdb08063605
frost <frost@easycast.ru>
2014-11-09 18:59:56 +0300

    Стиль кодирования

--- protected/modules/admin/extensions/QAdminFullDataList/views/_orderItem.php
+++ protected/modules/admin/extensions/QAdminFullDataList/views/_orderItem.php
@@ -7,27 +7,27 @@
 
 ?>
 <noindex>
-<div class="well well-small row-fluid" style="border-radius:10px;" id="<?= $data['baseContainerId']; ?>">
-    <div class="row-fluid">
-        <div class="span2" style="vertical-align:middle;"><?= $data["avatar"]; ?></div>
-        <div class="span8" id="<?= $data['shortInfoContainerId']; ?>">
-            <h3><?= $data['fullName']; ?></h3>
-            <?= $data['bages']; ?>
-            <br><br>
-            <?= $data['fullInfoButton']; ?><?= $data['shortInfoButton']; ?>
+    <div class="well well-small row-fluid" style="border-radius:10px;" id="<?= $data['baseContainerId']; ?>">
+        <div class="row-fluid">
+            <div class="span2" style="vertical-align:middle;"><?= $data["avatar"]; ?></div>
+            <div class="span8" id="<?= $data['shortInfoContainerId']; ?>">
+                <h3><?= $data['fullName']; ?></h3>
+                <?= $data['bages']; ?>
+                <br><br>
+                <?= $data['fullInfoButton']; ?><?= $data['shortInfoButton']; ?>
+            </div>
+            <div class="span2">
+                <div id="<?= $data['messageContainerId']; ?>" style="display:none;"></div>
+                <div><?= $data['actionButtons']; ?></div>    
+            </div>
         </div>
-        <div class="span2">
-            <div id="<?= $data['messageContainerId']; ?>" style="display:none;"></div>
-            <div><?= $data['actionButtons']; ?></div>    
+        <div class="row-fluid">
+            <div id="<?= $data['fullInfoContainerId']; ?>"></div>
+            <input type="hidden" id="<?= $data['fullInfoLoadedId']; ?>" 
+                name="<?= $data['fullInfoLoadedId']; ?>_flag" value="0">
+        </div>
+        <div class="row-fluid">
+           <?php $this->widget('admin.extensions.ExtraFieldsList.ExtraFieldsList', array('member' => $data['member'])); ?>
         </div>
     </div>
-    <div class="row-fluid">
-        <div id="<?= $data['fullInfoContainerId']; ?>"></div>
-        <input type="hidden" id="<?= $data['fullInfoLoadedId']; ?>" 
-            name="<?= $data['fullInfoLoadedId']; ?>_flag" value="0">
-    </div>
-    <div class="row-fluid">
-       <?php $this->widget('admin.extensions.ExtraFieldsList.ExtraFieldsList', array('member' => $data['member'])); ?>
-    </div>
-</div>
 </noindex>
