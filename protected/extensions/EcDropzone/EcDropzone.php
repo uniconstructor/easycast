efa2308f4263cb6148c272271fdbb5c41dd10095
frost <frost@easycast.ru>
2014-11-21 14:33:25 +0300

    Добавлена заготовка для виджета загрузки файлов dropzone

---
+++ protected/extensions/EcDropzone/EcDropzone.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 
+ */
+class EcDropzone extends CWidget
+{
+    /**
+     * @var string
+     */
+    public $action;
+    /**
+     * @var string
+     */
+    public $htmlOptions = array();
+    
+    /**
+     * @see CWidget::init()
+     */
+    public function init()
+    {
+        
+    }
+    
+    /**
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+    
+    }
+}
