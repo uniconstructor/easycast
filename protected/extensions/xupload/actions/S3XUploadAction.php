572c179e08e372b4667cbf7807cb78fc001f63ed
frost <frost@easycast.ru>
2014-11-21 12:30:57 +0300

    Отлажен процесс загрузки и оцифровки видео

--- protected/extensions/xupload/actions/S3XUploadAction.php
+++ protected/extensions/xupload/actions/S3XUploadAction.php
@@ -21,40 +21,40 @@ class S3XUploadAction extends XUploadAction
     /**
      * XUploadForm (or subclass of it) to be used.  Defaults to XUploadForm
      * 
-     * @see XUploadAction::init()
-     * @var string
+     * @see   XUploadAction::init()
+     * @var   string
      * @since 0.5
      */
-    public $formClass = 'xupload.models.S3XUploadForm';
+    public $formClass            = 'xupload.models.S3XUploadForm';
     /**
      * Name of the model attribute referring to the uploaded file.
      * Defaults to 'file', the default value in XUploadForm
      * 
-     * @var string
+     * @var   string
      * @since 0.5
      */
-    public $fileAttribute = 'file';
+    public $fileAttribute        = 'file';
     /**
      * Name of the model attribute used to store mimeType information.
      * Defaults to 'mime_type', the default value in XUploadForm
      * 
-     * @var string
+     * @var   string
      * @since 0.5
      */
-    public $mimeTypeAttribute = 'mime_type';
+    public $mimeTypeAttribute    = 'mime_type';
     /**
      * Name of the model attribute used to store file size.
      * Defaults to 'size', the default value in XUploadForm
      * 
-     * @var string
+     * @var   string
      * @since 0.5
      */
-    public $sizeAttribute = 'size';
+    public $sizeAttribute        = 'size';
     /**
      * Name of the model attribute used to store the file's display name.
      * Defaults to 'name', the default value in XUploadForm
      * 
-     * @var string
+     * @var   string
      * @since 0.5
      */
     public $displayNameAttribute = 'name';
@@ -62,33 +62,33 @@ class S3XUploadAction extends XUploadAction
      * Name of the model attribute used to store the file filesystem name.
      * Defaults to 'filename', the default value in XUploadForm
      * 
-     * @var string
+     * @var   string
      * @since 0.5
      */
-    public $fileNameAttribute = 'filename';
+    public $fileNameAttribute    = 'filename';
     /**
      * The query string variable name where the subfolder name will be taken from.
      * If false, no subfolder will be used.
      * Defaults to null meaning the subfolder to be used will be the result of date("mdY").
      *
-     * @see XUploadAction::init().
-     * @var string
+     * @see   XUploadAction::init().
+     * @var   string
      * @since 0.2
      */
     public $subfolderVar;
     /**
      * Path of the main uploading folder.
      * 
-     * @see XUploadAction::init()
-     * @var string
+     * @see   XUploadAction::init()
+     * @var   string
      * @since 0.1
      */
     public $path;
     /**
      * Public path of the main uploading folder.
      * 
-     * @see XUploadAction::init()
-     * @var string
+     * @see   XUploadAction::init()
+     * @var   string
      * @since 0.1
      */
     public $publicPath;
@@ -101,16 +101,11 @@ class S3XUploadAction extends XUploadAction
     /**
      * Name of the state variable the file array is stored in
      * 
-     * @see XUploadAction::init()
-     * @var string
+     * @see   XUploadAction::init()
+     * @var   string
      * @since 0.5
      */
-    public $stateVariable = 'xuploadFiles';
-    // Amazon S3 settings
-    /**
-     * @var string path to the original S3 upload handler
-     */
-    //public $s3HandlerClass = 'application.components.aws.S3UploadHandler';
+    public $stateVariable  = 'xuploadFiles';
     /**
      * @var string
      */
@@ -118,26 +113,28 @@ class S3XUploadAction extends XUploadAction
     /**
      * @var string - тип объекта к которому привязывается видео
      */
-    public $objectType  = 'questionary';
+    public $objectType     = 'questionary';
     /**
      * @var string - url of the default icon for the uploaded fule
      */
-    public $defaultIconUrl = '/images/documents.svg';
+    public $defaultIconUrl = '/images/video_placeholder.svg';
     
     /**
-     * @var string
+     * @var string|int
      */
     protected $objectId = '0';
     
     /**
      * The resolved subfolder to upload the file to
-     * @var string
+     * 
+     * @var   string
      * @since 0.2
      */
     private $_subfolder = "";
     /**
      * The form model we'll be saving our files to
-     * @var CModel (or subclass)
+     * 
+     * @var   CModel (or subclass)
      * @since 0.5
      */
     private $_formModel;
@@ -176,7 +173,7 @@ class S3XUploadAction extends XUploadAction
         }
         if( ! $this->_formModel )
         {
-            $this->formModel = Yii::createComponent(array('class' => $this->formClass));
+            $this->formModel  = Yii::createComponent(array('class' => $this->formClass));
         }
         if ( $this->secureFileNames )
         {
@@ -191,11 +188,8 @@ class S3XUploadAction extends XUploadAction
      */
     protected function handleUploading()
     {
-        //CVarDumper::dump($_POST, 10, true);
-        //CVarDumper::dump($_FILES, 10, true);die;
-        //print_r($_POST);
-        //print_r($_FILES);die;
-        
+        /* @var $api EcAwsApi */
+        $api = Yii::app()->getComponent('ecawsapi');
         // определяем куда прикрепить загруженный файл
         $this->objectType = Yii::app()->request->getParam('objectType', $this->objectType);
         $this->objectId   = Yii::app()->request->getParam('objectId', $this->objectId);
@@ -221,15 +215,16 @@ class S3XUploadAction extends XUploadAction
             
             if ( $model->validate() )
             {// проверки формы прошли - сохраняем информацию о файле и возвращаем результат
-                /* @var $s3 Aws\S3\S3Client */
-                $s3 = Yii::app()->getComponent('ecawsapi')->getS3();
-                // создаем модель файла
-                $newFile     = new ExternalFile;
+                $videoBucket = $api->settings['transcoder']['defaultVideoBucket'];
                 $newFileData = array(
-                    'bucket'  => $this->bucket,
+                    'bucket'  => $videoBucket,
                     'path'    => $this->objectType.'/'.$this->objectId,
                     'oldname' => $model->{$this->displayNameAttribute},
                 );
+                // создаем модель файла
+                $newFile            = new ExternalFile;
+                $newFile->storage   = 's3';
+                $newFile->bucket    = $videoBucket;
                 // объясняем модели откуда брать данные файла
                 $newFile->setInputModel($this->formModel);
                 $newFile->setInputName($this->fileAttribute);
@@ -238,27 +233,46 @@ class S3XUploadAction extends XUploadAction
                 $newFile->saveFile();
                 
                 if ( $newFile->save() )
-                {// @todo запускаем оцифровку видео
-                    $video = new Video();
+                {// сохраняем информацию о видео
+                    $video             = new Video();
                     $video->objecttype = $this->objectType;
                     $video->objectid   = $this->objectId;
-                    $video->externalid = $newFile->path.'/'.$newFile->name; // $newFile->path ?
+                    $video->externalid = $newFile->path.'/'.$newFile->name;
                     $video->type       = 'file';
                     $video->size       = $newFile->size;
                     $video->name       = $newFile->oldname;
-                    $video->link       = $s3->getObjectUrl(Yii::app()->params['AWSVideoBucket'], $newFile->path.'/'.$newFile->name);
+                    $video->link       = $api->s3->getObjectUrl(Yii::app()->params['AWSVideoBucket'], $newFile->path.'/'.$newFile->name);
                     $video->visible    = 0;
-                    $video->save();
+                    if ( $video->save() )
+                    {// сохраняем видео и запускаем оцифровку
+                        if ( Yii::app()->user->checkAccess('Admin') )
+                        {// автоматически помечаем видео как проверенное если его загрузил администратор
+                            $video->setStatus(swVideo::APPROVED);
+                        }
+                        if ( ! $video->transcode() )
+                        {// не удалось создать задачу оцифровки
+                            echo json_encode(array(array("error" => "Не удалось создать задачу оцифровки видео")));
+                            // удаляем загруженный файл
+                            // @todo сохранять загруженный файл и предлагать заново запустить задачу оцифровки
+                            $newFile->delete();
+                            return;
+                        }
+                    }else
+                    {// не удалось сохранить видео
+                        $errors = implode(', ', current($video->getErrors()));
+                        echo json_encode(array(array("error" => $errors)));
+                        Yii::log(get_class($this).": ".$errors, CLogger::LEVEL_ERROR, "xupload.actions.".get_class($this));
+                        return;
+                    }
                 }else
-                {
+                {// не удалось сохранить модель файла
                     $errors = implode(', ', current($newFile->getErrors()));
                     echo json_encode(array(array("error" => $errors)));
                     Yii::log(get_class($this).": ".$errors, CLogger::LEVEL_ERROR, "xupload.actions.".get_class($this));
                     return;
                 }
-                //$video = $this->saveVideoData($model, $this->objectId, $uploadResult);
                 
-                // выводим JSON с результатами сохранения
+                // отправляем JSON с результатами сохранения
                 echo json_encode(array(
                     array(
                         "name" => strip_tags($model->{$this->displayNameAttribute}),
@@ -267,10 +281,10 @@ class S3XUploadAction extends XUploadAction
                         "url"  => $video->link,
                         // @todo preview-иконка в зависимости от загруженного файла
                         "thumbnail_url" => $this->defaultIconUrl,
-                        "delete_url"    => '#',/*$this->getController()->createUrl($this->getId(), array(
+                        "delete_url"    => $this->getController()->createUrl($this->getId(), array(
                             "_method" => "delete",
-                            "file"    => $model->{$this->fileNameAttribute},
-                        )),*/
+                            "id"      => $video->id,
+                        )),
                         "delete_type" => "POST",
                     ),
                 ));
@@ -291,26 +305,13 @@ class S3XUploadAction extends XUploadAction
      */
     protected function handleDeleting()
     {
-        if ( isset($_GET["_method"]) AND $_GET["_method"] === "delete" )
+        $method  = Yii::app()->request->getParam('_method');
+        $id      = Yii::app()->request->getParam('id', 0);
+        
+        if ( $method === "delete" AND $video = Video::model()->findByPk($id) )
         {
             $success = false;
-            if ( $_GET["file"][0] !== '.' AND Yii::app()->user->hasState($this->stateVariable) )
-            {
-                // pull our userFiles array out of state and only allow them to delete
-                // files from within that array
-                $userFiles = Yii::app()->user->getState($this->stateVariable, array());
-    
-                if ( $this->fileExists($userFiles[$_GET["file"]]) )
-                {
-                    $success = $this->deleteFile($userFiles[$_GET["file"]]);
-                    if ( $success )
-                    {
-                        unset($userFiles[$_GET["file"]]);
-                        // remove it from our session and save that info
-                        Yii::app()->user->setState($this->stateVariable, $userFiles);
-                    }
-                }
-            }
+            
             echo json_encode($success);
             return true;
         }
@@ -321,7 +322,7 @@ class S3XUploadAction extends XUploadAction
      * We store info in session to make sure we only delete files we intended to
      * Other code can override this though to do other things with state, thumbnail generation, etc.
      * 
-     * @since 0.5
+     * @since  0.5
      * @author acorncom
      * @return boolean|string Returns a boolean unless there is an error, in which case it returns the error message
      */
@@ -330,11 +331,10 @@ class S3XUploadAction extends XUploadAction
         $path      = $this->getPath();
         // Now we need to save our file info to the user's session
         $userFiles = Yii::app()->user->getState($this->stateVariable, array());
-    
+        
         $userFiles[$this->formModel->{$this->fileNameAttribute}] = array(
             "path"     => $path.$this->formModel->{$this->fileNameAttribute},
-            // @todo иконка для предпросмотра файла
-            //"thumb"    => $path.$this->formModel->{$this->fileNameAttribute},
+            // @todo взять иконку для предпросмотра файла из анкеты или оригинала видео
             "thumb"    => $this->defaultIconUrl,
             "filename" => $this->formModel->{$this->fileNameAttribute},
             'size'     => $this->formModel->{$this->sizeAttribute},
@@ -347,34 +347,7 @@ class S3XUploadAction extends XUploadAction
     }
     
     /**
-     * Сохранить запись видео в БД если файл был успешно загружен
-     * 
-     * @param  S3XUploadForm $xUploadForm
-     * @param  int $objectId
-     * @param  array $uploadResult
-     * @return Video
-     */
-    /*protected function saveVideoData($xUploadForm, $objectId, $uploadResult)
-    {
-        $newFileName = $uploadResult[get_class($xUploadForm)][0]->name;
-        $externalId  = $this->objectType.'/'.$objectId.'/'.$newFileName;
-        
-        $video             = new Video;
-        $video->objecttype = $this->objectType;
-        $video->objectid   = $objectId;
-        $video->externalid = $externalId;
-        $video->type       = 'file';
-        $video->size       = $uploadResult[get_class($xUploadForm)][0]->size;
-        $video->name       = $xUploadForm->{$this->displayNameAttribute};
-        $video->link       = $this->createPreSignedUrl($objectId, $newFileName);
-        $video->visible    = 0;
-        $video->save();
-        
-        return $video;
-    }*/
-    
-    /**
-     * Создать постоянный или временный url для просмотра видео
+     * Создать постоянный или подписанный токеном url для просмотра видео
      * 
      * @param  int        $objectId
      * @param  string     $newFileName
@@ -388,4 +361,15 @@ class S3XUploadAction extends XUploadAction
         // получаем ссылку на скачивание
         return $s3->getObjectUrl($this->bucket, $this->objectType.'/'.$objectId.'/'.$newFileName, $expires);
     }
+    
+    /**
+     * Получить URL для удаления загруженного видео
+     * 
+     * @param  Video $video
+     * @return string
+     */
+    /*protected function createDeleteUrl($video)
+    {
+        
+    }*/
 }
