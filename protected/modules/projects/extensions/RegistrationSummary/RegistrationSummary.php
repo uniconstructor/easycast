d974c78faba35ad0850dd359119200ea08d912e5
frost <frost@easycast.ru>
2014-11-21 12:38:44 +0300

    Добавлена заготовка для виджета краткой информации о роли, мероприятии и проекте (будет использован на странице регистрации на роль)

---
+++ protected/modules/projects/extensions/RegistrationSummary/RegistrationSummary.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Краткая информация о проекте, событии и роли для вывода перед динамической формой регистрации
+ */
+class RegistrationSummary extends CWidget
+{
+    /**
+     * @var EventVacancy
+     */
+    public $vacancy;
+    
+    /**
+     * @see CWidget::run()
+     */
+    public function run()
+    {
+        
+    }
+}
---
+++ protected/modules/projects/extensions/RegistrationSummary/views/summary.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Разметка краткой информации о проекте и роли перед формой регистрации
+ */
+/* @var $this RegistrationSummary */
+?>
+<div class="row-fluid">
+    <div class="span4">
+        
+    </div>
+    <div class="span4">
+        
+    </div>
+    <div class="span4">
+        
+    </div>
+</div>
