a09af1b9f210d480f355a42ac84fad0cdb4e63bd
frost <frost@easycast.ru>
2014-11-21 13:24:55 +0300

    Документированы параметры конфигурации production-версии

--- protected/config/production.php
+++ protected/config/production.php
@@ -7,49 +7,47 @@ return CMap::mergeArray(
             // данные для подключения к серверу БД (используется amazon RDS)
             // серверные переменные заданы в параметрах Elastic Beanstalk
             // чтобы увидеть или изменить их - нужно зайти в контрольную панель Амазоновского хостинга
-            // https://console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1
+            // @see https://console.aws.amazon.com/elasticbeanstalk/home?region=us-east-1
             // выбрать среду (enviroment) в которой работает сайт и изменить ее настройки
             'db' => array(
                 'connectionString' => 'mysql:host='.$_SERVER['RDS_ENDPOINT'].';dbname=easycast',
                 'username'         => $_SERVER['RDS_LOGIN'],
 			    'password'         => $_SERVER['RDS_PASS'],
             ),
-            
+            // настройки CWebUser
             'user' => array(
                 // делаем так чтобы авторизация по cookie работала на easycast.ru и www.easycast.ru
                 'identityCookie' => array('domain' => '.easycast.ru'),
             ),
-            
+            // работа с сессией
             'session' => array(
                 // делаем так чтобы авторизация по cookie работала на easycast.ru и www.easycast.ru
-                'cookieParams' => array('domain' => '.easycast.ru'),
+                'cookieParams'           => array('domain' => '.easycast.ru'),
+                'autoCreateSessionTable' => false,
             ),
-            
+            // логи и трассировка
+            // @todo хранить логи production-сервера отдельно в NoSQL хранилище
             'log' => array(
                 'class'  => 'CLogRouter',
                 'routes' => array(
-                    array(
+                    'CDbLogRoute' => array(
                         'class'              => 'CDbLogRoute',
-                        // @todo хранить логи отдельно ото всех остальных данных для лучшей безопасности
                         'connectionID'       => 'db',
                         'levels'             => 'error, warning',
-                        // не храним логи о 404 страницах - и логи simpleWorkflow
                         'except'             => 'exception.CHttpException.404, simpleWorkflow',
                         'autoCreateLogTable' => false,
                     ),
                 ),
             ),
         ),
-        
+        // другие параметры приложения
         'params' => array(
             // API ID на vkontakte.ru (чтобы работал виджет "мне нравится")
             'vkontakteApiId' => '3534064',
-            
             // Данные для доступа к кластеру Amazon
             // @todo убрать из кода, использовать серверные переменные, как с паролем в БД
             'AWSAccessKey' => 'AKIAISQJ47JQQ2QOGBKA',
             'AWSSecret'    => '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',
-            
             // S3
             // Использовать хостинг amazon s3 для хранения картинок
             'useAmazonS3'    => true,
@@ -58,20 +56,17 @@ return CMap::mergeArray(
             'AWSVideoBucket' => 'video.easycast.ru',
             // SES
             // использовать сервисы amazon SES для отправки почты
-            'useAmazonSES'  => true,
+            'useAmazonSES'   => true,
             // SQS
             // использовать Amazon SQS для отправки большого количества почты (через очередь)
             'useAmazonSQS'      => true,
             'AWSEmailQueueUrl'  => 'https://sqs.us-east-1.amazonaws.com/507109426938/easycast_mail',
             'AWSEmailQueueName' => 'easycast_mail',
-            
             // Для отладки: отсылать ли вообще хоть какие-нибудь сообщения, даже на тестовые адреса?
-            'AWSSendMessages' => true,
-            
+            'AWSSendMessages'   => true,
             // использовать ли прокси сервера google для отображения картинок в письмах
             // (должно быть включено на production и выключено на машине разработчика)
             'useGoogleImageProxy' => true,
-            
             // cron: на production-системе обязательно включен
             'useCron' => true,
         ),
