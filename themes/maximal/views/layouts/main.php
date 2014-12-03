6b3b8e48d3b433db74fb28dbde8c2778466afd93
frost <frost@easycast.ru>
2014-12-03 03:10:46 +0300

    Подклюдчены css и js jgrowl-оповещений

--- themes/maximal/views/layouts/main.php
+++ themes/maximal/views/layouts/main.php
@@ -29,7 +29,7 @@ Yii::app()->clientScript->registerCoreScript('jquery');
     <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/css/main.css" rel="stylesheet" type='text/css'>
     <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" type='text/css'>
     <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/css/responsive.css" rel="stylesheet" type="text/css">
-    <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/css/settings.css" rel="stylesheet" type="text/css">
+    <!--link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/css/settings.css" rel="stylesheet" type="text/css"-->
     <!-- Dropzone.js -->
     <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/dropzone/css/basic.css" rel="stylesheet" type="text/css">
     <link href="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/dropzone/css/dropzone.css" rel="stylesheet" type="text/css">
@@ -40,6 +40,8 @@ Yii::app()->clientScript->registerCoreScript('jquery');
     <!-- modernizr lib -->
     <script src="<?= Yii::app()->baseUrl; ?>/js/modernizr.custom.min.js" type="text/javascript"></script>
     <script src="<?= Yii::app()->baseUrl; ?>/js/yiiFormExtensions.js" type="text/javascript"></script>
+    <!-- jGrowl notifications -->
+    <link href="<?= Yii::app()->baseUrl; ?>/css/jquery.jgrowl.min.css" rel="stylesheet" type='text/css'>
     
     <!--script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script-->
     <!--script type="text/javascript">stLight.options({publisher: "9144efb6-c5a7-4360-9b70-24e468be66c3", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script-->
@@ -64,9 +66,12 @@ Yii::app()->clientScript->registerCoreScript('jquery');
     <script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/placeholder.js" type="text/javascript"></script>
     <script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/plugins.js" type="text/javascript"></script>
     <!-- script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/theme.js" type="text/javascript"></script -->
-    <script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script> <!-- Revolution Slider -->
-    <script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/pluginsources/jquery.themepunch.plugins.min.js" type="text/javascript"></script> <!-- Revolution Slider -->
-    <!-- script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/jquery.flexslider-min.js" type="text/javascript"></script -->
-    
+    <!-- Revolution Slider -->
+    <!--script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/js/jquery.themepunch.revolution.min.js" type="text/javascript"></script-->
+    <!-- script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/rs-plugin/pluginsources/jquery.themepunch.plugins.min.js" type="text/javascript"></script-->
+    <!-- Flex Slider -->
+    <script src="<?= Yii::app()->theme->baseUrl; ?>/bootstrap/js/jquery.flexslider-min.js" type="text/javascript"></script>
+    <!-- jGrowl notifications -->
+    <script src="<?= Yii::app()->baseUrl; ?>/js/jquery.jgrowl.min.js" type="text/javascript"></script>
 </body>
 </html>
