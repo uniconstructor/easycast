e5f071980db7443791756382435f051ec1b2fb4b
frost <frost@easycast.ru>
2014-12-04 18:31:50 +0300

    Обновлены настройки сборки разработчика

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
 <?php
-// EasyCast development index
+// EasyCast dev index
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/dev.php';
+$yii    = dirname(__FILE__).'/framework/yiilite.php';
+$config = dirname(__FILE__).'/protected/config/dev.php';
 
-$sapi = php_sapi_name();
-if ( $sapi != 'cli' )
-{// maintaince mode if needed
-    //header('Location: http://easycast.ru/maintaince.html');
-    //die('maintance');
-}
 
 define('YII_EASYCAST_CONFIG_DB_CONNECTION_STRING', 'mysql:host=localhost;dbname=easycast');
 define('YII_EASYCAST_CONFIG_DB_LOGIN', 'root');
 define('YII_EASYCAST_CONFIG_DB_PASSWORD', 'root');
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG', true);
 // specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
--- protected/config/dev.php
+++ protected/config/dev.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Конфигурация версии разработчика
+ */
 return CMap::mergeArray(
     require(dirname(__FILE__).'/main.php'),
     array(
@@ -8,95 +11,84 @@ return CMap::mergeArray(
             'gii' => array(
                 'class'     => 'system.gii.GiiModule',
                 'password'  => '12345',
-                'ipFilters' => array('127.0.0.1','::1'),
+                'ipFilters' => array('127.0.0.1', '::1'),
                 'generatorPaths' => array(
                     'application.gii',
                     'bootstrap.gii',
                     'ext.simpleWorkflow.command.gii',
                 ),
             ),
-            'log' => array(
-                'routes' => array(
-                    array(
-                        'class'     => 'ext.yii-debug-toolbar.YiiDebugToolbarRoute',
-                        'ipFilters' => array('127.0.0.1'),
-                    ),
-                    array(
-                        'class'              => 'CDbLogRoute',
-                        // @todo хранить логи отдельно ото всех остальных данных для лучшей безопасности
-                        'connectionID'       => 'db',
-                        //'levels'             => 'error, warning, info, trace',
-                        // не храним логи о 404 страницах - и логи simpleWorkflow
-                        'except'             => 'CHttpException.404, simpleWorkflow',
-                        'autoCreateLogTable' => false,
-                    ),
-                ),
-            ),
-            // бонус: мой маленький робот для торговли на бирже :)
-            // Он вообще к сайту никак не относится, просто воткнуть его сюда было быстрее чем
-            // создавать новый проект :)
-            'trader' => array(
-                'class' => 'application.modules.trader.TraderModule',
-            ),
         ),
-
         'components' => array(
             // настройки соединения с БД (для среды разработчика)
             'db' => array(
                 // данные для работы сайта локально (для разработки)
-                'connectionString' => 'mysql:host=localhost;dbname=easycast',
-                'username'         => 'root',
-                'password'         => 'root',
+                'connectionString'   => 'mysql:host=localhost;dbname=easycast',
+                'username'           => 'root',
+                'password'           => 'root',
+                'enableProfiling'    => false,
+                'enableParamLogging' => false,
             ),
+            // сессия пользователя
             'session' => array(
-                // на dev-машине постоянно разворачиваются новые дампы, таблицу логов каждый раз выкачивать
-                // долго, поэтому в дампах она как правило не присутствует,
-                // поэтому автоматическое создание включено
-                'autoCreateSessionTable' => true,
+                'autoCreateSessionTable' => false,
+            ),
+            // трассировка и логи
+            'log' => array(
+                'class'  => 'CLogRouter',
+                'routes' => array(
+                    // виджет разработчика для отладки приложения
+                    // (папка с виджетом должна обязательно называться yii-debug-toolbar)
+                    /*'YiiDebugToolbarRoute' => array(
+                        'class' => 'application.extensions.yii-debug-toolbar.YiiDebugToolbarRoute',
+                        'panels' => array(
+                            'YiiDebugToolbarPanelRequest',
+                            'YiiDebugToolbarPanelSettings',
+                            'YiiDebugToolbarPanelViews',
+                            'YiiDebugToolbarPanelSql',
+                            'YiiDebugToolbarPanelLogging',
+                        ),
+                    ),*/
+                    'CDbLogRoute' => array(
+                        'class'              => 'CDbLogRoute',
+                        'connectionID'       => 'db',
+                        'autoCreateLogTable' => false,
+                    ),
+                ),
             ),
         ),
-        
         // другие параметры приложения
         'params' => array(
             // API ID на vkontakte.ru (чтобы работал виджет "мне нравится")
             'vkontakteApiId' => '3534064',
-            
             // Настройки хостинга Amazon
             // Данные для доступа к кластеру Amazon (можно отключить, чтобы не посылать запросы с машины разработчика)
             'AWSAccessKey' => 'AKIAISQJ47JQQ2QOGBKA',
             'AWSSecret'    => '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',
-            //'AWSAccessKey' => '',
-            //'AWSSecret'    => '',
-            
             // S3
             // Использовать хостинг amazon s3 для хранения картинок
             'useAmazonS3'    => true,
             'AWSBucket'      => 'test.easycast.ru',
-            'AWSBucketPath' => 'http://bglance',
+            //'AWSBucketPath' => 'http://bglance',
             'AWSBucketPath'  => 'https://img.easycast.ru.s3.amazonaws.com',
             //'AWSBucketPath'  => 'http://test.easycast.ru.s3.amazonaws.com',
             'AWSVideoBucket' => 'video.easycast.ru',
-            
             // SES
             // использовать сервис Amazon SES для отправки почты на реальные адреса
             // должно быть выключено в этом файле (dev.php)
             // включается только на production
             // в положении false все письма уходят только на один тестовый адрес
             'useAmazonSES'  => false,
-            
             // SQS
             // использовать Amazon SQS для отправки большого количества почты (через очередь)
             'useAmazonSQS'      => false,
             'AWSEmailQueueUrl'  => 'https://sqs.us-east-1.amazonaws.com/507109426938/test_easycast_mail',
             'AWSEmailQueueName' => 'test_easycast_mail',
-            
             // Для отладки: отсылать ли вообще хоть какие-нибудь сообщения, даже на тестовые адреса?
             'AWSSendMessages' => true,
-            
             // использовать ли прокси сервера google для отображения картинок в письмах
             // (должно быть включено на production и выключено на машине разработчика)
             'useGoogleImageProxy' => false,
-            
             // cron: на dev-системе сильно замедляет работу, поэтому отключен
             // включается только для целей тестирования
             'useCron' => false,
