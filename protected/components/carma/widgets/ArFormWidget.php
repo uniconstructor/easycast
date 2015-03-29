f0fd37c87d0b3db326a40f252c4816bf14c42477
Smirnov Ilia <frost@easycast.ru>
2015-03-30 00:12:05 +0300

    Добавлены виджеты для ручного управления разметкой страниц и виджетов

---
+++ protected/components/carma/widgets/ArFormWidget.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Виджет для сбора и отрисовки формы 
+ */
+class ArFormWidget extends CWidget
+{
+    /**
+     * @var ArForm|CustomActiveRecord
+     */
+    public $arForm;
+    
+    /**
+     * @see parent::init()
+     */
+    public function init()
+    {
+        parent::init();
+    }
+    
+    /**
+     * @see parent::run()
+     */
+    public function run()
+    {
+        
+    }
+}
---
+++ protected/components/carma/widgets/ArLayoutWidget.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * 
+ */
+class ArLayoutWidget extends CWidget
+{
+    /**
+     * @var ArLayout|CustomActiveRecord
+     */
+    public $arLayout;
+    
+    /**
+     * @see parent::init()
+     */
+    public function init()
+    {
+        parent::init();
+    }
+    
+    /**
+     * @see parent::run()
+     */
+    public function run()
+    {
+        
+    }
+}
