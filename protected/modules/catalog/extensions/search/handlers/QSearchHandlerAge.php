af551188c563b711c5f047dc5321fd199ee45958
Smirnov Ilia <frost@easycast.ru>
2015-04-10 11:08:06 +0300

    Оптимизирован поиск по возрасту

--- protected/modules/catalog/extensions/search/handlers/QSearchHandlerAge.php
+++ protected/modules/catalog/extensions/search/handlers/QSearchHandlerAge.php
@@ -14,30 +14,18 @@ class QSearchHandlerAge extends QSearchHandlerBase
     {
         $data     = $this->getFilterData();
         $criteria = new CDbCriteria();
-        $alias    = Questionary::model()->getTableAlias(true);
         
-        $minTimestamp = null;
-        $maxTimestamp = null;
-        
+        $alias = Questionary::model()->getTableAlias(true);
         if ( isset($data['minage']) AND $minAge = intval($data['minage']) )
         {// минимальный возраст
             $minTimestamp = time() - ( $minAge * 365 * 24 * 3600 );
+            $criteria->compare($alias.'.`birthdate`', '<='.$minTimestamp);
         }
         if ( isset($data['maxage']) AND $maxAge = intval($data['maxage']) )
         {// максимальный возраст: прибавляем единицу к указанному в форме,
             // чтобы найти всех участников до указанного возраста включительно
             $maxTimestamp = time() - ( (1 + $maxAge) * 365 * 24 * 3600 );
-        }
-        // условие
-        if ( $minTimestamp AND ! $maxTimestamp )
-        {
-            $criteria->compare($alias.'.`birthdate`', '<='.$minTimestamp);
-        }elseif ( $maxTimestamp AND ! $minTimestamp )
-        {
             $criteria->compare($alias.'.`birthdate`', '>='.$maxTimestamp);
-        }elseif ( $minTimestamp AND $maxTimestamp )
-        {
-            $criteria->addBetweenCondition($alias.'.`birthdate`', $minTimestamp, $maxTimestamp);
         }
         return $criteria;
     }
