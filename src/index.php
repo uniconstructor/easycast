f849334cf4c86cece87b3b8d854ad16ff1dbd3f6
Smirnov Ilia <frost@easycast.ru>
2015-05-21 18:42:59 +0300

    Исправлен и протестирован новый входной скрипт

--- src/index.php
+++ src/index.php
@@ -1,29 +1,32 @@
 <?php
 // EasyCast development version index
-// 
+
+// параметры соединения с БД
+define('YII_EASYCAST_CONFIG_DB_CONNECTION_STRING', 'mysql:host=localhost;dbname=easycast');
+define('YII_EASYCAST_CONFIG_DB_LOGIN', 'root');
+define('YII_EASYCAST_CONFIG_DB_PASSWORD', 'root');
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
+// specify enfiroment
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
 // install Composer autoloader
 require(__DIR__ . '/protected/vendor/autoload.php');
 
 // путь к основному классу Yii объединяющий первую и вторую версии
 require(__DIR__ . '/protected/components/Yii.php');
 
-define('YII_EASYCAST_CONFIG_DB_CONNECTION_STRING', 'mysql:host=localhost;dbname=easycast');
-define('YII_EASYCAST_CONFIG_DB_LOGIN', 'root');
-define('YII_EASYCAST_CONFIG_DB_PASSWORD', 'root');
 // задаем временную зону вручную - новая версия PHP может некорректно работать с настройкой php.ini
 date_default_timezone_set('Europe/Moscow');
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
-
 // путь к конфигурации Yii 2.x
 $yii2Config = require(__DIR__ . '/protected/config/yii2/dev.php');
-//print_r($yii2Config);die;
 new yii\web\Application($yii2Config); // Do NOT call run()
 
-set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext){
+/*set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext){
     $info = array(
         'errno' => $errno,
         'errstr' => $errstr,
@@ -32,7 +35,7 @@ set_error_handler(function($errno, $errstr, $errfile, $errline, $errcontext){
         'errcontext' => $errcontext,
     );
     throw new Exception(print_r($info, true));
-});
+});*/
 
 // путь к конфигурации Yii 1.x
 $yii1Config = __DIR__ . '/protected/config/dev.php';
