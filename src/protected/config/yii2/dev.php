a83499e7b46d6174884b6f15fa9a303f7a24f694
Smirnov Ilia <frost@easycast.ru>
2015-05-21 18:42:34 +0300

    Обновлена конфигурация Yii2

--- src/protected/config/yii2/dev.php
+++ src/protected/config/yii2/dev.php
@@ -1,14 +1,33 @@
 <?php
 
 return [
-    'id' => 'easyCast',
-    'basePath' => dirname(dirname(__DIR__)),
+    'id'   => 'easyCast',
+    'name' => 'easyCast',
+    'basePath' => dirname(dirname(dirname(__DIR__))),
+    'aliases' => [
+        '@protected' => '@app/protected',
+        '@vendor'    => '@protected/vendor',
+        '@runtime'   => '@protected/runtime',
+        '@data'      => '@protected/data',
+        '@cockpit'   => '@protected/vendor/aheinze/cockpit',
+        '@assets'    => '@app/assets',
+    ],
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=easycast',
             'username' => 'root',
             'password' => 'root',
+        ],
+        'cockpit' => [
+            'class' => 'omnilight\cockpit\Cockpit',
+            'cockpitConfigFile'  => '@runtime/cockpit/config.php',
+            'cockpitStoragePath' => '@protected/data/cockpit',
+            'config' => [
+                'sec-key'  => 'c2109878c-009a-2787-ac66-ab558e8a15e5e1',
+                'i18n'     => 'en',
+                'app.name' => 'easyCast',
+            ],
         ],
     ],
 ];
