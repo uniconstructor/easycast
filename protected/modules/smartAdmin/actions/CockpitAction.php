29cd6fc61c06a9f1ef49e50dcd73b98ea4c3d7df
Smirnov Ilia <frost@easycast.ru>
2015-05-21 18:24:58 +0300

    Убран устаревший код

--- protected/modules/smartAdmin/actions/CockpitAction.php
+++ protected/modules/smartAdmin/actions/CockpitAction.php
@@ -10,14 +10,6 @@ class CockpitAction extends AjaxAction
      */
     public function run()
     {
-        define('COCKPIT_ADMIN', 1);
-        define('COCKPIT_ADMIN_ROUTE', '/smartAdmin/console/cockpit');
-        set_include_path(get_include_path().PATH_SEPARATOR.Yii::app()->basePath.'/../cockpit/vendor');
-        include(Yii::app()->basePath.'/../cockpit/bootstrap.php');
-        //$this->controller->renderPartial('index');
-        //$this->controller->renderPartial('cockpit');
-        //$this->controller->render('cockpit');
-        // run backend
-        $cockpit->set('route', COCKPIT_ADMIN_ROUTE)->trigger("admin.init")->run();
+        
     }
 }
