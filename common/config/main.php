b3fa4773fdbe3f2c07400321a98d6d4b54d7bb6a
Smirnov Ilia <frost@easycast.ru>
2015-06-27 02:42:39 +0300

    Добавлен модуль управления пользователями и модуль отправки писем

--- common/config/main.php
+++ common/config/main.php
@@ -12,20 +12,40 @@ return [
         '@cockpit'   => '@vendor/aheinze/cockpit',
     ],
     'extensions' => $extensions,
+    'modules' => [
+        // GUI для управления пользователями
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => true,
+        ],
+    ],
     'components' => [
         'cache' => [
-            'class' => 'yii\caching\DummyCache',
+            'class' => 'yii\mongodb\Cache',
+        ],
+        'session' => [
+            'class' => 'yii\mongodb\Session',
+        ],
+        'formatter' => [
+            'dateFormat'        => 'dd.MM.yyyy',
+            'datetimeFormat'    => 'HH:mm:ss dd.MM.yyyy',
+            'decimalSeparator'  => ',',
+            'thousandSeparator' => ' ',
+            'currencyCode'      => 'RUR',
+            'locale'            => 'ru-RU',
         ],
         'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
+            'enablePrettyUrl'     => true,
+            'showScriptName'      => false,
             'enableStrictParsing' => false,
             'rules' => [
-                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
+                //'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
             ],
         ],
         'assetManager' => [
+            // создаем символические ссылки на assets вместо копирования
+            'linkAssets' => true,
             'bundles' => [
                 'yii\bootstrap\BootstrapAsset' => [
                     'jsOptions' => [
@@ -44,8 +64,12 @@ return [
                 ],
             ],
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+        ],
     ],
     'controllerMap' => [
-        'page' => 'common\components\PageController',
+        'page'            => 'common\components\PageController',
+        'mongodb-migrate' => 'yii\mongodb\console\controllers\MigrateController',
     ],
 ];
