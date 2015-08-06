fd353b7e56fe59c569fa675e1c970d3836bfc04c
Smirnov Ilia <frost@easycast.ru>
2015-08-06 23:58:29 +0300

    Изменены адреса mysql и mongodb для локальной версии сайта.

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,21 +1,22 @@
 <?php
+
 return [
     'components' => [
-        'olddb' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=easycast',
-            'username' => 'root',
-            'password' => 'root',
-            'charset' => 'utf8',
+        'olddb'   => [
+            'class'       => 'yii\db\Connection',
+            'dsn'         => 'mysql:host=mysql.dev;dbname=easycast',
+            'username'    => 'root',
+            'password'    => 'root',
+            'charset'     => 'utf8',
             'tablePrefix' => 'bgl_',
         ],
         'mongodb' => [
             'class' => '\yii\mongodb\Connection',
-            'dsn'   => 'mongodb://localhost:27017/easycast',
+            'dsn'   => 'mongodb://mongodb.dev:27017/easycast',
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
+        'mailer'  => [
+            'class'            => 'yii\swiftmailer\Mailer',
+            'viewPath'         => '@common/mail',
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
