d1015047288f204ddfd04c83bf312ffdcacb857c
Smirnov Ilia <frost@easycast.ru>
2015-10-21 21:58:21 +0300

    Исправлено отображение баннера проекта

--- protected/modules/projects/views/vacancy/registration.php
+++ protected/modules/projects/views/vacancy/registration.php
@@ -4,45 +4,35 @@
  */
 /* @var $this  VacancyController */
 /* @var $model QDynamicFormModel */
+/** @var $project Project */
+$project = $model->vacancy->event->project;
 
 // заголовок страницы для поисковой индексации и публикации в соцсетях
 // @todo брать название из настроек роли
-$this->pageTitle = "Регистрация для участия в проекте ".$model->vacancy->event->project->name;
-
+$this->pageTitle = "Регистрация для участия в проекте ".strip_tags($project->name);
 // Описание страницы для поисковиков
 // @todo брать описание из настроек роли
-$vacancyDescription = strip_tags($model->vacancy->event->project->shortdescription);
+$vacancyDescription = strip_tags($project->shortdescription);
 Yii::app()->clientScript->registerMetaTag($vacancyDescription, 'description', null, array('lang' => 'ru'));
-
-// ссылка на баннер
-$bannerUrl = Yii::app()->createAbsoluteUrl('/projects/project/banner', array('id' => $model->vacancy->event->project->id));
-if ( $bannerUrl )
+if ( $project->isCloaked() )
+{// запрет индексации поисковиками
+    Yii::app()->clientScript->registerMetaTag('noindex', 'robots');
+}
+if ( $project->hasBanner() )
 {// выводим баннер до основного содержимого, чтобы он располагался по во всю ширину страницы
-    $bannerImage = CHtml::image($bannerUrl, $model->vacancy->event->project->name, array('style' => 'max-width:100%;'));
+    $bannerImage = CHtml::image($project->getBannerUrl(), CHtml::encode($project->name), array('style' => 'max-width:100%;'));
     echo CHtml::tag('div', array('class' => 'row-fluid text-center'), $bannerImage);
 }
 ?>
 <div class="container">
     <div class="row-fluid">
         <?php 
-        if ( ! $bannerUrl )
+        if ( ! $project->hasBanner() )
         {// баннера нет - выводим стандартную информацию для подачи заявки через форму
             $this->widget('projects.extensions.ProjectInfo.ProjectInfo', array(
                 'eventId'     => $model->vacancy->event->id,
                 'displayTabs' => array('main'),
             ));
-        }else
-        {
-            // и собственное описание перед формой
-            /*if ( $greeting = $model->vacancy->getConfig('customGreeting') )
-            {
-                echo $greeting;
-                echo CHtml::tag('div', array('class' => 'well'), $bannerImage);
-            }
-            $this->widget('ext.FlipCountDown.FlipCountDown', array(
-                'beforeUnixTime' => $model->vacancy->getConfig(''),
-                'settings'       => array('size' => 'lg'),
-            ));*/
         }
         ?>
     </div>
@@ -56,7 +46,7 @@ if ( $bannerUrl )
     </div>
     <div class="row text-center">
         <?php
-        // @todo другие возможные роли (включить после тестирования)
+        // @todo другие возможные роли
         /*$this->widget('questionary.extensions.widgets.QUserInvites.QUserInvites', array(
             'questionary' => $model->questionary,
         ));*/
